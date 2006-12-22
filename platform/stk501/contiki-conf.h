d4b3436d21f618dd7ae331c32d4db4ee679bdc0c
barner <barner>
2006-12-22 17:19:22 +0000

    Add new plattform definition for the STK 501 board featuring an Atmel ATmega128.
    Very brief instructions:
    
    - Configure your programmer etc. in Makefile.stk501
    - Use 'make upload' to flash your STK501.
    - Setup a SLIP connection to your board (on the first serial port)
      and make sure you can ping it.
    - Use 'make loadable_prg.ko' to build a demo module.
    - gcc -ocodeprop ../../tools/codeprop.c
    - ./codeprop <IP of STK> loadable_prg.ko
    - Watch the module print "Tick, Tack, Tick, ..." on the second
      serial port.

---
+++ platform/stk501/contiki-conf.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2006, Technical University of Munich
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * @(#)$$
+ */
+
+/**
+ * \file
+ *         Configuration for sample STK 501 Contiki kernel
+ *
+ * \author
+ *         Simon Barner <barner@in.tum.de
+ */
+
+#ifndef __CONTIKI_CONF_H__
+#define __CONTIKI_CONF_H__
+
+/*
+ * MCU and clock rate
+ */
+#define MCU_MHZ 16
+#define PLATFORM PLATFORM_AVR
+
+/* Cock ticks per second */
+#define CLOCK_CONF_SECOND 125
+
+/* COM port to be used for SLIP connection */
+#define SLIP_PORT RS232_PORT_0
+
+/* Pre-allocated memory for loadable modules heap space (in bytes)*/
+#define MMEM_CONF_SIZE 256
+
+/* Use the following address for code received via the codeprop
+ * facility
+ */
+#define EEPROMFS_ADDR_CODEPROP 0x8000
+
+#define CCIF
+#define CLIF
+
+#define UIP_CONF_PINGADDRCONF    0
+#define UIP_CONF_MAX_CONNECTIONS 4
+#define UIP_CONF_MAX_LISTENPORTS 4
+#define UIP_CONF_BUFFER_SIZE     100
+#define UIP_CONF_TCP_SPLIT       1
+#define UIP_CONF_FWCACHE_SIZE    2
+#define UIP_CONF_BROADCAST       1
+#define UIP_UDP			 1
+
+typedef unsigned short clock_time_t;
+typedef unsigned char u8_t;
+typedef unsigned short u16_t;
+typedef unsigned long u32_t;
+typedef unsigned short uip_stats_t;
+typedef unsigned long off_t;
+
+void clock_delay(unsigned int us2);
+void clock_wait(int ms10);
+void clock_set_seconds(unsigned long s);
+unsigned long clock_seconds(void);
+
+#endif /* __CONTIKI_CONF_H__ */
