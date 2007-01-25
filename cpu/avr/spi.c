20fa9351f243bd7aaa8ad8b0caf82138dc86fefc
bg- <bg->
2007-01-25 18:22:55 +0000

    * First port of CC2420 driver to the AVR.

---
+++ cpu/avr/spi.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2007, Swedish Institute of Computer Science
+ * All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution. 
+ * 3. Neither the name of the Institute nor the names of its contributors 
+ *    may be used to endorse or promote products derived from this software 
+ *    without specific prior written permission. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND 
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ * SUCH DAMAGE. 
+ *
+ * @(#)$Id: spi.c,v 1.1 2007/01/25 18:22:55 bg- Exp $
+ */
+
+#include <avr/io.h>
+
+#include "contiki-conf.h"
+
+/*
+ * On the Tmote sky access to I2C/SPI/UART0 must always be
+ * exclusive. Set spi_busy so that interrupt handlers can check if
+ * they are allowed to use the bus or not. Only the CC2420 radio needs
+ * this in practice.
+ * 
+ */
+unsigned char spi_busy = 0;
+
+/*
+ * Initialize SPI bus.
+ */
+void
+spi_init(void)
+{
+  static unsigned char spi_inited = 0;
+
+  if (spi_inited)
+    return;
+
+  /* Initalize ports for communication with SPI units. */
+  /* CSN=SS and must be output when master! */
+  DDRB  |= BV(MOSI) | BV(SCK) | BV(CSN);
+  PORTB |= BV(MOSI) | BV(SCK);
+
+  /* Enables SPI, selects "master", clock rate FCK / 2, and SPI mode 0 */
+  SPCR = BV(SPE) | BV(MSTR);
+  SPSR = BV(SPI2X);
+}
