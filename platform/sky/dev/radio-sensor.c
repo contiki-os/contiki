c5aeed9d00790a1df8b181193a325be98b8207fc
adamdunkels <adamdunkels>
2007-03-22 23:53:56 +0000

    Radio sensor (RSSI) interface

---
+++ platform/sky/dev/radio-sensor.c
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2005, Swedish Institute of Computer Science
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * @(#)$Id: radio-sensor.c,v 1.1 2007/03/22 23:53:56 adamdunkels Exp $
+ */
+
+#include "lib/sensors.h"
+#include "dev/irq.h"
+#include "dev/simple-cc2420.h"
+#include "dev/radio-sensor.h"
+
+const struct sensors_sensor radio_sensor;
+
+/*---------------------------------------------------------------------------*/
+static void
+init(void)
+{
+}
+/*---------------------------------------------------------------------------*/
+static int
+irq(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static void
+activate(void)
+{
+}
+/*---------------------------------------------------------------------------*/
+static void
+deactivate(void)
+{
+}
+/*---------------------------------------------------------------------------*/
+static int
+active(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static unsigned int
+value(int type)
+{
+  switch(type) {
+  case RADIO_SENSOR_LAST_PACKET:
+    return simple_cc2420_last_correlation;
+  case RADIO_SENSOR_LAST_VALUE:
+  default:
+    return simple_cc2420_last_rssi;
+  }
+}
+/*---------------------------------------------------------------------------*/
+static int
+configure(int type, void *c)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static void *
+status(int type)
+{
+  return NULL;
+}
+/*---------------------------------------------------------------------------*/
+SENSORS_SENSOR(radio_sensor, RADIO_SENSOR,
+	       init, irq, activate, deactivate, active,
+	       value, configure, status);
