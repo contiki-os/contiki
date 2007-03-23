7a5d74e407d01db66151945b5566440052cc6263
fros4943 <fros4943>
2007-03-23 20:48:29 +0000

    removed pressed button check

--- tools/cooja/java/se/sics/cooja/plugins/Visualizer2D.java
+++ tools/cooja/java/se/sics/cooja/plugins/Visualizer2D.java
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: Visualizer2D.java,v 1.7 2007/03/22 11:14:27 fros4943 Exp $
+ * $Id: Visualizer2D.java,v 1.8 2007/03/23 20:48:29 fros4943 Exp $
  */
 
 package se.sics.cooja.plugins;
@@ -103,7 +103,7 @@ public abstract class Visualizer2D extends VisPlugin {
 
   private class ButtonClickMoteMenuAction implements MoteMenuAction {
     public boolean isEnabled(Mote mote) {
-      return mote.getInterfaces().getButton() != null && !mote.getInterfaces().getButton().isPressed();
+      return mote.getInterfaces().getButton() != null;
     }
     public String getDescription(Mote mote) {
       return "Click button on " + mote;
