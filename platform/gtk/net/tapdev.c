cdea3d04dfe52b7d10fcaea5993ccca424e3f0a5
oliverschmidt <oliverschmidt>
2007-03-27 20:49:09 +0000

    Copied tapdev from minimal-net to gtk.

--- platform/gtk/net/tapdev.c
+++ platform/gtk/net/tapdev.c
@@ -1,39 +1,38 @@
 /*
  * Copyright (c) 2001, Swedish Institute of Computer Science.
- * All rights reserved. 
+ * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions 
- * are met: 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * 1. Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  *
- * 2. Redistributions in binary form must reproduce the above copyright 
- *    notice, this list of conditions and the following disclaimer in the 
- *    documentation and/or other materials provided with the distribution. 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
  *
- * 3. Neither the name of the Institute nor the names of its contributors 
- *    may be used to endorse or promote products derived from this software 
- *    without specific prior written permission. 
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND 
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE 
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS 
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
- * SUCH DAMAGE. 
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  *
  * Author: Adam Dunkels <adam@sics.se>
  *
- * $Id: tapdev.c,v 1.1 2006/06/17 22:41:29 adamdunkels Exp $
+ * $Id: tapdev.c,v 1.2 2007/03/27 20:49:09 oliverschmidt Exp $
  */
-
 
 #include <fcntl.h>
 #include <stdlib.h>
@@ -47,7 +46,6 @@
 #include <sys/uio.h>
 #include <sys/socket.h>
 
-
 #ifdef linux
 #include <sys/ioctl.h>
 #include <linux/if.h>
@@ -58,46 +56,20 @@
 #endif /* linux */
 
 #include "contiki-net.h"
+#include "tapdev.h"
 
 #define DROP 0
 
+#if DROP
 static int drop = 0;
+#endif
+
 static int fd;
 
 static unsigned long lasttime;
-static struct timezone tz;
 
 #define BUF ((struct uip_eth_hdr *)&uip_buf[0])
 
-static void do_send(void);
-u8_t tapdev_send(void);
-
-
-u16_t
-tapdev_poll(void)
-{
-  fd_set fdset;
-  struct timeval tv;
-  int ret;
-  
-  tv.tv_sec = 0;
-  tv.tv_usec = 0;
-  
-  FD_ZERO(&fdset);
-  if(fd > 0) {
-    FD_SET(fd, &fdset);
-  }
-
-  ret = select(fd + 1, &fdset, NULL, NULL, &tv);
-  if(ret == 0) {
-    return 0;
-  } 
-  ret = read(fd, uip_buf, UIP_BUFSIZE);  
-  if(ret == -1) {
-    perror("tapdev_poll: read");
-  }
-  return ret; 
-}
 /*---------------------------------------------------------------------------*/
 void
 tapdev_init(void)
@@ -124,27 +96,51 @@ tapdev_init(void)
 
   snprintf(buf, sizeof(buf), "ifconfig tap0 inet 192.168.2.1");
   system(buf);
-  printf("%s\n", buf); 
+  printf("%s\n", buf);
 
   lasttime = 0;
-
-  /*  gdk_input_add(fd, GDK_INPUT_READ,
-      read_callback, NULL);*/
-
 }
 /*---------------------------------------------------------------------------*/
-static void 
-do_send(void) 
+u16_t
+tapdev_poll(void)
+{
+  fd_set fdset;
+  struct timeval tv;
+  int ret;
+  
+  tv.tv_sec = 0;
+  tv.tv_usec = 0;
+  
+  FD_ZERO(&fdset);
+  if(fd > 0) {
+    FD_SET(fd, &fdset);
+  }
+
+  ret = select(fd + 1, &fdset, NULL, NULL, &tv);
+
+  if(ret == 0) {
+    return 0;
+  }
+  ret = read(fd, uip_buf, UIP_BUFSIZE);
+
+  if(ret == -1) {
+    perror("tapdev_poll: read");
+  }
+  return ret;
+}
+/*---------------------------------------------------------------------------*/
+void
+tapdev_send(void)
 {
   int ret;
 
   if(fd <= 0) {
     return;
   }
-
  
   /*  printf("tapdev_send: sending %d bytes\n", size);*/
   /*  check_checksum(uip_buf, size);*/
+
 #if DROP
   drop++;
   if(drop % 8 == 7) {
@@ -159,20 +155,5 @@ do_send(void)
     perror("tap_dev: tapdev_send: writev");
     exit(1);
   }
-}  
-/*---------------------------------------------------------------------------*/
-u8_t
-tapdev_send(void)
-{
-  uip_arp_out();
-
-  do_send();
-  return 0;
-}
-/*---------------------------------------------------------------------------*/
-void
-tapdev_do_send(void)
-{
-  do_send();
 }
 /*---------------------------------------------------------------------------*/
