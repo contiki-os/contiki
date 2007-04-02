094c72b1183c9fd6754cf66338b9837638c52753
fros4943 <fros4943>
2007-04-02 08:47:28 +0000

    new mulithreading switch function.
    should work on both linux and win32 platforms

--- platform/cooja/sys/cooja_mtarch.c
+++ platform/cooja/sys/cooja_mtarch.c
@@ -1,9 +1,38 @@
 /*
- * This file is ripped from x86/mtarch.c of the Contiki Multi-threading library.
- * Fredrik Osterlind <fros@sics.se>
+ * Copyright (c) 2005, Swedish Institute of Computer Science
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * @(#)$Id: cooja_mtarch.c,v 1.5 2007/04/02 08:47:28 fros4943 Exp $
  */
 
 #include <stdio.h>
+#include <string.h>
 #include "sys/cooja_mt.h"
 
 struct frame {
@@ -19,19 +48,16 @@ struct frame {
   unsigned long retaddr2;
   unsigned long data;
 };
-
 /*--------------------------------------------------------------------------*/
 void
 cooja_mtarch_init(void)
 {
-  
 }
 /*--------------------------------------------------------------------------*/
 void
 cooja_mtarch_start(struct cooja_mtarch_thread *t,
-	     void (*function)(void *), void *data)
+		   void (*function)(void *), void *data)
 {
-  
   struct frame *f = (struct frame *)&t->stack[COOJA_MTARCH_STACKSIZE - sizeof(struct frame)/4];
   int i;
   
@@ -46,39 +72,44 @@ cooja_mtarch_start(struct cooja_mtarch_thread *t,
   f->ebp     = (unsigned long)&f->eax;
 }
 /*--------------------------------------------------------------------------*/
-__asm__ (
-	 "cooja_sw:\n\t"
-	 "pushl %ebp\n\t"
-	 "movl %esp, %ebp\n\t"
-	 
-	 "pushl %eax\n\t"
-	 "pushl %ebx\n\t"
-	 "pushl %ecx\n\t"
-	 "pushl %edx\n\t"
-	 "pushl %esi\n\t"
-	 "pushl %edi\n\t"
-	 "pushl %ebp\n\t"
-	 "pushl %ebp\n\t"
-	 
-	 "movl cooja_running_thread, %eax\n\t"
-	 "movl 0(%eax), %ebx\n\t"
-	 "movl %esp, 0(%eax)\n\t"
-	 "movl %ebx, %esp\n\t"
-	 
-	 "popl %ebp\n\t"
-	 "popl %ebp\n\t"
-	 "popl %edi\n\t"
-	 "popl %esi\n\t"
-	 "popl %edx\n\t"
-	 "popl %ecx\n\t"
-	 "popl %ebx\n\t"
-	 "popl %eax\n\t"
-	 
-	 "leave\n\t"
-	 "ret\n\t"							
-	 );
-/*--------------------------------------------------------------------------*/
 static struct cooja_mtarch_thread *cooja_running_thread;
+/*--------------------------------------------------------------------------*/
+void cooja_sw(void)
+{
+  /* Store registers */
+  __asm__ (
+	   "pushl %eax\n\t"
+	   "pushl %ebx\n\t"
+	   "pushl %ecx\n\t"
+	   "pushl %edx\n\t"
+	   "pushl %esi\n\t"
+	   "pushl %edi\n\t"
+	   "pushl %ebp\n\t"
+	   "pushl %ebp\n\t");
+  
+  /* Switch stack pointer */
+  __asm__ ("movl %0, %%eax\n\t" : : "m" (cooja_running_thread));
+  __asm__ (
+	   "movl 0(%eax), %ebx\n\t"
+	   "movl %esp, 0(%eax)\n\t"
+	   "movl %ebx, %esp\n\t"
+	   );
+  
+  /* Restore stored registers and return "our" way */
+  __asm__ (
+	   "popl %ebp\n\t"
+	   "popl %ebp\n\t"
+	   "popl %edi\n\t"
+	   "popl %esi\n\t"
+	   "popl %edx\n\t"
+	   "popl %ecx\n\t"
+	   "popl %ebx\n\t"
+	   "popl %eax\n\t"
+	   
+	   "leave\n\t"
+	   "ret\n\t"							
+	   );
+}
 /*--------------------------------------------------------------------------*/
 void
 cooja_mtarch_exec(struct cooja_mtarch_thread *t)
@@ -91,7 +122,6 @@ cooja_mtarch_exec(struct cooja_mtarch_thread *t)
 void
 cooja_mtarch_remove(void)
 {
-
 }
 /*--------------------------------------------------------------------------*/
 void
@@ -103,13 +133,11 @@ cooja_mtarch_yield(void)
 void
 cooja_mtarch_pstop(void)
 {
-  
 }
 /*--------------------------------------------------------------------------*/
 void
 cooja_mtarch_pstart(void)
 {
-  
 }
 /*--------------------------------------------------------------------------*/
 int
