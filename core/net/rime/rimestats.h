f34bced85e6eb6a7d371ac8d4adfd296cc54fe0e
adamdunkels <adamdunkels>
2007-05-22 20:48:41 +0000

    Rime statistics gathering

---
+++ core/net/rime/rimestats.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * $Id: rimestats.h,v 1.1 2007/05/22 20:48:41 adamdunkels Exp $
+ */
+
+/**
+ * \file
+ *         Header file for Rime statistics
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#ifndef __RIMESTATS_H__
+#define __RIMESTATS_H__
+
+struct rimestats {
+  unsigned long tx, rx;
+
+  unsigned long reliabletx, reliablerx,
+    rexmit, acktx, noacktx, ackrx, timedout, badackrx;
+
+  /* Reasons for dropping incoming packets: */
+  unsigned long toolong, tooshort, badsynch, badcrc;
+
+  unsigned long contentiondrop, /* Packet dropped due to contention */
+    sendingdrop; /* Packet dropped when we were sending a packet */
+
+  unsigned long lltx, llrx;
+};
+
+extern struct rimestats rimestats;
+
+#define RIMESTATS_ADD(x) rimestats.x++
+
+#endif /* __RIMESTATS_H__ */
