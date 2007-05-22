f801ab10fff18652dfa79785f1ba313bb5a05293
oliverschmidt <oliverschmidt>
2007-05-22 21:19:34 +0000

    Explicitly shut down network driver.

--- platform/minimal-net/contiki-main.c
+++ platform/minimal-net/contiki-main.c
@@ -29,9 +29,13 @@
  *
  * This file is part of the Contiki OS
  *
- * $Id: contiki-main.c,v 1.6 2007/05/20 21:36:31 oliverschmidt Exp $
+ * $Id: contiki-main.c,v 1.7 2007/05/22 21:19:34 oliverschmidt Exp $
  *
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #include "contiki.h"
 
@@ -48,6 +52,16 @@ PROCINIT(&etimer_process, &tcpip_process, &wpcap_process);
 PROCINIT(&etimer_process, &tcpip_process, &tapdev_process);
 #endif
 
+/*-----------------------------------------------------------------------------------*/
+void
+exit_handler(void)
+{
+#ifdef __CYGWIN__
+  process_post_synch(&wpcap_process, PROCESS_EVENT_EXIT, NULL);
+#else
+  process_post_synch(&tapdev_process, PROCESS_EVENT_EXIT, NULL);
+#endif
+}
 /*---------------------------------------------------------------------------*/
 int
 main(void)
@@ -68,6 +82,8 @@ main(void)
 
   uip_ipaddr(&addr, 255,255,255,0);
   uip_setnetmask(&addr);
+
+  atexit(exit_handler);
 
   while(1) {
     int n;
