3b303571d8154050328c4701fc73e4e6b92bd985
adamdunkels <adamdunkels>
2007-05-22 21:04:00 +0000

    Renamed uabc to polite

--- examples/rime/test-polite.c
+++ examples/rime/test-polite.c
@@ -1,46 +1,46 @@
 
-#include "net/rime/uabc.h"
+#include "net/rime/polite.h"
 #include "contiki.h"
 
 #include <stdio.h>
 
 /*---------------------------------------------------------------------------*/
-PROCESS(test_uabc_process, "");
-AUTOSTART_PROCESSES(&test_uabc_process);
+PROCESS(test_polite_process, "");
+AUTOSTART_PROCESSES(&test_polite_process);
 /*---------------------------------------------------------------------------*/
 static void
-recv(struct uabc_conn *c)
+recv(struct polite_conn *c)
 {
   printf("recv '%s'\n", (char *)rimebuf_dataptr());
 }
 static void
-sent(struct uabc_conn *c)
+sent(struct polite_conn *c)
 {
   printf("sent\n");
 }
 static void
-dropped(struct uabc_conn *c)
+dropped(struct polite_conn *c)
 {
   printf("dropped\n");
 }
-static const struct uabc_callbacks callbacks = { recv, sent, dropped };
+static const struct polite_callbacks callbacks = { recv, sent, dropped };
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(test_uabc_process, ev, data)
+PROCESS_THREAD(test_polite_process, ev, data)
 {
-  static struct uabc_conn c;
+  static struct polite_conn c;
 
-  PROCESS_EXITHANDLER(uabc_close(&c));
+  PROCESS_EXITHANDLER(polite_close(&c));
   
   PROCESS_BEGIN();
 
-  uabc_open(&c, 128, &callbacks);
+  polite_open(&c, 128, &callbacks);
 
   while(1) {
     static struct etimer et;
     etimer_set(&et, CLOCK_SECOND * 4);
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
     rimebuf_copyfrom("Hej", 4);
-    uabc_send(&c, CLOCK_SECOND * 4);
+    polite_send(&c, CLOCK_SECOND * 4, 4);
   
   
   }
