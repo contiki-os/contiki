86f8f2a8af2dffc3bad4d66427629c2abadfd935
oliverschmidt <oliverschmidt>
2007-05-23 23:18:51 +0000

    Moved CTK service related stuff into backyard.

--- backyard/core/ctk/ctk-draw-service.h
+++ backyard/core/ctk/ctk-draw-service.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * Copyright (c) 2004, Adam Dunkels.
  * All rights reserved. 
  *
  * Redistribution and use in source and binary forms, with or without 
@@ -28,15 +28,46 @@
  *
  * This file is part of the Contiki operating system.
  * 
- * Author: Oliver Schmidt <ol.sc@web.de>
+ * Author: Adam Dunkels <adam@sics.se>
  *
- * $Id: ctk-conio-service.h,v 1.1 2006/08/14 23:36:14 oliverschmidt Exp $
+ * $Id: ctk-draw-service.h,v 1.1 2007/05/23 23:19:13 oliverschmidt Exp $
  */
-#ifndef __CTK_CONIO_SERVICE_H__
-#define __CTK_CONIO_SERVICE_H__
+#ifndef __CTK_DRAW_SERVICE_H__
+#define __CTK_DRAW_SERVICE_H__
 
-#include "contiki.h"
 
-PROCESS_NAME(ctk_conio_service_process);
+#include "ctk/ctk.h"
+#include "sys/service.h"
 
-#endif /* __CTK_CONIO_SERVICE_H__ */
+#define ctk_draw_service_name "CTK driver"
+SERVICE_INTERFACE(ctk_draw_service, {
+  unsigned char windowborder_width;
+  unsigned char windowborder_height;
+  unsigned char windowtitle_height;
+  
+  void (* draw_init)(void);
+  void (* draw_clear)(unsigned char clipy1, unsigned char clipy2);
+  void (* draw_clear_window)(struct ctk_window *window,
+			     unsigned char focus,
+			     unsigned char clipy1,
+			     unsigned char clipy2);
+  void (* draw_window)(struct ctk_window *window,
+		       unsigned char focus,
+		       unsigned char clipy1,
+		       unsigned char clipy2,
+		       unsigned char draw_borders);
+  void (* draw_dialog)(struct ctk_window *dialog);
+  void (* draw_widget)(struct ctk_widget *widget,
+		       unsigned char focus,
+		       unsigned char clipy1,
+		       unsigned char clipy2);
+  void (* draw_menus)(struct ctk_menus *menus);
+
+  unsigned char (* width)(void);
+  unsigned char (* height)(void);
+
+  unsigned short (* mouse_xtoc)(unsigned short);
+  unsigned short (* mouse_ytoc)(unsigned short);
+});
+
+#endif /* __CTK_DRAW_SERVICE_H__ */
