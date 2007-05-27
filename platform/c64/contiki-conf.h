a7f423d6c9d457ab1541651a5b05cd9a423b2ba2
oliverschmidt <oliverschmidt>
2007-05-27 20:54:13 +0000

    Start out with minimal Contiki config.

---
+++ platform/c64/contiki-conf.h
@@ -0,0 +1,13 @@
+#ifndef __CONTIKI_CONF_H__
+#define __CONTIKI_CONF_H__
+
+#include "6502-conf.h"
+
+#define LOG_CONF_ENABLED 1
+
+#define UIP_CONF_BUFFER_SIZE   1500
+#define UIP_CONF_TCP_SPLIT     1
+#define UIP_CONF_UDP_CHECKSUMS 1
+#define UIP_CONF_LOGGING       1
+
+#endif /* __CONTIKI_CONF_H__ */
