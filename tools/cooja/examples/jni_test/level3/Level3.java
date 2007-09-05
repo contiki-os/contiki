e04e128a59f8aad9334aa0aad9571b04396b421a
fros4943 <fros4943>
2007-09-05 18:39:42 +0000

    updated jni_tests to new external tools configuration

--- tools/cooja/examples/jni_test/level3/Level3.java
+++ tools/cooja/examples/jni_test/level3/Level3.java
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: Level3.java,v 1.2 2007/01/11 14:26:47 fros4943 Exp $
+ * $Id: Level3.java,v 1.3 2007/09/05 18:39:42 fros4943 Exp $
  */
 
 import java.io.*;
@@ -44,7 +44,7 @@ public class Level3 {
     "^.data[ \t]*0x([0-9A-Fa-f]*)[ \t]*0x[0-9A-Fa-f]*[ \t]*$";
   final static private String dataSectionSizeRegExp =
     "^.data[ \t]*0x[0-9A-Fa-f]*[ \t]*0x([0-9A-Fa-f]*)[ \t]*$";
-  
+
   static {
     System.load(new File("level3.library").getAbsolutePath());
   }
@@ -65,20 +65,20 @@ public class Level3 {
 
     System.err.println("Parsing map file");
     int relDataSectionAddr = loadRelDataSectionAddr(mapContents);
-    int dataSectionSize = (int) loadDataSectionSize(mapContents);
+    int dataSectionSize = loadDataSectionSize(mapContents);
     int relBssSectionAddr = loadRelBssSectionAddr(mapContents);
-    int bssSectionSize = (int) loadBssSectionSize(mapContents);
+    int bssSectionSize = loadBssSectionSize(mapContents);
 
     System.err.println("Found relative data section address: 0x" + Integer.toHexString(relDataSectionAddr));
     System.err.println("Found data section size: 0x" + Integer.toHexString(dataSectionSize));
     System.err.println("Found relative bss section address: 0x" + Integer.toHexString(relBssSectionAddr));
     System.err.println("Found bss section size: 0x" + Integer.toHexString(bssSectionSize));
-    
-    if (dataSectionSize != 4) {
+
+    if (dataSectionSize < 4) {
       System.err.println("Data section size is " + Integer.toHexString(dataSectionSize) + " but should be 0x4!");
       System.exit(1);
     }
-    if (bssSectionSize != 4) {
+    if (bssSectionSize < 4) {
       System.err.println("BSS section size is " + Integer.toHexString(bssSectionSize) + " but should be 0x4!");
       System.exit(1);
     }
@@ -87,13 +87,13 @@ public class Level3 {
 
   private static Vector<String> loadMapFile(File mapFile) {
     Vector<String> mapContents = new Vector<String>();
-    
+
     try {
       BufferedReader in =
         new BufferedReader(
             new InputStreamReader(
                 new FileInputStream(mapFile)));
-      
+
       while (in.ready())
       {
         mapContents.add(in.readLine());
@@ -105,40 +105,48 @@ public class Level3 {
       System.err.println("IO error: " + e);
       return null;
     }
-    
+
     return mapContents;
   }
 
   private static int loadRelDataSectionAddr(Vector<String> mapFile) {
     String retString = getFirstMatchGroup(mapFile, dataSectionAddrRegExp, 1);
 
-    if (retString != null)
+    if (retString != null) {
       return Integer.parseInt(retString.trim(), 16);
-    else return 0;
+    } else {
+      return 0;
+    }
   }
 
   private static int loadDataSectionSize(Vector<String> mapFile) {
     String retString = getFirstMatchGroup(mapFile, dataSectionSizeRegExp, 1);
 
-    if (retString != null)
+    if (retString != null) {
       return Integer.parseInt(retString.trim(), 16);
-    else return 0;
+    } else {
+      return 0;
+    }
   }
 
   private static int loadRelBssSectionAddr(Vector<String> mapFile) {
     String retString = getFirstMatchGroup(mapFile, bssSectionAddrRegExp, 1);
 
-    if (retString != null)
+    if (retString != null) {
       return Integer.parseInt(retString.trim(), 16);
-    else return 0;
+    } else {
+      return 0;
+    }
   }
 
   private static int loadBssSectionSize(Vector<String> mapFile) {
     String retString = getFirstMatchGroup(mapFile, bssSectionSizeRegExp, 1);
 
-    if (retString != null)
+    if (retString != null) {
       return Integer.parseInt(retString.trim(), 16);
-    else return 0;
+    } else {
+      return 0;
+    }
   }
 
   private static String getFirstMatchGroup(Vector<String> lines, String regexp, int groupNr) {
