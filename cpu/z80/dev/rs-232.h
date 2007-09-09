6f950220e49f6cd7d03649f02505c01acfe47d27
matsutsuka <matsutsuka>
2007-09-09 12:22:34 +0000

    Not signal but polling-based rs-232c process.

---
+++ cpu/z80/dev/rs-232.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2007, Takahide Matsutsuka.
+ * All rights reserved. 
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met: 
+ * 1. Redistributions of source code must retain the above copyright 
+ *    notice, this list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided
+ *    with the distribution. 
+ * 3. The name of the author may not be used to endorse or promote
+ *    products derived from this software without specific prior
+ *    written permission.  
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+ *
+ * $Id: rs-232.h,v 1.1 2007/09/09 12:22:34 matsutsuka Exp $
+ *
+ */
+/*
+ * \file
+ * 	This is RS-232C process based on polling.
+ * \author
+ * 	Takahide Matsutsuka <markn@markn.org>
+ */
+
+#ifndef __RS_232C_H__
+#define __RS_232C_H__
+
+/*
+ * Implement the following methods for each platform.
+ */
+
+/*
+ * RS-232C initialize.
+ */
+void rs232_arch_init(void);
+
+/*
+ * RS-232C polling.
+ * @return character, zero if no input.
+ */
+unsigned char rs232_arch_poll(void);
+
+/*
+ * RS-232C write a byte.
+ */
+void rs232_arch_writeb(u8_t ch);
+
+PROCESS_NAME(rs232_process);
+
+#endif /* __RS_232C_H__ */
