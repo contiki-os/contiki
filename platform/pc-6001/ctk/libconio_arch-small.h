d6ec25e37c5476b207b92323579e511fc86711f9
matsutsuka <matsutsuka>
2007-09-30 12:46:34 +0000

    The libconio_arch-small is architecture-depend implementation of console I/O with minimum size for the server mode.

---
+++ platform/pc-6001/ctk/libconio_arch-small.h
@@ -0,0 +1,8 @@
+#ifndef __LIBCONIO_ARCH_MSLL_H__
+#define __LIBCONIO_ARCH_MSLL_H__
+#include "contiki.h"
+
+void clrscr_arch();
+void libputc_arch(unsigned char ch);
+
+#endif /* __LIBCONIO_ARCH_MSLL_H__ */
