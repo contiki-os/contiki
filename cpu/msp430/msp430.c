213246271615d9c7a0c827622bbf42fe31a8fbc1
adamdunkels <adamdunkels>
2007-11-17 10:28:04 +0000

    Updated to use the watchdog API

--- cpu/msp430/msp430.c
+++ cpu/msp430/msp430.c
@@ -28,12 +28,12 @@
  *
  * This file is part of the Contiki operating system.
  *
- * @(#)$Id: msp430.c,v 1.6 2007/05/21 15:29:46 bg- Exp $
+ * @(#)$Id: msp430.c,v 1.7 2007/11/17 10:28:18 adamdunkels Exp $
  */
 #include <io.h>
 #include <signal.h>
 #include <sys/unistd.h>
-
+#include "dev/watchdog.h"
 #include "net/uip.h"
 
 /*---------------------------------------------------------------------------*/
@@ -53,7 +53,6 @@ msp430_init_dco(void)
   BCSCTL2 = 0x00; /* Init FLL to desired frequency using the 32762Hz
 		     crystal DCO frquenzy = 2,4576 MHz  */
 
-  WDTCTL = WDTPW + WDTHOLD;             /* Stop WDT */
   BCSCTL1 |= DIVA1 + DIVA0;             /* ACLK = LFXT1CLK/8 */
   for(i = 0xffff; i > 0; i--);          /* Delay for XTAL to settle */
 
@@ -156,6 +155,7 @@ void
 msp430_cpu_init(void)
 {
   dint();
+  watchdog_stop();
   init_ports();
   msp430_init_dco();
   eint();
