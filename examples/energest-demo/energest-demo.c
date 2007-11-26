a677abb6b137bc5b1e215cd49c9df280f2f3ba34
adamdunkels <adamdunkels>
2007-11-26 23:36:09 +0000

    Fixed compiler warning

--- examples/energest-demo/energest-demo.c
+++ examples/energest-demo/energest-demo.c
@@ -28,7 +28,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: energest-demo.c,v 1.1 2007/10/25 12:56:28 adamdunkels Exp $
+ * $Id: energest-demo.c,v 1.2 2007/11/26 23:36:09 adamdunkels Exp $
  */
 
 /**
@@ -90,7 +90,7 @@ static struct energy_time last;
 static struct energy_time diff;
 
 // FIXME: workaround to turn on/off radio. Rime should export an MAC on/off interface to avoid forcing the user to do this explicitly
-static struct mac_driver *mac = &nullmac_driver;
+static const struct mac_driver *mac = &nullmac_driver;
 
 static clock_time_t sleep_cycles;
 static const char send_string[] = "I am a sending string.";
