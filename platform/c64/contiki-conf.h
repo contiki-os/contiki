bcfc99fa5b872289925fbba00ff76b3c2819beae
oliverschmidt <oliverschmidt>
2007-11-28 00:00:29 +0000

    Limit the maximum number of simultaneous connections to the cfs webserver to the maximum number of open files.

--- platform/c64/contiki-conf.h
+++ platform/c64/contiki-conf.h
@@ -30,7 +30,7 @@
  * 
  * Author: Oliver Schmidt <ol.sc@web.de>
  *
- * $Id: contiki-conf.h,v 1.9 2007/11/26 21:52:26 oliverschmidt Exp $
+ * $Id: contiki-conf.h,v 1.10 2007/11/28 00:00:29 oliverschmidt Exp $
  */
 
 #ifndef __CONTIKI_CONF_H__
@@ -46,6 +46,6 @@
 #define UIP_CONF_LOGGING       1
 
 #define WEBSERVER_CONF_CGI_CONNS UIP_CONNS
-#define WEBSERVER_CONF_CFS_CONNS UIP_CONNS
+#define WEBSERVER_CONF_CFS_CONNS 8
 
 #endif /* __CONTIKI_CONF_H__ */
