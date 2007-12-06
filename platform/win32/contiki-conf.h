32ae3317a66b524b62a7545231a85624ee295125
oliverschmidt <oliverschmidt>
2007-12-06 22:09:56 +0000

    Several minor changes and reformatting.

--- platform/win32/contiki-conf.h
+++ platform/win32/contiki-conf.h
@@ -6,14 +6,12 @@
 #define CC_CONF_INLINE        __inline
 
 
-
 #if _USRDLL
 #define CCIF __declspec(dllimport)
 #else
 #define CCIF __declspec(dllexport)
 #endif
 #define CLIF __declspec(dllexport)
-
 
 
 #ifdef __CYGWIN__
@@ -25,16 +23,37 @@ char* strdup(const char*);
 #endif
 
 
-
 #define LOG_CONF_ENABLED 1
 #include "sys/log.h"
 CCIF void debug_printf(char *format, ...);
 
 
-
 #define CLOCK_CONF_SECOND 1000
 typedef unsigned long clock_time_t;
 
+
+typedef   signed char    int8_t;
+typedef unsigned char   uint8_t;
+typedef   signed short  int16_t;
+typedef unsigned short uint16_t;
+typedef   signed long   int32_t;
+typedef unsigned long  uint32_t;
+
+/* These names are deprecated, use C99 names. */
+typedef unsigned char   u8_t;
+typedef unsigned short u16_t;
+typedef unsigned long  u32_t;
+typedef          long  s32_t;
+
+typedef unsigned short uip_stats_t;
+
+#define UIP_CONF_MAX_CONNECTIONS 40
+#define UIP_CONF_MAX_LISTENPORTS 40
+#define UIP_CONF_BUFFER_SIZE     420
+#define UIP_CONF_BYTE_ORDER      UIP_LITTLE_ENDIAN
+#define UIP_CONF_TCP_SPLIT       1
+#define UIP_CONF_LOGGING         1
+#define UIP_CONF_UDP_CHECKSUMS   1
 
 
 #include <ctype.h>
@@ -58,6 +77,7 @@ typedef unsigned long clock_time_t;
 #define CTK_CONF_WIDGETDOWN_KEY   '\t'
 #define CTK_CONF_WIDGETUP_KEY     -7  /* Shift-Tab */
 #define CTK_CONF_MOUSE_SUPPORT    1
+#define CTK_CONF_WINDOWS          1
 #define CTK_CONF_ICONS            1
 #define CTK_CONF_ICON_BITMAPS     0
 #define CTK_CONF_ICON_TEXTMAPS    1
@@ -93,75 +113,32 @@ typedef unsigned long clock_time_t;
 #define ACTIVEMENUITEMCOLOR COLOR_YELLOW | COLOR_BLUE * 0x10
 
 
-
-#define EMAIL_CONF_WIDTH 76
-#define EMAIL_CONF_HEIGHT 25
-
-
-
-#define IRC_CONF_WIDTH 78
-#define IRC_CONF_HEIGHT 30
-#define IRC_CONF_SYSTEM_STRING "Win32"
-
-
-
 #define LOADER_CONF_ARCH "loader/dll-loader.h"
 
 #define PROGRAM_HANDLER_CONF_MAX_NUMDSCS 10
 #define PROGRAM_HANDLER_CONF_QUIT_MENU   1
 
 
+#define EMAIL_CONF_WIDTH  76
+#define EMAIL_CONF_HEIGHT 25
+
+
+#define IRC_CONF_WIDTH         78
+#define IRC_CONF_HEIGHT        30
+#define IRC_CONF_SYSTEM_STRING "Win32"
+
 
 #define SHELL_CONF_WITH_PROGRAM_HANDLER 1
-
 
 
 #define SHELL_GUI_CONF_XSIZE 46
 #define SHELL_GUI_CONF_YSIZE 22
 
 
-
-typedef   signed char    int8_t;
-typedef unsigned char   uint8_t;
-typedef   signed short  int16_t;
-typedef unsigned short uint16_t;
-typedef   signed long   int32_t;
-typedef unsigned long  uint32_t;
-
-/* These names are deprecated, use C99 names. */
-typedef unsigned char   u8_t;
-typedef unsigned short u16_t;
-typedef unsigned long  u32_t;
-typedef          long  s32_t;
-
-typedef unsigned short uip_stats_t;
-
-#define UIP_CONF_MAX_CONNECTIONS 40
-#define UIP_CONF_MAX_LISTENPORTS 40
-#define UIP_CONF_BUFFER_SIZE     420
-#define UIP_CONF_BYTE_ORDER      UIP_LITTLE_ENDIAN
-#define UIP_CONF_TCP_SPLIT       1
-#define UIP_CONF_LOGGING         1
-#define UIP_CONF_UDP_CHECKSUMS   1
+#define TELNETD_CONF_GUI 1
 
 
-
-#define WWW_CONF_WEBPAGE_WIDTH      76
-#define WWW_CONF_WEBPAGE_HEIGHT     30
-#define WWW_CONF_HISTORY_SIZE       40
-#define WWW_CONF_MAX_URLLEN         200
-#define WWW_CONF_MAX_NUMPAGEWIDGETS 80
-#define WWW_CONF_RENDERSTATE        1
-#define WWW_CONF_FORMS              1
-#define WWW_CONF_MAX_FORMACTIONLEN  200
-#define WWW_CONF_MAX_FORMNAMELEN    200
-#define WWW_CONF_MAX_INPUTNAMELEN   200
-#define WWW_CONF_MAX_INPUTVALUELEN  240
-
-
-
-#define TELNETD_CONF_LINELEN  64
-#define TELNETD_CONF_NUMLINES 16
-#define TELNETD_CONF_GUI      1
+#define WWW_CONF_WEBPAGE_WIDTH  76
+#define WWW_CONF_WEBPAGE_HEIGHT 30
 
 #endif /* __CONTIKI_CONF_H__ */
