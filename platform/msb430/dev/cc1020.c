e56be49e2c924a912532f190456135e43ad8da4b
nvt-se <nvt-se>
2007-12-18 15:26:25 +0000

    Wake up from sleep in the interrupt when the receiver process is going to be polled.

--- platform/msb430/dev/cc1020.c
+++ platform/msb430/dev/cc1020.c
@@ -272,6 +272,7 @@ int
 cc1020_read(void *buf, unsigned short size)
 {
   unsigned len;
+printf("Read time: %lu\n", RTIMER_NOW());
 
   if (cc1020_rxlen <= HDRSIZE)
     return 0;
@@ -450,7 +451,8 @@ interrupt(UART0RX_VECTOR) cc1020_rxhandler(void)
         // disable receiver
         DISABLE_RX_IRQ();
 	CC1020_SET_OPSTATE(CC1020_RX | CC1020_RX_PROCESSING);
-
+	_BIC_SR_IRQ(LPM3_bits);
+printf("Poll time: %lu\n", RTIMER_NOW());
         // call receiver to copy from buffer
         process_poll(&cc1020_receiver_process);
       }
