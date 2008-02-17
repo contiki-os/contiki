93789de9ddb586f40ebc7346ceeaa36e7b7b4c60
oliverschmidt <oliverschmidt>
2008-02-17 14:11:48 +0000

    Now that process initialization has been changed (back) to synchronous it needs to be called after reading the config file in order to preserve the existing logic.

--- platform/apple2enh/contiki-main.c
+++ platform/apple2enh/contiki-main.c
@@ -30,7 +30,7 @@
  * 
  * Author: Oliver Schmidt <ol.sc@web.de>
  *
- * $Id: contiki-main.c,v 1.15 2008/02/10 22:45:26 oliverschmidt Exp $
+ * $Id: contiki-main.c,v 1.16 2008/02/17 14:11:48 oliverschmidt Exp $
  */
 
 #include "contiki-net.h"
@@ -66,10 +66,6 @@ main(void)
 
   process_init();
 
-  procinit_init();
-
-  autostart_start(autostart_processes);
-
 #if 1
 
   ethernet_config = config_read("contiki.cfg");
@@ -95,7 +91,11 @@ main(void)
   }
 #endif
 
+  procinit_init();
+
   process_start((struct process *)&ethernet_process, (char *)ethernet_config);
+
+  autostart_start(autostart_processes);
 
   log_message("Contiki up and running ...", "");
   
