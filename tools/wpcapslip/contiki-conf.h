57f2dfa7434e14888eeb03113f181e3d2896fe67
adamdunkels <adamdunkels>
2008-02-24 21:14:25 +0000

    Decrease TTL for forwarded packets

--- tools/wpcapslip/contiki-conf.h
+++ tools/wpcapslip/contiki-conf.h
@@ -10,6 +10,8 @@ typedef uint32_t u32_t;
 typedef  int32_t s32_t;
 typedef unsigned short uip_stats_t;
 
+#define UIP_CONF_UIP_IP4ADDR_T_WITH_U32 1
+
 typedef unsigned long clock_time_t;
 #define CLOCK_CONF_SECOND 1000
 
