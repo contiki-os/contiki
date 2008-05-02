4ae5848d06f967db65d6d03a74380969503c52b5
fros4943 <fros4943>
2008-05-02 06:15:16 +0000

    shorter test duration

--- tools/cooja/contiki_tests/ctimer.js
+++ tools/cooja/contiki_tests/ctimer.js
@@ -1,4 +1,4 @@
-if (msg.startsWith("Callback function called at time 2000000 (counter=4000)")) {
+if (msg.startsWith("Callback function called at time 50000 (counter=100)")) {
   log.log("TEST OK\n");
   mote.getSimulation().getGUI().doQuit(false);
 }
