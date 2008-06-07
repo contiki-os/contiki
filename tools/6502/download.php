63d3ba3ba7bb2d6fc00eb21ca4711a2f5f070b7d
oliverschmidt <oliverschmidt>
2008-06-07 00:26:44 +0000

    Allow to download complete disk images (incl. custom config file) beside the pure custom config file. Is this kind of a hack as there are no disk image utilities used. Instead the offsets to the config file content in the current example-webserver disk image files is hardcoded into the PHP source code. As soon as the disk images change the offsets need to be adjusted.

--- tools/6502/download.php
+++ tools/6502/download.php
@@ -9,14 +9,20 @@ switch ($_GET['machine']) {
 case "apple2":
     $hex = $_GET['apple2-addr'];
     $drv = $_GET['apple2-drv'];
+    $ext = 'dsk';
+    $ofs = 0x0B500;
     break;
 case "c64":
     $hex = strtok($_GET['c64-addr-drv'],  '-');
     $drv = strtok('-');
+    $ext = 'd64';
+    $ofs = 0x17802;
     break;
 case "c128":
     $hex = strtok($_GET['c128-addr-drv'], '-');
     $drv = strtok('-');
+    $ext = 'd71';
+    $ofs = 0x17802;
     break;
 }
 
@@ -24,8 +30,15 @@ $addr = hexdec($hex);
 $cfg .= chr($addr % 0x100).chr($addr / 0x100);
 $cfg .= $drv;
 
+if ($_GET['disk']) {
+    $out = substr_replace(file_get_contents('contiki.' . $ext), $cfg, $ofs, strlen($cfg));
+} else {
+    $ext = 'cfg';
+    $out = $cfg;
+}
+
 header('Content-Type: application/octetstream');
-header('Content-Disposition: attachment; filename=contiki.cfg');
-print($cfg);
+header('Content-Disposition: attachment; filename=contiki.' . $ext);
+print($out);
 
 ?>
