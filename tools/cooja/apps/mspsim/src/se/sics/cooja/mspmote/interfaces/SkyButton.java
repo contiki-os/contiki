4eb02a56a02355df4c6b7defe7064261b43958ef
nifi <nifi>
2008-06-27 14:07:05 +0000

    added Button interface for the Sky plattform

---
+++ tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/interfaces/SkyButton.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2008, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: SkyButton.java,v 1.1 2008/06/27 14:07:05 nifi Exp $
+ */
+
+package se.sics.cooja.mspmote.interfaces;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Collection;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import org.jdom.Element;
+import se.sics.cooja.ClassDescription;
+import se.sics.cooja.Mote;
+import se.sics.cooja.interfaces.Button;
+import se.sics.cooja.mspmote.SkyMote;
+
+@ClassDescription("Button")
+public class SkyButton extends Button {
+
+  private SkyMote skyMote;
+
+  public SkyButton(Mote mote) {
+    skyMote = (SkyMote) mote;
+  }
+
+  public void clickButton() {
+    pressButton();
+    releaseButton();
+  }
+
+  public void releaseButton() {
+    skyMote.skyNode.setButton(false);
+    setChanged();
+    notifyObservers();
+  }
+
+  public void pressButton() {
+    skyMote.skyNode.setButton(true);
+    setChanged();
+    notifyObservers();
+  }
+
+  public boolean isPressed() {
+    return false;
+  }
+
+  public void doActionsBeforeTick() {
+  }
+
+  public void doActionsAfterTick() {
+  }
+
+  public double energyConsumptionPerTick() {
+    return 0.0;
+  }
+
+  public JPanel getInterfaceVisualizer() {
+    JPanel panel = new JPanel();
+    final JButton clickButton = new JButton("Click button");
+
+    panel.add(clickButton);
+
+    clickButton.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        clickButton();
+      }
+    });
+
+    return panel;
+  }
+
+  public void releaseInterfaceVisualizer(JPanel panel) {
+  }
+
+  public Collection<Element> getConfigXML() {
+    return null;
+  }
+
+  public void setConfigXML(Collection<Element> configXML, boolean visAvailable) {
+  }
+
+}
