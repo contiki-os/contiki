adecd32249af59fa87df4e464e80b94a21ee65e9
adamdunkels <adamdunkels>
2008-07-03 23:13:55 +0000

    CFS Coffee arch file for the native platform

---
+++ platform/native/cfs-coffee-arch.h
@@ -0,0 +1,75 @@
+#ifndef CFS_COFFEE_ARCH_H
+#define CFS_COFFEE_ARCH_H
+
+/*
+ * Copyright (c) 2008, Swedish Institute of Computer Science
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *	Coffee architecture-dependent header for the Tmote Sky platform.
+ * \author
+ * 	Nicolas Tsiftes <nvt@sics.se>
+ */
+
+#include "contiki-conf.h"
+#include "dev/xmem.h"
+
+#define COFFEE_SECTOR_SIZE		65536UL
+#define COFFEE_PAGE_SIZE		256UL
+#define COFFEE_START			COFFEE_SECTOR_SIZE
+#define COFFEE_SIZE			((1024 kb) - COFFEE_START)
+#define COFFEE_PAGE_COUNT		(COFFEE_SIZE / COFFEE_PAGE_SIZE)
+#define COFFEE_PAGES_PER_SECTOR		(COFFEE_SECTOR_SIZE / COFFEE_PAGE_SIZE)
+#define COFFEE_NAME_LENGTH		16
+#define COFFEE_DYN_SIZE			(16 kb)
+#define COFFEE_FD_SET_SIZE		8
+#define COFFEE_LOG_DIVISOR		4
+#define COFFEE_LOG_SIZE			(8 kb)
+#define COFFEE_LOG_TABLE_LIMIT		256
+#define COFFEE_DIR_CACHE_ENTRIES	16
+
+#define COFFEE_WRITE(buf, size, offset)				\
+		xmem_pwrite((char *)(buf), (size), COFFEE_START + (offset))
+
+#define COFFEE_READ(buf, size, offset)				\
+  		xmem_pread((char *)(buf), (size), COFFEE_START + (offset))
+
+#define COFFEE_ERASE(sector)					\
+  		xmem_erase(COFFEE_SECTOR_SIZE, COFFEE_START + (sector) * COFFEE_SECTOR_SIZE)
+
+#define READ_HEADER(hdr, page)						\
+  COFFEE_READ((hdr), sizeof (*hdr), (page) * COFFEE_PAGE_SIZE)
+
+#define WRITE_HEADER(hdr, page)						\
+  COFFEE_WRITE((hdr), sizeof (*hdr), (page) * COFFEE_PAGE_SIZE)
+
+#endif /* !COFFEE_ARCH_H */
