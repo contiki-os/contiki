1f7f0381dd1964df68f9ff634b7ca962735c6f48
adamdunkels <adamdunkels>
2008-07-03 23:32:03 +0000

    enable coffee code coffee and netcmd commands

--- examples/sky-shell/sky-shell.c
+++ examples/sky-shell/sky-shell.c
@@ -28,7 +28,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: sky-shell.c,v 1.1 2008/02/04 23:52:33 adamdunkels Exp $
+ * $Id: sky-shell.c,v 1.2 2008/07/03 23:32:03 adamdunkels Exp $
  */
 
 /**
@@ -98,16 +98,18 @@ PROCESS_THREAD(test_shell_process, ev, data)
   serial_shell_init();
   
   shell_blink_init();
+  shell_coffee_init();
   /*  shell_exec_init();*/
   shell_file_init();
   shell_netfile_init();
   shell_ps_init();
   shell_reboot_init();
   shell_rime_init();
+  shell_rime_netcmd_init();
   shell_rime_ping_init();
   shell_rime_sniff_init();
   /*  shell_rsh_init();*/
-  shell_sendtest_init();
+  /*  shell_sendtest_init();*/
   shell_sky_init();
   shell_text_init();
   shell_time_init();
