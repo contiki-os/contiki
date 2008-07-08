d2d54ad141d9b1d37e83f29423f2078647f3ece2
nifi <nifi>
2008-07-08 09:36:48 +0000

    changed clock resolution to 64 (needs to be a power of 2) and updated uIP settings

--- platform/esb/contiki-conf.h
+++ platform/esb/contiki-conf.h
@@ -13,13 +13,17 @@
 #define ENERGEST_CONF_ON 1
 
 #define HAVE_STDINT_H
-#include <msp430def.h>
+#include "msp430def.h"
 
 #define PROCESS_CONF_NUMEVENTS 8
 #define PROCESS_CONF_STATS 0
 
 /* CPU target speed in Hz */
 #define F_CPU 2457600uL
+
+/* Our clock resolution, this is the same as Unix HZ. Must be a power
+   of two (see clock.c for details). */
+#define CLOCK_CONF_SECOND 64
 
 #define IRQ_PORT1_VECTOR 1
 
@@ -41,7 +45,6 @@
 #define CLIF
 
 typedef unsigned short clock_time_t;
-#define CLOCK_CONF_SECOND 100
 
 void clock_wait(int ms10);
 
@@ -58,25 +61,25 @@ unsigned long clock_seconds(void);
  */
 typedef unsigned short uip_stats_t;
 
+#define UIP_CONF_ICMP_DEST_UNREACH 1
 
+#define UIP_CONF_IP_FORWARD      1
+#define UIP_CONF_DHCP_LIGHT
+#define UIP_CONF_LLH_LEN         0
+#define UIP_CONF_BUFFER_SIZE     110
+#define UIP_CONF_RECEIVE_WINDOW  (UIP_CONF_BUFFER_SIZE - 40)
 #define UIP_CONF_MAX_CONNECTIONS 4
 #define UIP_CONF_MAX_LISTENPORTS 8
-#define UIP_CONF_BUFFER_SIZE     140
-/*#define UIP_CONF_RECEIVE_WINDOW  140*/
-
-#define UIP_CONF_LLH_LEN         6
-
+#define UIP_CONF_UDP_CONNS       6
+#define UIP_CONF_FWCACHE_SIZE    1
+#define UIP_CONF_BROADCAST       1
+#define UIP_ARCH_IPCHKSUM        1
+#define UIP_CONF_UDP_CHECKSUMS   1
+#define UIP_CONF_PINGADDRCONF    0
+#define UIP_CONF_LOGGING         0
 #define UIP_CONF_RESOLV_ENTRIES  1
 
-#define UIP_CONF_PINGADDRCONF    1
-
-#define UIP_CONF_LOGGING         0
-
-#define UIP_CONF_UDP_CONNS       6
-
-#define UIP_CONF_FWCACHE_SIZE    1
-
-#define UIP_CONF_BROADCAST       1
+#define UIP_CONF_TCP_SPLIT       0
 
 #define LOADER_CONF_ARCH "loader/loader-arch.h"
 
