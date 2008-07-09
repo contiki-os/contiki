0d87322a18841cad7a45b1e2ffa2f8b2e564f2bf
nifi <nifi>
2008-07-09 20:45:06 +0000

    minor documentation update and removed unused function prototypes

--- platform/esb/dev/tr1001.h
+++ platform/esb/dev/tr1001.h
@@ -28,7 +28,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * @(#)$Id: tr1001.h,v 1.7 2008/07/08 12:27:22 nifi Exp $
+ * @(#)$Id: tr1001.h,v 1.8 2008/07/09 20:45:06 nifi Exp $
  */
 #ifndef __TR1001_H__
 #define __TR1001_H__
@@ -44,17 +44,17 @@
 extern const struct radio_driver tr1001_driver;
 
 /**
- * Initialize the radio tranciever.
+ * Initialize the radio transceiver.
  *
  * Turns on reception of bytes and installs the receive interrupt
  * handler.
  */
-void tr1001_init();
+void tr1001_init(void);
 
 /**
  * Set the speed of the TR1001 radio device.
  *
- * This function sets the speed of the TR1001 radio tranciever. Both
+ * This function sets the speed of the TR1001 radio transceiver. Both
  * the sender and the receiver must have the same speed for
  * communication to work.
  *
@@ -62,7 +62,6 @@ void tr1001_init();
  * TR1001_38400, TR1001_57600 or TR1001_115200.
  *
  */
-
 void tr1001_set_speed(unsigned char s);
 #define TR1001_19200  1
 #define TR1001_38400  2
@@ -70,17 +69,17 @@ void tr1001_set_speed(unsigned char s);
 #define TR1001_115200 4
 
 /**
- * Set the transmission power of the tranciever.
+ * Set the transmission power of the transceiver.
  *
  * The sensor board is equipped with a DS1804 100 position trimmer
  * potentiometer which is used to set the transmission input current
- * to the radio tranciever chip, thus setting the transmission power
- * of the radio tranciever.
+ * to the radio transceiver chip, thus setting the transmission power
+ * of the radio transceiver.
  *
  * This function sets the trimmer potentiometer to a value between 1
  * and 100.
  *
- * \param p The power of the tranciever, between 1 (lowest) and 100
+ * \param p The power of the transceiver, between 1 (lowest) and 100
  * (highest).
  */
 void tr1001_set_txpower(unsigned char p);
@@ -95,15 +94,8 @@ void tr1001_set_txpower(unsigned char p);
  */
 #define TR1001_TXPOWER_LOWEST  1
 
-void tr1001_set_sendtype(unsigned char t);
-#define TR1001_SENDTYPE_NORMAL  0
-#define TR1001_SENDTYPE_ACKED   1
-
-void tr1001_set_numrexmit(unsigned char acks);
-#define TR1001_NUMREXMIT_DEFAULT 2
-
 /**
- * Send a packet from the uip_buf buffer.
+ * Send a packet.
  *
  * This function causes a packet to be sent out after a small random
  * delay, but without doing any MAC layer collision detection or
@@ -112,7 +104,7 @@ void tr1001_set_numrexmit(unsigned char acks);
  * packet in network byte order.
  *
  * This function should normally not be called from user
- * programs. Rather, the uIP TCP/IP stack should be used.
+ * programs. Rather, the uIP TCP/IP or Rime stack should be used.
  */
 int tr1001_send(const void *packet, unsigned short len);
 
@@ -122,6 +114,9 @@ int tr1001_send(const void *packet, unsigned short len);
  * This function checks the receive buffer to see if an entire packet
  * has been received. The actual reception is handled by an interrupt
  * handler.
+ *
+ * This function should normally not be called from user
+ * programs. Rather, the uIP TCP/IP or Rime stack should be used.
  *
  * \return The length of the received packet, or 0 if no packet has
  * been received.
@@ -134,7 +129,7 @@ extern volatile unsigned char tr1001_rxstate;
 /**
  * Calculate the signal strength of a received packet.
  *
- * This function calculates the recevied signal strength of the last
+ * This function calculates the received signal strength of the last
  * received packet. This function typically is called when a packet
  * has been received.
  */
