61bf7cae0963a7f5da0054f8c5f0143f85cd0221
adamdunkels <adamdunkels>
2008-08-15 19:16:09 +0000

    Hardy Griech's timer bugfix: avoid direct comparison for timer values since they may wrap. Instead, use the CLOCK_LT() that works correctly even for wrapped timers.

--- core/sys/clock.h
+++ core/sys/clock.h
@@ -53,14 +53,23 @@
  *
  * Author: Adam Dunkels <adam@sics.se>
  *
- * $Id: clock.h,v 1.6 2008/07/10 17:09:32 adamdunkels Exp $
+ * $Id: clock.h,v 1.7 2008/08/15 19:16:09 adamdunkels Exp $
  */
 #ifndef __CLOCK_H__
 #define __CLOCK_H__
 
 #include "contiki-conf.h"
 
-#define CLOCK_LT(a,b)     ((signed short)((a)-(b)) < 0)
+/**
+ * Check if a clock time value is less than another clock time value.
+ *
+ * This macro checks if a clock time value is less than another clock
+ * time value. This macro is needed to correctly handle wrap-around of
+ * clock time values.
+ *
+ */
+#define CLOCK_LT(a, b) ((clock_time_t)((a) - (b)) < \
+                       ((clock_time_t)(~((clock_time_t)0)) >> 1))
 
 /**
  * Initialize the clock library.
