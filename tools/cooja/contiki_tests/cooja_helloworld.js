9438027d2cdbb10bb99f47de9cbc57f65c899860
fros4943 <fros4943>
2008-11-05 18:20:30 +0000

    using new testOK() method

--- tools/cooja/contiki_tests/cooja_helloworld.js
+++ tools/cooja/contiki_tests/cooja_helloworld.js
@@ -1,13 +1,5 @@
-/* Script is called once for every node log output. */
-/* Input variables: Mote mote, int id, String msg. */
-
-log.log('MOTE=' + mote + '\n');
-log.log('ID=' + id + '\n');
-log.log('TIME=' + mote.getSimulation().getSimulationTime() + '\n');
-log.log('MSG=' + msg + '\n');
-
 if (msg.startsWith('Hello, world')) {
-  log.log('TEST OK\n'); /* Report test success */
-  /* To increase test run speed, close the simulator when done */
-  mote.getSimulation().getGUI().doQuit(false); /* Quit simulator (to end test run)*/
+  log.testOK();
+} else {
+  log.testFailed();
 }
