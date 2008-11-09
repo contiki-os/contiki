ee68f4bfda70ab8aa1b443d9d8a3fc015fae3650
adamdunkels <adamdunkels>
2008-11-09 10:58:06 +0000

    Reduce the default number of pagewidgets to make file compile out-of-the-box on MSP430

--- apps/webbrowser/www.h
+++ apps/webbrowser/www.h
@@ -1,19 +1,19 @@
 /*
  * Copyright (c) 2002, Adam Dunkels.
- * All rights reserved. 
+ * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions 
- * are met: 
- * 1. Redistributions of source code must retain the above copyright 
- *    notice, this list of conditions and the following disclaimer. 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above
  *    copyright notice, this list of conditions and the following
  *    disclaimer in the documentation and/or other materials provided
- *    with the distribution. 
+ *    with the distribution.
  * 3. The name of the author may not be used to endorse or promote
  *    products derived from this software without specific prior
- *    written permission.  
+ *    written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -25,11 +25,11 @@
  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * This file is part of the Contiki desktop environment for the C64.
  *
- * $Id: www.h,v 1.3 2007/11/30 21:51:59 oliverschmidt Exp $
+ * $Id: www.h,v 1.4 2008/11/09 10:58:06 adamdunkels Exp $
  *
  */
 #ifndef __WWW_H__
@@ -48,7 +48,7 @@
 #define WWW_CONF_MAX_URLLEN 200
 #endif
 #ifndef WWW_CONF_MAX_NUMPAGEWIDGETS
-#define WWW_CONF_MAX_NUMPAGEWIDGETS 80
+#define WWW_CONF_MAX_NUMPAGEWIDGETS 20
 #endif
 #ifndef WWW_CONF_RENDERSTATE
 #define WWW_CONF_RENDERSTATE 1
