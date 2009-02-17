663a0b2c9b58dc24097f5d539af7234db71f43ef
fros4943 <fros4943>
2009-02-17 12:53:59 +0000

    increased test timeout and also comparing number of received notifications vs sent notifications

--- tools/cooja/contiki_tests/rime_runicast.js
+++ tools/cooja/contiki_tests/rime_runicast.js
@@ -1,4 +1,4 @@
-TIMEOUT(120000);
+TIMEOUT(240000);
 
 nr_recv = 0;
 nr_timedout = 0;
@@ -9,38 +9,43 @@ while (nr_sent < 10) {
 /* Count received packets */
 if (msg.contains('received')) {
   nr_recv++;
-  log.log("Received packets count now: " + nr_recv + "\n");
+  log.log("RECV=" + nr_recv + ", sent=" + nr_sent + ", timedout=" + nr_timedout + "\n");
 }
 
 /* Count timed out packets */
 else if (msg.contains('timed out')) {
   nr_timedout++;
-  log.log("Timed out packets count now: " + nr_timedout + "\n");
+  log.log("recv=" + nr_recv + ", sent=" + nr_sent + ", TIMEDOUT=" + nr_timedout + "\n");
 }
 
 /* Count sent packets */
 else if (msg.contains('sent to')) {
   nr_sent++;
-  log.log("Sent packets count now: " + nr_sent + "\n");
+  log.log("recv=" + nr_recv + ", SENT=" + nr_sent + ", timedout=" + nr_timedout + "\n");
 }
 
 YIELD();
 }
 
 /* Make sure received counter matches sent counter */
-if (nr_recv < nr_sent) {
-  log.log("Received < Sent: " + nr_recv + " < " + nr_sent + "\n");
-  log.log("Received packets less than acked sent packets!\n");
+if (nr_recv < nr_sent-3) {
+  log.log("Error: Received << Sent!\n");
+  log.log("recv=" + nr_recv + ", sent=" + nr_sent + ", timedout=" + nr_timedout + "\n");
+  log.testFailed();
+}
+
+if (nr_recv > nr_sent+3) {
+  log.log("Error: Received >> Sent!\n");
+  log.log("recv=" + nr_recv + ", sent=" + nr_sent + ", timedout=" + nr_timedout + "\n");
   log.testFailed();
 }
 
 /* Make sure some packets timed out (all from node 4) */
 if (nr_timedout < 2) {
-  log.log("Timed out: " + nr_timedout + "\n");
-  log.log("Too few packets timed out!\n");
+  log.log("Error: Too few packets timed out!\n");
+  log.log("recv=" + nr_recv + ", sent=" + nr_sent + ", timedout=" + nr_timedout + "\n");
   log.testFailed();
 }
 
-log.log("Received / Sent: " + nr_recv + " / " + nr_sent + "\n");
-log.log("Timed out: " + nr_timedout + "\n");
+log.log("recv=" + nr_recv + ", sent=" + nr_sent + ", timedout=" + nr_timedout + "\n");
 log.testOK();
