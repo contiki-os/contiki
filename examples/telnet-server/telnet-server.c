b4d6dd6893fcf30641191258325e995ed7b919b9
oliverschmidt <oliverschmidt>
2009-03-01 23:37:49 +0000

    Added webserver and irc.

--- examples/telnet-server/telnet-server.c
+++ examples/telnet-server/telnet-server.c
@@ -28,7 +28,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: telnet-server.c,v 1.3 2008/11/10 22:10:33 oliverschmidt Exp $
+ * $Id: telnet-server.c,v 1.4 2009/03/01 23:37:49 oliverschmidt Exp $
  */
 
 /**
@@ -49,11 +49,13 @@ PROCESS_THREAD(shell_init_process, ev, data)
 {
   PROCESS_BEGIN();
 
-  shell_file_init();   
-  shell_ps_init();   
-  shell_run_init();   
-  shell_text_init();   
-  shell_time_init();   
+  shell_file_init();
+  shell_httpd_init();
+  shell_irc_init();
+  shell_ps_init();
+  shell_run_init();
+  shell_text_init();
+  shell_time_init();
   shell_wget_init();
 
   PROCESS_END();
