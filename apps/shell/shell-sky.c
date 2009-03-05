a3daf1d3cc0b9a8370e077ccd20e2369a94a0def
adamdunkels <adamdunkels>
2009-03-05 21:12:02 +0000

    Refactored the power commands out from the sky-specific shell commands into its own module. Also added compower idle consumption in the energy and power reports.

--- apps/shell/shell-sky.c
+++ apps/shell/shell-sky.c
@@ -28,7 +28,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: shell-sky.c,v 1.12 2008/09/18 21:30:24 joxe Exp $
+ * $Id: shell-sky.c,v 1.13 2009/03/05 21:12:02 adamdunkels Exp $
  */
 
 /**
@@ -57,16 +57,6 @@
 #include <stdio.h>
 #include <string.h>
 
-struct power_msg {
-  uint16_t len;
-  uint32_t cpu;
-  uint32_t lpm;
-  uint32_t transmit;
-  uint32_t listen;
-};
-
-#define WITH_POWERGRAPH 0
-
 /*---------------------------------------------------------------------------*/
 PROCESS(shell_nodeid_process, "nodeid");
 SHELL_COMMAND(nodeid_command,
@@ -93,28 +83,6 @@ SHELL_COMMAND(rfchannel_command,
 	      "rfchannel",
 	      "rfchannel <channel>: change CC2420 radio channel (11 - 26)",
 	      &shell_rfchannel_process);
-PROCESS(shell_power_process, "power");
-SHELL_COMMAND(power_command,
-	      "power",
-	      "power: print power profile",
-	      &shell_power_process);
-PROCESS(shell_energy_process, "energy");
-SHELL_COMMAND(energy_command,
-	      "energy",
-	      "energy: print energy profile",
-	      &shell_energy_process);
-PROCESS(shell_powerconv_process, "powerconv");
-SHELL_COMMAND(powerconv_command,
-	      "powerconv",
-	      "powerconv: convert power profile to human readable output",
-	      &shell_powerconv_process);
-#if WITH_POWERGRAPH
-PROCESS(shell_powergraph_process, "powergraph");
-SHELL_COMMAND(powergraph_command,
-	      "powergraph",
-	      "powergraph: convert power profile to a 'graphical' repressentation",
-	      &shell_powergraph_process);
-#endif /* WITH_POWERGRAPH */
 /*---------------------------------------------------------------------------*/
 #define MAX(a, b) ((a) > (b)? (a): (b))
 #define MIN(a, b) ((a) < (b)? (a): (b))
@@ -280,164 +248,6 @@ PROCESS_THREAD(shell_rfchannel_process, ev, data)
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(shell_power_process, ev, data)
-{
-  static unsigned long last_cpu, last_lpm, last_transmit, last_listen;
-  struct power_msg msg;
-
-  PROCESS_BEGIN();
-
-  energest_flush();
-  
-  msg.len = 8;
-  msg.cpu = energest_type_time(ENERGEST_TYPE_CPU) - last_cpu;
-  msg.lpm = energest_type_time(ENERGEST_TYPE_LPM) - last_lpm;
-  msg.transmit = energest_type_time(ENERGEST_TYPE_TRANSMIT) - last_transmit;
-  msg.listen = energest_type_time(ENERGEST_TYPE_LISTEN) - last_listen;
-
-  last_cpu = energest_type_time(ENERGEST_TYPE_CPU);
-  last_lpm = energest_type_time(ENERGEST_TYPE_LPM);
-  last_transmit = energest_type_time(ENERGEST_TYPE_TRANSMIT);
-  last_listen = energest_type_time(ENERGEST_TYPE_LISTEN);
-
-  shell_output(&power_command, &msg, sizeof(msg), "", 0);
-
-  PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(shell_energy_process, ev, data)
-{
-  struct power_msg msg;
-
-  PROCESS_BEGIN();
-
-  energest_flush();
-  
-  msg.len = 8;
-  msg.cpu = energest_type_time(ENERGEST_TYPE_CPU);
-  msg.lpm = energest_type_time(ENERGEST_TYPE_LPM);
-  msg.transmit = energest_type_time(ENERGEST_TYPE_TRANSMIT);
-  msg.listen = energest_type_time(ENERGEST_TYPE_LISTEN);
-
-  shell_output(&energy_command, &msg, sizeof(msg), "", 0);
-
-  PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
-#define DEC2FIX(h,d) ((h * 64L) + (unsigned long)((d * 64L) / 1000L))
-static void
-printpower(struct power_msg *msg)
-{
-  char buf[50];
-  unsigned long avg_power;
-  unsigned long time;
-
-  time = msg->cpu + msg->lpm;
-  
-  avg_power = (3L *
-	       (msg->cpu       * DEC2FIX(1L,800L) +
-		msg->lpm       * DEC2FIX(0L,545L) +
-		msg->transmit  * DEC2FIX(17L,700L) +
-		msg->listen    * DEC2FIX(20L,0))) / ((64L * time) / 1000);
-  snprintf(buf, sizeof(buf), "CPU %d%% LPM %d%% tx %d%% rx %d%% tot %lu uW",
-	   (int)((100L * (unsigned long)msg->cpu) / time),
-	   (int)((100L * (unsigned long)msg->lpm) / time),
-	   (int)((100L * (unsigned long)msg->transmit) / time),
-	   (int)((100L * (unsigned long)msg->listen) / time),
-	   avg_power);
-  shell_output_str(&powerconv_command, buf, "");
-}
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(shell_powerconv_process, ev, data)
-{
-  struct power_msg *msg;
-  struct shell_input *input;
-  int len;
-
-  PROCESS_BEGIN();
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(ev == shell_event_input);
-    input = data;
-    
-    if(input->len1 + input->len2 == 0) {
-      PROCESS_EXIT();
-    }
-    len = input->len1;
-    for(msg = (struct power_msg *)input->data1;
-	len > 0;
-	msg++, len -= sizeof(struct power_msg)) {
-      printpower(msg);
-    }
-    len = input->len2;
-    for(msg = (struct power_msg *)input->data2;
-	len > 0;
-	msg++, len -= sizeof(struct power_msg)) {
-      printpower(msg);
-    }
-
-  }
-  
-  PROCESS_END();
-}
-/*---------------------------------------------------------------------------*/
-#if WITH_POWERGRAPH
-#define MAX_POWERGRAPH 34
-static void
-printpowergraph(struct power_msg *msg)
-{
-  int i;
-  unsigned long avg_power;
-  unsigned long time;
-  char buf[MAX_POWERGRAPH];
-
-  time = msg->cpu + msg->lpm;
-  
-  avg_power = (3L *
-	       (msg->cpu       * DEC2FIX(1L,800L) +
-		msg->lpm       * DEC2FIX(0L,545L) +
-		msg->transmit  * DEC2FIX(17L,700L) +
-		msg->listen    * DEC2FIX(20L,0))) / ((64L * time) / 1000);
-  memset(buf, 0, MAX_POWERGRAPH);
-  for(i = 0; avg_power > 0 && i < MAX_POWERGRAPH; ++i) {
-    buf[i] = '*';
-    avg_power -= MIN(2000, avg_power);
-  }
-  shell_output_str(&powergraph_command, buf, "");
-}
-/*---------------------------------------------------------------------------*/
-PROCESS_THREAD(shell_powergraph_process, ev, data)
-{
-  struct power_msg *msg;
-  struct shell_input *input;
-  int len;
-
-  PROCESS_BEGIN();
-  while(1) {
-    PROCESS_WAIT_EVENT_UNTIL(ev == shell_event_input);
-    input = data;
-    
-    if(input->len1 + input->len2 == 0) {
-      PROCESS_EXIT();
-    }
-    len = input->len1;
-    for(msg = (struct power_msg *)input->data1;
-	len > 0;
-	msg++, len -= sizeof(struct power_msg)) {
-      printpowergraph(msg);
-    }
-    len = input->len2;
-    for(msg = (struct power_msg *)input->data2;
-	len > 0;
-	msg++, len -= sizeof(struct power_msg)) {
-      printpowergraph(msg);
-    }
-
-  }
-  
-  PROCESS_END();
-}
-#endif /* WITH_POWERGRAPH */
-/*---------------------------------------------------------------------------*/
 PROCESS_THREAD(shell_nodeid_process, ev, data)
 {
   uint16_t nodeid;
@@ -471,17 +281,11 @@ PROCESS_THREAD(shell_nodeid_process, ev, data)
 void
 shell_sky_init(void)
 {
-  shell_register_command(&power_command);
-  shell_register_command(&powerconv_command);
-  shell_register_command(&energy_command);
   shell_register_command(&txpower_command);
   shell_register_command(&rfchannel_command);
   shell_register_command(&sense_command);
   shell_register_command(&senseconv_command);
   shell_register_command(&nodeid_command);
 
-#if WITH_POWERGRAPH
-  shell_register_command(&powergraph_command);
-#endif /* WITH_POWERGRAPH */
 }
 /*---------------------------------------------------------------------------*/
