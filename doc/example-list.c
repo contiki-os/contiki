9fd33a1211543852d4b82ff6a455d242505822fe
adamdunkels <adamdunkels>
2009-03-24 17:46:17 +0000

    Improved list example

--- doc/example-list.c
+++ doc/example-list.c
@@ -1,4 +1,4 @@
-#include "list.h"
+#include "lib/list.h"
 
 struct example_list_struct {
   struct *next;
@@ -7,21 +7,24 @@ struct example_list_struct {
 
 LIST(example_list);
 
+static struct example_list_struct element1, element2;
+
 void
 example_function(void)
 {
   struct example_list_struct *s;
-  struct example_list_struct element1, element2;
 
   list_init(example_list);
 
+  element1.number = 1;
   list_add(example_list, &element1);
+
+  element2.number = 2;
   list_add(example_list, &element2);
-  
+
   for(s = list_head(example_list);
       s != NULL;
       s = s->next) {
     printf("List element number %d\n", s->number);
   }
-  
 }
