e9ad329bb5e1ea5bc6c321a2ddd75e4650f3d699
fros4943 <fros4943>
2009-03-26 15:41:59 +0000

    make sure components are initialized at tick event

--- tools/cooja/java/se/sics/cooja/plugins/SimControl.java
+++ tools/cooja/java/se/sics/cooja/plugins/SimControl.java
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: SimControl.java,v 1.11 2009/03/09 15:39:33 fros4943 Exp $
+ * $Id: SimControl.java,v 1.12 2009/03/26 15:41:59 fros4943 Exp $
  */
 
 package se.sics.cooja.plugins;
@@ -122,8 +122,10 @@ public class SimControl extends VisPlugin {
               }
             }
 
-            sliderDelay.setValue(convertTimeToSlide(simulation.getDelayTime()));
-            simulationTime.setText("Current simulation time: " + simulation.getSimulationTime());
+            if (sliderDelay != null) {
+              sliderDelay.setValue(convertTimeToSlide(simulation.getDelayTime()));
+              simulationTime.setText("Current simulation time: " + simulation.getSimulationTime());
+            }
           }
         });
       }
