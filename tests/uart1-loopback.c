5fc1a9f2247677032ceaeb6786a68e497fb0216b
Mariano Alvira <mar@devl.org>
2009-03-30 16:11:08 -0400

    baudrate mystery was my bad code.

--- tests/uart1-loopback.c
+++ tests/uart1-loopback.c
@@ -17,8 +17,10 @@ void main(void) {
 
 	*(volatile uint32_t *)UART1_CON = 0x0000c800; /* mask interrupts, 16 bit sample --- helps explain the baud rate */
 
-	/* INC = 76; MOD = 1000 */
-	*(volatile uint32_t *)UART1_BR = 0x004C03E8; /* Baud rate: (INC<<16 || MOD) */ /* is 115200 @ 24 MHz --- unexplained */
+	/* INC = 767; MOD = 9999 works: 115200 @ 24 MHz 16 bit sample */
+	#define INC 767
+	#define MOD 9999
+	*(volatile uint32_t *)UART1_BR = INC<<16 | MOD; 
 
 	/* see Section 11.5.1.2 Alternate Modes */
 	/* you must enable the peripheral first BEFORE setting the function in GPIO_FUNC_SEL */
