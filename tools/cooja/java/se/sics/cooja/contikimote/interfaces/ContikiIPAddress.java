fd1cdfa36d8b09780735e12de0ee0b5d80af824a
fros4943 <fros4943>
2009-04-01 17:43:17 +0000

    update ip interface to supports ipv6 addresses.
    also disabled writing IP addresses from inside COOJA, since the these should rather be determined by Contiki

--- tools/cooja/java/se/sics/cooja/contikimote/interfaces/ContikiIPAddress.java
+++ tools/cooja/java/se/sics/cooja/contikimote/interfaces/ContikiIPAddress.java
@@ -26,16 +26,12 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: ContikiIPAddress.java,v 1.5 2009/02/25 14:46:24 fros4943 Exp $
+ * $Id: ContikiIPAddress.java,v 1.6 2009/04/01 17:43:18 fros4943 Exp $
  */
 
 package se.sics.cooja.contikimote.interfaces;
 
-import java.util.*;
-import javax.swing.*;
 import org.apache.log4j.Logger;
-import org.jdom.Element;
-
 import se.sics.cooja.*;
 import se.sics.cooja.contikimote.ContikiMoteInterface;
 import se.sics.cooja.interfaces.IPAddress;
@@ -43,135 +39,46 @@ import se.sics.cooja.interfaces.IPAddress;
 /**
  * uIP IP address.
  *
- * Contiki variables:
+ * Contiki variables (currently not activated):
  * <ul>
- * <li>char simIPa
- * <li>char simIPb
- * <li>char simIPc
- * <li>char simIPd
+ * <li>char simIP[4], or char simIP[16]
+ * <li>int simIPv4
+ * <li>int simIPv6
  * <li>char simIPChanged (1 if new IP should be set)
  * </ul>
  * <p>
- *
- * The new IP will be "simIPa.simIPb.simIPc.simIPd".
- * Note that this mote interface does not detect if Contiki changes IP address.
  *
  * Core interface:
  * <ul>
  * <li>ip_interface
  * </ul>
  *
- * This observable notifies when the IP address is set or altered.
+ * This observable notifies when the IP address is set.
+ * Note that this mote interface does not detect if Contiki changes IP address at run-time.
  *
  * @author Fredrik Osterlind
  */
 public class ContikiIPAddress extends IPAddress implements ContikiMoteInterface {
-  private SectionMoteMemory moteMem = null;
   private static Logger logger = Logger.getLogger(ContikiIPAddress.class);
 
   /**
    * Creates an interface to the IP address at mote.
    *
-   * @param mote
-   *          IP address' mote.
+   * @param mote IP address' mote.
    * @see Mote
    * @see se.sics.cooja.MoteInterfaceHandler
    */
-  public ContikiIPAddress(Mote mote) {
-    this.moteMem = (SectionMoteMemory) mote.getMemory();
+  public ContikiIPAddress(final Mote mote) {
+    super(mote);
   }
 
   public static String[] getCoreInterfaceDependencies() {
-    return new String[]{"ip_interface"};
-  }
-
-  public String getIPString() {
-    return
-    (int) moteMem.getByteValueOf("simIPa")
-    + "." +
-    (int) moteMem.getByteValueOf("simIPb")
-    + "." +
-    (int) moteMem.getByteValueOf("simIPc")
-    + "." +
-    (int) moteMem.getByteValueOf("simIPd");
-  }
-
-  public void setIPString(String ipAddress) {
-    String[] ipArray = ipAddress.split("\\.");
-    if (ipArray.length < 4) {
-      logger.warn("Could not set ip address (" + ipAddress + ")");
-    } else {
-      setIPNumber((char) Integer.parseInt(ipArray[0]), (char) Integer
-          .parseInt(ipArray[1]), (char) Integer.parseInt(ipArray[2]),
-          (char) Integer.parseInt(ipArray[3]));
-    }
-  }
-
-  public void setIPNumber(char a, char b, char c, char d) {
-    moteMem.setByteValueOf("simIPa", (byte) a);
-    moteMem.setByteValueOf("simIPb", (byte) b);
-    moteMem.setByteValueOf("simIPc", (byte) c);
-    moteMem.setByteValueOf("simIPd", (byte) d);
-    moteMem.setByteValueOf("simIPChanged", (byte) 1);
-
-    setChanged();
-    notifyObservers();
-  }
-
-  public JPanel getInterfaceVisualizer() {
-    JPanel panel = new JPanel();
-    final JLabel ipLabel = new JLabel();
-
-    ipLabel.setText("IPv4 address: " + getIPString());
-
-    panel.add(ipLabel);
-
-    Observer observer;
-    this.addObserver(observer = new Observer() {
-      public void update(Observable obs, Object obj) {
-        ipLabel.setText("IPv4 address: " + getIPString());
-      }
-    });
-
-    // Saving observer reference for releaseInterfaceVisualizer
-    panel.putClientProperty("intf_obs", observer);
-
-    return panel;
-  }
-
-  public void releaseInterfaceVisualizer(JPanel panel) {
-    Observer observer = (Observer) panel.getClientProperty("intf_obs");
-    if (observer == null) {
-      logger.fatal("Error when releasing panel, observer is null");
-      return;
-    }
-
-    this.deleteObserver(observer);
+    /*return new String[]{"ip_interface"};*/
+    return null;
   }
 
   public double energyConsumption() {
     // Virtual interface, does not require any energy
     return 0.0;
   }
-
-  public Collection<Element> getConfigXML() {
-    Vector<Element> config = new Vector<Element>();
-    Element element;
-
-    // Infinite boolean
-    element = new Element("ipv4address");
-    element.setText(getIPString());
-    config.add(element);
-
-    return config;
-  }
-
-  public void setConfigXML(Collection<Element> configXML, boolean visAvailable) {
-    for (Element element : configXML) {
-      if (element.getName().equals("ipv4address")) {
-        setIPString(element.getText());
-      }
-    }
-  }
-
 }
