e4858a2ade13704b10e9b490186600bd8ed60a3e
fros4943 <fros4943>
2009-04-01 17:43:17 +0000

    update ip interface to supports ipv6 addresses.
    also disabled writing IP addresses from inside COOJA, since the these should rather be determined by Contiki

--- tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/interfaces/MspIPAddress.java
+++ tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/interfaces/MspIPAddress.java
@@ -26,136 +26,23 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: MspIPAddress.java,v 1.3 2009/03/21 15:49:29 fros4943 Exp $
+ * $Id: MspIPAddress.java,v 1.4 2009/04/01 17:43:18 fros4943 Exp $
  */
 
 package se.sics.cooja.mspmote.interfaces;
 
-import java.util.Collection;
-import java.util.Observable;
-import java.util.Observer;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import org.apache.log4j.Logger;
-import org.jdom.Element;
-
 import se.sics.cooja.Mote;
-import se.sics.cooja.TimeEvent;
-import se.sics.cooja.AddressMemory.UnknownVariableException;
 import se.sics.cooja.interfaces.IPAddress;
-import se.sics.cooja.mspmote.MspMote;
-import se.sics.cooja.mspmote.MspMoteMemory;
 
 public class MspIPAddress extends IPAddress {
   private static Logger logger = Logger.getLogger(MspIPAddress.class);
 
-  private MspMote mote;
-  private MspMoteMemory moteMem = null;
-
   public MspIPAddress(Mote mote) {
-    this.mote = (MspMote) mote;
-    this.moteMem = (MspMoteMemory) mote.getMemory();
-
-    TimeEvent updateWhenAddressReady = new TimeEvent(0) {
-      public void execute(long t) {
-        if (getIPString().equals("0.0.0.0")) {
-          MspIPAddress.this.mote.getSimulation().scheduleEvent(
-              this,
-              MspIPAddress.this.mote.getSimulation().getSimulationTime() + 10);
-          return;
-        }
-        setChanged();
-        notifyObservers();
-      }
-    };
-    mote.getSimulation().scheduleEvent(
-        updateWhenAddressReady,
-        mote.getSimulation().getSimulationTime() + 10
-    );
-  }
-
-  public String getIPString() {
-    try {
-      byte[] addr = moteMem.getByteArray("uip_hostaddr", 4);
-      return
-      (addr[0]&0xFF) + "." +
-      (addr[1]&0xFF) + "." +
-      (addr[2]&0xFF) + "." +
-      (addr[3]&0xFF);
-    } catch (UnknownVariableException e) {
-      /*logger.warn("IP Address not available on mote: " + e.getMessage());*/
-    }
-    return "?.?.?.?";
-  }
-
-  public void setIPString(String ipAddress) {
-    String[] ipArray = ipAddress.split("\\.");
-    if (ipArray.length < 4) {
-      logger.warn("Could not set ip address (" + ipAddress + ")");
-    } else {
-      setIPNumber((char) Integer.parseInt(ipArray[0]), (char) Integer
-          .parseInt(ipArray[1]), (char) Integer.parseInt(ipArray[2]),
-          (char) Integer.parseInt(ipArray[3]));
-    }
-  }
-
-  public void setIPNumber(char a, char b, char c, char d) {
-    try {
-      byte[] addr = new byte[4];
-
-      addr[0] = (byte) a;
-      addr[1] = (byte) b;
-      addr[2] = (byte) c;
-      addr[3] = (byte) d;
-      moteMem.setByteArray("uip_hostaddr", addr);
-    } catch (UnknownVariableException e) {
-      logger.fatal("IP Address not available on mote: " + e.getMessage());
-    }
-
-    setChanged();
-    notifyObservers();
-  }
-
-  public JPanel getInterfaceVisualizer() {
-    JPanel panel = new JPanel();
-    final JLabel ipLabel = new JLabel();
-
-    ipLabel.setText("IPv4 address: " + getIPString());
-
-    panel.add(ipLabel);
-
-    Observer observer;
-    this.addObserver(observer = new Observer() {
-      public void update(Observable obs, Object obj) {
-        ipLabel.setText("IPv4 address: " + getIPString());
-      }
-    });
-
-    // Saving observer reference for releaseInterfaceVisualizer
-    panel.putClientProperty("intf_obs", observer);
-
-    return panel;
-  }
-
-  public void releaseInterfaceVisualizer(JPanel panel) {
-    Observer observer = (Observer) panel.getClientProperty("intf_obs");
-    if (observer == null) {
-      logger.fatal("Error when releasing panel, observer is null");
-      return;
-    }
-
-    this.deleteObserver(observer);
+    super(mote);
   }
 
   public double energyConsumption() {
     return 0;
   }
-
-  public Collection<Element> getConfigXML() {
-    return null;
-  }
-
-  public void setConfigXML(Collection<Element> configXML, boolean visAvailable) {
-  }
-
 }
--- tools/cooja/java/se/sics/cooja/interfaces/IPAddress.java
+++ tools/cooja/java/se/sics/cooja/interfaces/IPAddress.java
@@ -26,41 +26,236 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: IPAddress.java,v 1.2 2008/10/28 12:30:48 fros4943 Exp $
+ * $Id: IPAddress.java,v 1.3 2009/04/01 17:43:17 fros4943 Exp $
  */
 
 package se.sics.cooja.interfaces;
 
+import java.util.Collection;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Vector;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.apache.log4j.Logger;
+import org.jdom.Element;
+
 import se.sics.cooja.*;
 
 /**
- * IP Address represents a mote Internet address. An implementation should notify all
- * observers if the address is set or changed.
+ * IPv4 or IPv6 address.
  *
  * @author Fredrik Osterlind
  */
-@ClassDescription("IPv4 Address")
+@ClassDescription("IP Address")
 public abstract class IPAddress extends MoteInterface {
+  private static Logger logger = Logger.getLogger(IPAddress.class);
+  private AddressMemory moteMem;
+  private static boolean warnedNotImplemented = false;
+
+  public IPAddress(final Mote mote) {
+    moteMem = (AddressMemory) mote.getMemory();
+
+    /* Detect startup IP (only zeroes) */
+    TimeEvent updateWhenAddressReady = new TimeEvent(0) {
+      public void execute(long t) {
+        if (!isVersion4() && !isVersion6()) {
+          return;
+        }
+
+        String ipString = getIPString();
+        ipString = ipString.replace(".", "");
+        ipString = ipString.replace("0", "");
+        if (!ipString.isEmpty()) {
+          setChanged();
+          notifyObservers();
+          return;
+        }
+
+        /* Postpone until IP has been set */
+        mote.getSimulation().scheduleEvent(
+            this,
+            mote.getSimulation().getSimulationTime() + 1);
+        return;
+      }
+    };
+    updateWhenAddressReady.execute(0);
+  }
 
   /**
-   * Get current IP address on the form a.b.c.d.
+   * Returns IP address string.
+   * Supports both IPv4 and IPv6 addresses.
+   *
+   * @see #setIPString(String)
    * @return IP address string
    */
-  public abstract String getIPString();
+  public String getIPString() {
+    if (isVersion4()) {
+      String ipString = "";
+      byte[] ip = moteMem.getByteArray("uip_hostaddr", 4);
+      for (int i=0; i < 3; i++) {
+        ipString += (0xFF & ip[i]) + ".";
+      }
+      ipString += ip[3];
+      return ipString;
+    } else if (isVersion6()) {
+      String ipString = "";
+
+      /* XXX Assuming fixed offset in struct uip_netif (uip-netif.h) */
+      int offset =
+        4*4 /* 4 uint32_t */ + 2*moteMem.getIntegerLength() /* 2 uint8_t */;
+      byte[] tmp = moteMem.getByteArray(
+          "uip_netif_physical_if",
+          offset + 16);
+      byte[] ip = new byte[16];
+      System.arraycopy(tmp, offset, ip, 0, 16);
+
+      int i=0;
+      while (i < 14) {
+        int val = (0xFF&ip[i+1]) + ((0xFF&ip[i])<<8);
+        ipString += hex16ToString(val) + ".";
+        i+=2;
+      }
+      int val = (0xFF&ip[15]) + ((0xFF&ip[14])<<8);
+      ipString += hex16ToString(val);
+
+      return ipString;
+    }
+    return null;
+  }
 
   /**
-   * Change/Set IP address.
-   * @param ipAddress IP string on the form a.b.c.d
+   * @return True if mote has an IPv4 address
    */
-  public abstract void setIPString(String ipAddress);
+  public boolean isVersion4() {
+    return moteMem.variableExists("uip_hostaddr");
+  }
 
   /**
-   * Change/Set IP address.
-   * @param a First byte of IP address
-   * @param b Second byte of IP address
-   * @param c Third byte of IP address
-   * @param d Fourth byte of IP address
+   * @return True if mote has an IPv6 address
    */
-  public abstract void setIPNumber(char a, char b, char c, char d);
+  public boolean isVersion6() {
+    return moteMem.variableExists("uip_netif_physical_if");
+  }
 
+  /**
+   * Set IPv4 address: a.b.c.d.
+   *
+   * @param a First byte
+   * @param b Second byte
+   * @param c Third byte
+   * @param d Fourth byte
+   */
+  public void setIPv4Address(int a, int b, int c, int d) {
+    /* TODO Implement */
+    if (!warnedNotImplemented) {
+      warnedNotImplemented = true;
+      logger.warn("Changing IP addresses is no longer supported.");
+      logger.warn("The IP address is instead determined by Contiki, for example based on the MAC/Rime address, or the node ID.");
+    }
+  }
+
+  /**
+   * Set IPv6 address.
+   *
+   * @param address 16 element IP address array
+   */
+  public void setIPv6Address(int[] address) {
+    /* TODO Implement */
+    if (!warnedNotImplemented) {
+      warnedNotImplemented = true;
+      logger.warn("Changing IP addresses is no longer supported.");
+      logger.warn("The IP address is instead determined by Contiki, for example based on the MAC/Rime address, or the node ID.");
+    }
+  }
+
+  /**
+   * Set IP address.
+   * Supports both IPv4 and IPv6 addresses.
+   *
+   * @see #getIPString()
+   * @param ipString IP address string
+   */
+  public void setIPString(String ipString) {
+    /* TODO Implement */
+    if (!warnedNotImplemented) {
+      warnedNotImplemented = true;
+      logger.warn("Changing IP addresses is no longer supported.");
+      logger.warn("The IP address is instead determined by Contiki, for example based on the MAC/Rime address, or the node ID.");
+    }
+  }
+
+  public JPanel getInterfaceVisualizer() {
+    JPanel panel = new JPanel();
+    final JLabel ipLabel = new JLabel();
+
+    if (isVersion4()) {
+      ipLabel.setText("IPv4 address: " + getIPString());
+    } else if (isVersion6()) {
+      ipLabel.setText("IPv6 address: " + getIPString());
+    } else {
+      ipLabel.setText("Unknown IP");
+    }
+
+    panel.add(ipLabel);
+
+    Observer observer;
+    this.addObserver(observer = new Observer() {
+      public void update(Observable obs, Object obj) {
+        if (isVersion4()) {
+          ipLabel.setText("IPv4 address: " + getIPString());
+        } else if (isVersion6()) {
+          ipLabel.setText("IPv6 address: " + getIPString());
+        } else {
+          ipLabel.setText("Unknown IP");
+        }
+      }
+    });
+
+    panel.putClientProperty("intf_obs", observer);
+
+    return panel;
+  }
+
+  public void releaseInterfaceVisualizer(JPanel panel) {
+    Observer observer = (Observer) panel.getClientProperty("intf_obs");
+    if (observer == null) {
+      logger.fatal("Error when releasing panel, observer is null");
+      return;
+    }
+
+    this.deleteObserver(observer);
+  }
+
+
+  public Collection<Element> getConfigXML() {
+    Vector<Element> config = new Vector<Element>();
+    Element element;
+
+    // Infinite boolean
+    element = new Element("ip");
+    element.setText(getIPString());
+    config.add(element);
+
+    return config;
+  }
+
+  public void setConfigXML(Collection<Element> configXML, boolean visAvailable) {
+    for (Element element : configXML) {
+      if (element.getName().equals("ip")) {
+        setIPString(element.getText());
+      }
+    }
+  }
+
+  private static String hex16ToString(int data) {
+    String str16 = "0000000000000000";
+    String s = Integer.toString(data & 0xffff, 16);
+    if (s.length() < 4) {
+      s = str16.substring(0, 4 - s.length()) + s;
+    }
+    return s;
+  }
 }
