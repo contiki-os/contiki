438c940ed7328ea97db1753326fef51b203e9370
Mariano Alvira <mar@devl.org>
2009-04-15 17:32:00 -0400

    nvm read works now.

--- include/nvm.h
+++ include/nvm.h
@@ -35,6 +35,6 @@ typedef enum
 /* ROM code seems to be THUMB */
 /* need to be in a THUMB block before calling them */
 volatile nvmErr_t (*nvm_detect)(nvmInterface_t nvmInterface,nvmType_t* pNvmType) = 0x00006cb9;
-volatile nvmErr_t (*nvm_read)(nvmInterface_t nvmInterface , nvmType_t nvmType , void *pDest, uint32_t address, uint32_t numBytes);
-
+volatile nvmErr_t (*nvm_read)(nvmInterface_t nvmInterface , nvmType_t nvmType , void *pDest, uint32_t address, uint32_t numBytes) = 0x00006d69;
+volatile void(*nvm_setsvar)(uint32_t zero_for_awesome) = 0x00007085;
 #endif //NVM_H
--- tests/nvm-read.c
+++ tests/nvm-read.c
@@ -29,10 +29,13 @@ void put_hex32(uint32_t x);
 const uint8_t hex[16]={'0','1','2','3','4','5','6','7',
 		 '8','9','a','b','c','d','e','f'};
 
+#define NBYTES 128
 __attribute__ ((section ("startup")))
 void main(void) {
-	uint32_t type=0xdeadbeef;
+	nvmType_t type=0;
 	nvmErr_t err;
+	uint32_t buf[NBYTES/4];
+	uint32_t i;
 
 	*(volatile uint32_t *)GPIO_PAD_DIR0 = 0x00000100;
 	
@@ -62,12 +65,26 @@ void main(void) {
 	puts("Detecting internal nvm\n\r");
 
 	err = nvm_detect(gNvmInternalInterface_c, &type);
-
+		
 	puts("nvm_detect returned: 0x");
 	put_hex(err);
 	puts(" type is: 0x");
 	put_hex32(type);
 	puts("\n\r");
+
+	nvm_setsvar(0);
+
+	err = nvm_read(gNvmInternalInterface_c, type, (uint8_t *)buf, 0x1F000, NBYTES);
+	puts("nvm_read returned: 0x");
+	put_hex(err);
+	puts("\n\r");
+
+	for(i=0; i<NBYTES/4; i++) {
+		puts("0x");
+		put_hex32(buf[i]);
+		puts("\n\r");
+	}
+		
 
 	while(1) {continue;};
 }
