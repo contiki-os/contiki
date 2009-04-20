7d7f2348d901bfaf8ca3257e69f161b83461af55
nifi <nifi>
2009-04-20 14:11:01 +0000

    increased tooltip timeout to one minute

--- tools/cooja/java/se/sics/cooja/GUI.java
+++ tools/cooja/java/se/sics/cooja/GUI.java
@@ -24,7 +24,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * $Id: GUI.java,v 1.123 2009/03/24 15:47:10 fros4943 Exp $
+ * $Id: GUI.java,v 1.124 2009/04/20 14:11:01 nifi Exp $
  */
 
 package se.sics.cooja;
@@ -95,6 +95,7 @@ import javax.swing.JSeparator;
 import javax.swing.JTabbedPane;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
+import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
@@ -1004,6 +1005,8 @@ public class GUI extends Observable {
 
     JFrame.setDefaultLookAndFeelDecorated(true);
     JDialog.setDefaultLookAndFeelDecorated(true);
+
+    ToolTipManager.sharedInstance().setDismissDelay(60000);
 
     /* Nimbus */
     try {
