c0bb4cb81098d57d6ebf6b2c98002d91dc140c7b
Mariano Alvira <mar@devl.org>
2009-04-24 17:07:15 -0400

    slight timer cleanup

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#define reg32(x) (*(volatile uint32_t *)(x))
+#define reg16(x) (*(volatile uint16_t *)(x))
+
+#endif
--- tests/tmr.c
+++ tests/tmr.c
@@ -1,53 +1,12 @@
-/* Timer registers are all 16-bit wide with 16-bit access only */
-#define TMR_OFFSET      (0x20)
-#define TMR_BASE        (0x80007000)
-#define TMR0_BASE       (TMR_BASE)
-#define TMR1_BASE       (TMR_BASE + TMR_OFFSET*1)
-#define TMR2_BASE       (TMR_BASE + TMR_OFFSET*2)
-#define TMR3_BASE       (TMR_BASE + TMR_OFFSET*3)
-
-#define TMR_REGOFF_COMP1    (0x0)
-#define TMR_REGOFF_COMP2    (0x2)
-#define TMR_REGOFF_CAPT     (0x4)
-#define TMR_REGOFF_LOAD     (0x6)
-#define TMR_REGOFF_HOLD     (0x8)
-#define TMR_REGOFF_CNTR     (0xa)
-#define TMR_REGOFF_CTRL     (0xc)
-#define TMR_REGOFF_SCTRL    (0xe)
-#define TMR_REGOFF_CMPLD1   (0x10)
-#define TMR_REGOFF_CMPLD2   (0x12)
-#define TMR_REGOFF_CSCTRL   (0x14)
-#define TMR_REGOFF_ENBL     (0x1e)
-
-/* Timer 0 registers */
-#define TMR0_COMP1   (TMR0_BASE + TMR_REGOFF_COMP1)
-#define TMR0_COMP_UP TMR0_COMP1
-#define TMR0_COMP2   (TMR0_BASE + TMR_REGOFF_COMP2)
-#define TMR0_COMP_DOWN TMR0_COMP2
-#define TMR0_CAPT    (TMR0_BASE + TMR_REGOFF_CAPT)
-#define TMR0_LOAD    (TMR0_BASE + TMR_REGOFF_LOAD)
-#define TMR0_HOLD    (TMR0_BASE + TMR_REGOFF_HOLD)
-#define TMR0_CNTR    (TMR0_BASE + TMR_REGOFF_CTRL)
-#define TMR0_CTRL    (TMR0_BASE + TMR_REGOFF_CTRL)
-#define TMR0_SCTRL   (TMR0_BASE + TMR_REGOFF_SCTRL)
-#define TMR0_CMPLD1  (TMR0_BASE + TMR_REGOFF_CMPLD1)
-#define TMR0_CMPLD2  (TMR0_BASE + TMR_REGOFF_CMPLD2)
-#define TMR0_CSCTRL  (TMR0_BASE + TMR_REGOFF_CSCTRL)
-
-/* one enable register to rule them all */
-#define TMR_ENBL    TMR0_BASE + TMR_REGOFF_ENBL
-
 #define MBAR_GPIO       0x80000000
 #define GPIO_PAD_DIR0   0x80000000
 #define GPIO_DATA0      0x80000008
 #define UART1_DATA      0x80005008
 #define DELAY 400000
 
-#define reg32(x) (*(volatile uint32_t *)(x))
-#define reg16(x) (*(volatile uint16_t *)(x))
-
 #include "embedded_types.h"
-#include "isr.h"
+#include "utils.h"
+#include "timer.h"
 
 __attribute__ ((section ("startup")))
 void main(void) {
