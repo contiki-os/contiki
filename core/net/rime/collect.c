2a981045837cb0c62eac8691f290a46596448a07
adamdunkels <adamdunkels>
2009-04-30 20:21:06 +0000

    Fixes by Ward Van Heddeghem (reapplied from versioin 1.24)

--- core/net/rime/collect.c
+++ core/net/rime/collect.c
@@ -1,5 +1,5 @@
 /* XXX: send explicit congestion notification if already forwarding
-   packet + add queue for keeping packets to forward. */
+   packet. */
 
 /**
  * \addtogroup rimecollect
@@ -36,7 +36,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: collect.c,v 1.25 2009/04/29 20:48:57 adamdunkels Exp $
+ * $Id: collect.c,v 1.26 2009/04/30 20:21:06 adamdunkels Exp $
  */
 
 /**
@@ -410,7 +410,6 @@ collect_set_sink(struct collect_conn *tc, int should_be_sink)
   if(should_be_sink) {
     tc->rtmetric = SINK;
     /*    neighbor_discovery_start(&tc->neighbor_discovery_conn, tc->rtmetric);*/
-    announcement_set_value(&tc->announcement, tc->rtmetric);
   } else {
     tc->rtmetric = RTMETRIC_MAX;
   }
@@ -429,7 +428,7 @@ collect_send(struct collect_conn *tc, int rexmits)
   packetbuf_set_attr(PACKETBUF_ATTR_TTL, MAX_HOPLIM);
   packetbuf_set_attr(PACKETBUF_ATTR_MAX_REXMIT, rexmits);
 
-  if(tc->rtmetric == 0) {
+  if(tc->rtmetric == SINK) {
     packetbuf_set_attr(PACKETBUF_ATTR_HOPS, 0);
     if(tc->cb->recv != NULL) {
       tc->cb->recv(packetbuf_addr(PACKETBUF_ADDR_ESENDER),
