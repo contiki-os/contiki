ea829cadb0f3ad3a59ef68bd2b1b9dab8882617b
fros4943 <fros4943>
2009-05-18 14:48:10 +0000

    moved obsolete cooja projects to backyard

--- backyard/tools/cooja/apps/serial2pipe/java/se/sics/cooja/plugins/Serial2Pipe.java
+++ backyard/tools/cooja/apps/serial2pipe/java/se/sics/cooja/plugins/Serial2Pipe.java
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: Serial2Pipe.java,v 1.1 2007/02/26 13:28:14 fros4943 Exp $
+ * $Id: Serial2Pipe.java,v 1.1 2009/05/18 14:48:10 fros4943 Exp $
  */
 
 package se.sics.cooja.plugins;
