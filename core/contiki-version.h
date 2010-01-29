11823ad6d5df002e13b1cfa521ed0b1bb373c750
adamdunkels <adamdunkels>
2010-01-29 18:03:55 +0000

    Update to Contiki version number 2.4

--- core/contiki-version.h
+++ core/contiki-version.h
@@ -30,13 +30,13 @@
  *
  * Author: Adam Dunkels <adam@sics.se>
  *
- * $Id: contiki-version.h,v 1.8 2009/06/22 20:40:43 adamdunkels Exp $
+ * $Id: contiki-version.h,v 1.9 2010/01/29 18:04:54 adamdunkels Exp $
  */
 #ifndef __CONTIKI_VERSION__
 #define __CONTIKI_VERSION__
 
 #ifndef CONTIKI_VERSION_STRING
-#define CONTIKI_VERSION_STRING "Contiki 2.3"
+#define CONTIKI_VERSION_STRING "Contiki 2.4"
 #endif /* CONTIKI_VERSION_STRING */
 
 #endif /* __CONTIKI_VERSION__ */
