d8c32de4dbc28c744208c5d17da69baf5242fa82
fros4943 <fros4943>
2010-02-03 10:19:58 +0000

    minor fix: avoid too wide dialog by printing each project on a separate line

--- tools/cooja/java/se/sics/cooja/dialogs/ProjectDirectoriesDialog.java
+++ tools/cooja/java/se/sics/cooja/dialogs/ProjectDirectoriesDialog.java
@@ -26,7 +26,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: ProjectDirectoriesDialog.java,v 1.11 2010/01/15 10:09:23 adamdunkels Exp $
+ * $Id: ProjectDirectoriesDialog.java,v 1.12 2010/02/03 10:19:58 fros4943 Exp $
  */
 
 package se.sics.cooja.dialogs;
@@ -48,7 +48,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.Vector;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -153,9 +152,9 @@ public class ProjectDirectoriesDialog extends JDialog {
         newDefaultProjectDirs = newDefaultProjectDirs.replace('\\', '/');
 
         String question = "External tools setting DEFAULT_PROJECTDIRS will change from:\n"
-          + GUI.getExternalToolsSetting("DEFAULT_PROJECTDIRS")
-          + "\n to:\n"
-          + newDefaultProjectDirs;
+          + GUI.getExternalToolsSetting("DEFAULT_PROJECTDIRS", "").replace(';', '\n')
+          + "\n\n to:\n\n"
+          + newDefaultProjectDirs.replace(';', '\n');
         String title = "Change external tools settings?";
         int answer = JOptionPane.showOptionDialog(ProjectDirectoriesDialog.this, question, title,
             JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null,
