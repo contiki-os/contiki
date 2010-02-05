760a42aacce1ebaf73ed88b441a3e736e55726fb
ksb <ksb>
2010-02-05 14:47:42 +0000

    Generic STM32F103 platform

---
+++ platform/stm32test/contiki-conf.h
@@ -0,0 +1,44 @@
+#ifndef __CONTIKI_CONF_H__CDBB4VIH3I__
+#define __CONTIKI_CONF_H__CDBB4VIH3I__
+
+#include <stdint.h>
+
+#define CCIF
+#define CLIF
+
+#define WITH_UIP 1
+#define WITH_ASCII 1
+
+#define CLOCK_CONF_SECOND 100
+typedef uint8_t u8_t;
+typedef uint16_t u16_t;
+typedef uint32_t u32_t;
+typedef int8_t s8_t;
+typedef int16_t s16_t;
+typedef int32_t s32_t;
+
+typedef unsigned int clock_time_t;
+typedef unsigned int uip_stats_t;
+
+#ifndef BV
+#define BV(x) (1<<(x))
+#endif
+
+/* uIP configuration */
+#define UIP_CONF_LLH_LEN         0
+#define UIP_CONF_BROADCAST       1
+#define UIP_CONF_LOGGING 1
+#define UIP_CONF_BUFFER_SIZE 116
+
+#define UIP_CONF_TCP_FORWARD 1
+
+/* Prefix for relocation sections in ELF files */
+#define REL_SECT_PREFIX ".rel"
+
+#define CC_BYTE_ALIGNED __attribute__ ((packed, aligned(1)))
+
+#define USB_EP1_SIZE 64
+#define USB_EP2_SIZE 64
+
+#define RAND_MAX 0x7fff
+#endif /* __CONTIKI_CONF_H__CDBB4VIH3I__ */
---
+++ platform/stm32test/structgen_opts.gen.h
@@ -0,0 +1,2 @@
+#pragma target_endian little
+#pragma max_target_int_bytes 8
