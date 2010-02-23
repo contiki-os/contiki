5292005962bdd173e112bd4dd9e69260d43d5bd3
adamdunkels <adamdunkels>
2010-02-23 18:18:53 +0000

    A null radio driver that does not do anything - useful in the native port and as a template for new radio drivers

---
+++ core/dev/nullradio.c
@@ -0,0 +1,79 @@
+#include "dev/nullradio.h"
+
+
+/*---------------------------------------------------------------------------*/
+static int
+init(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+prepare(const void *payload, unsigned short payload_len)
+{
+  return 1;
+}
+/*---------------------------------------------------------------------------*/
+static int
+transmit(unsigned short transmit_len)
+{
+  return RADIO_TX_OK;
+}
+/*---------------------------------------------------------------------------*/
+static int
+send(const void *payload, unsigned short payload_len)
+{
+  prepare(payload, payload_len);
+  return transmit(payload_len);
+}
+/*---------------------------------------------------------------------------*/
+static int
+read(void *buf, unsigned short buf_len)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+channel_clear(void)
+{
+  return 1;
+}
+/*---------------------------------------------------------------------------*/
+static int
+receiving_packet(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+pending_packet(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+on(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+off(void)
+{
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+const struct radio_driver nullradio_driver =
+  {
+    init,
+    prepare,
+    transmit,
+    send,
+    read,
+    channel_clear,
+    receiving_packet,
+    pending_packet,
+    on,
+    off,
+  };
+/*---------------------------------------------------------------------------*/
