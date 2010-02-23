eeced404a95a296497b807c7b2eb6d87797b0ea1
adamdunkels <adamdunkels>
2010-02-23 18:32:44 +0000

    Added the option to specify the old value to observers so that they can determine if the announcement should be sent out quicklier than otherwise

--- core/net/rime/polite-announcement.c
+++ core/net/rime/polite-announcement.c
@@ -33,7 +33,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: polite-announcement.c,v 1.10 2010/01/26 10:18:55 adamdunkels Exp $
+ * $Id: polite-announcement.c,v 1.11 2010/02/23 18:32:44 adamdunkels Exp $
  */
 
 /**
@@ -158,10 +158,12 @@ send_timer(void *ptr)
 }
 /*---------------------------------------------------------------------------*/
 static void
-new_announcement(uint16_t id, uint16_t val)
+new_announcement(uint16_t id, uint16_t newval, uint16_t oldval)
 {
-  c.interval = c.min_interval;
-  send_timer(&c);
+  if(newval != oldval) {
+    c.interval = c.min_interval;
+    send_timer(&c);
+  }
 }
 /*---------------------------------------------------------------------------*/
 static const struct ipolite_callbacks ipolite_callbacks =
