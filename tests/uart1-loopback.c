637c9ddab2107edd056772c5c2f01e0287072442
Mariano Alvira <mar@devl.org>
2010-02-24 14:45:45 -0500

    cleaned up uart1-loopback

--- tests/uart1-loopback.c
+++ tests/uart1-loopback.c
@@ -1,44 +1,34 @@
-#define GPIO_FUNC_SEL0  0x80000018 /* GPIO 15 - 0;  2 bit blocks */
+#include <mc1322x.h>
+#include <board.h>
+#include <uart1.h>
 
-#define BASE_UART1      0x80005000
-#define UART1_CON       0x80005000
-#define UART1_STAT      0x80005004
-#define UART1_DATA      0x80005008
-#define UR1CON          0x8000500c
-#define UT1CON          0x80005010
-#define UART1_CTS       0x80005014
-#define UART1_BR        0x80005018
+/* INC = 767; MOD = 9999 works: 115200 @ 24 MHz 16 bit sample */
+#define INC 767
+#define MOD 9999
 
-#include "embedded_types.h"
-#include "isr.h"
-
-__attribute__ ((section ("startup")))
 void main(void) {
-	/* Restore UART regs. to default */
-	/* in case there is still bootloader state leftover */
 
-	*(volatile uint32_t *)UART1_CON = 0x0000c800; /* mask interrupts, 16 bit sample --- helps explain the baud rate */
-
-	/* INC = 767; MOD = 9999 works: 115200 @ 24 MHz 16 bit sample */
-	#define INC 767
-	#define MOD 9999
-	*(volatile uint32_t *)UART1_BR = INC<<16 | MOD; 
-
+        /* mask interrupts, 16 bit sample --- helps explain the baud rate */
+	*UART1_CON = 0x0000c800; 
+	
+	
+	*UART1_BR = INC<<16 | MOD; 
+	
 	/* see Section 11.5.1.2 Alternate Modes */
 	/* you must enable the peripheral first BEFORE setting the function in GPIO_FUNC_SEL */
 	/* From the datasheet: "The peripheral function will control operation of the pad IF */
 	/* THE PERIPHERAL IS ENABLED. */
-	*(volatile uint32_t *)UART1_CON = 0x00000003; /* enable receive and transmit */
-	*(volatile uint32_t *)GPIO_FUNC_SEL0 = ( (0x01 << (14*2)) | (0x01 << (15*2)) ); /* set GPIO15-14 to UART (UART1 TX and RX)*/
-
+	*UART1_CON = 0x00000003; /* enable receive and transmit */
+	*GPIO_FUNC_SEL0 = ( (0x01 << (14*2)) | (0x01 << (15*2)) ); /* set GPIO15-14 to UART (UART1 TX and RX)*/
+	
 	uint8_t c;
 	while(1) {
-//		*(volatile uint32_t *)UART1_DATA = (uint8_t)'U';
-		if(*(volatile uint32_t*)UR1CON > 0) {
+		if(*UART1_CON > 0) {
 			/* Receive buffer isn't empty */
 			/* read a byte and write it to the transmit buffer */
-			c = *(volatile uint32_t *)UART1_DATA;
-			*(volatile uint32_t *)UART1_DATA = c;
+			c = *UART1_DATA;
+			*UART1_DATA = c;
 		}
-	};
+	}
+	
 }
