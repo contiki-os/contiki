59dca22769b2bcd73dbfdae48db6a3c08f97d2db
Mariano Alvira <mar@devl.org>
2010-02-26 07:30:53 -0500

    clean up default_vreg_init

--- board/lowlevel.c
+++ board/lowlevel.c
@@ -1,9 +1,10 @@
-/* has been tested and it good */
+#include <crm.h>
+
 void default_vreg_init(void) {
 	volatile uint32_t i;
-	*(volatile uint32_t *)(0x80003000) = 0x00000018; /* set default state */
-	*(volatile uint32_t *)(0x80003048) = 0x00000f04; /* bypass the buck */
+	*CRM_SYS_CNTL = 0x00000018; /* set default state */
+	*CRM_VREG_CNTL = 0x00000f04; /* bypass the buck */
 	for(i=0; i<0x161a8; i++) { continue; } /* wait for the bypass to take */
 //	while((((*(volatile uint32_t *)(0x80003018))>>17) & 1) !=1) { continue; } /* wait for the bypass to take */
-	*(volatile uint32_t *)(0x80003048) = 0x00000ff8; /* start the regulators */
+	*CRM_VREG_CNTL = 0x00000ff8; /* start the regulators */
 }
