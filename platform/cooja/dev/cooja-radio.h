07753fb01f6bd0ebad0984282e4f34760a1bf871
fros4943 <fros4943>
2010-03-09 08:11:05 +0000

    updated radio driver, simplified code

--- platform/cooja/dev/cooja-radio.h
+++ platform/cooja/dev/cooja-radio.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Swedish Institute of Computer Science.
+ * Copyright (c) 2010, Swedish Institute of Computer Science.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,45 +28,16 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: cooja-radio.h,v 1.5 2009/04/01 13:44:34 fros4943 Exp $
+ * $Id: cooja-radio.h,v 1.6 2010/03/09 08:11:05 fros4943 Exp $
  */
 
 #ifndef __COOJA_RADIO_H__
 #define __COOJA_RADIO_H__
 
 #include "contiki.h"
-#include "contiki-conf.h"
-
 #include "dev/radio.h"
 
-#include "net/uip.h"
-#include "net/uip-fw.h"
-
-#define COOJA_RADIO_BUFSIZE UIP_BUFSIZE
-#define COOJA_RADIO_DROPPED UIP_FW_DROPPED
-#define COOJA_RADIO_TOOLARGE UIP_FW_TOOLARGE
-#define COOJA_RADIO_ZEROLEN UIP_FW_ZEROLEN
-#define COOJA_RADIO_OK UIP_FW_OK
-
-extern const struct radio_driver cooja_radio;
-
-/**
- * Turn radio hardware on.
- */
-int
-radio_on(void);
-
-/**
- * Turn radio hardware off.
- */
-int
-radio_off(void);
-
-/**
- * Set radio receiver.
- */
-void
-radio_set_receiver(void (* recv)(const struct radio_driver *));
+extern const struct radio_driver cooja_radio_driver;
 
 /**
  * Set radio channel.
@@ -77,45 +48,21 @@ radio_set_channel(int channel);
 /**
  * Set transmission power of transceiver.
  *
- * \param p The power of the tranceiver, between 1 (lowest) and 100
- * (highest).
+ * \param p The power of the transceiver, between 1 and 100.
  */
 void
 radio_set_txpower(unsigned char p);
 
 /**
- * Send a packet from the given buffer with the given length.
+ * The signal strength of the last received packet
  */
 int
-radio_send(const void *payload, unsigned short payload_len);
+radio_signal_strength_last(void);
 
 /**
- * Check if an incoming packet has been received.
- *
- * This function checks the receive buffer to see if an entire packet
- * has been received.
- *
- * \return The length of the received packet, or 0 if no packet has
- * been received.
+ * This current signal strength.
  */
 int
-radio_read(void *buf, unsigned short bufsize);
-
-/**
- * This function returns the signal strength of the last
- * received packet. This function typically is called when a packet
- * has been received.
- */
-int radio_sstrength(void);
-
-/**
- * This function measures and returns the signal strength.
- */
-int radio_current_sstrength(void);
-
-/**
- * Internal COOJA function, calls the current receiver function.
- */
-void radio_call_receiver(void);
+radio_signal_strength_current(void);
 
 #endif /* __COOJA_RADIO_H__ */
