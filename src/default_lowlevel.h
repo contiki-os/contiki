334c316c0a0dd7fc0409ad018d9feb1d9812b981
Mariano Alvira <mar@devl.org>
2010-03-16 10:40:25 -0400

    make xtal trimming part of the lowlevel init routines.

--- src/default_lowlevel.h
+++ src/default_lowlevel.h
@@ -1,8 +1,8 @@
 #ifndef LOWLEVEL_H
 #define LOWLEVEL_H
 
+#define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS);
 void default_vreg_init(void);
-
 void uart1_init(uint16_t inc, uint16_t mod, uint8_t samp);
 
 #endif
