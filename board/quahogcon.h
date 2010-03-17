7d02575d16c4fcb0d238a7502bd93767dbc2e6b7
Mariano Alvira <mar@devl.org>
2010-03-16 22:06:06 -0400

    add new board for the QuahogCon badge

---
+++ board/quahogcon.h
@@ -0,0 +1,22 @@
+#ifndef BOARD_REDBEE_DEV_H
+#define BOARD_REDBEE_DEV_H
+
+#define LED_RED   12
+#define LED_GREEN 21
+#define LED_BLUE  35
+
+/* XTAL TUNE parameters */
+/* see http://devl.org/pipermail/mc1322x/2009-December/000162.html */
+/* for details about how to make this measurment */
+
+/* Econotag also needs an addtional 12pf on board */
+/* Coarse tune: add 4pf */
+#define CTUNE_4PF 1
+/* Coarse tune: add 0-15 pf (CTUNE is 4 bits) */
+#define CTUNE 11
+/* Fine tune: add FTUNE * 156fF (FTUNE is 5bits) */
+#define FTUNE 7
+
+#include <std_conf.h>
+
+#endif
