fd3b773eb45f1c0e9bc8f7b23bb99a6166e61100
joxe <joxe>
2010-04-17 21:22:39 +0000

    updated for new uIPv6 APIs

--- examples/udp-ipv6/udp-server.c
+++ examples/udp-ipv6/udp-server.c
@@ -81,13 +81,14 @@ static void
 print_local_addresses(void)
 {
   int i;
-  uip_netif_state state;
+  uint8_t state;
 
   PRINTF("Server IPv6 addresses: ");
   for(i = 0; i < UIP_CONF_NETIF_MAX_ADDRESSES; i++) {
-    state = uip_netif_physical_if.addresses[i].state;
-    if(state == TENTATIVE || state == PREFERRED) {
-      PRINT6ADDR(&uip_netif_physical_if.addresses[i].ipaddr);
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
       PRINTF("\n");
     }
   }
@@ -102,7 +103,8 @@ PROCESS_THREAD(udp_server_process, ev, data)
 
 #if UIP_CONF_ROUTER
   uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
-  uip_netif_addr_add(&ipaddr, 64, 0, AUTOCONF);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
 #endif /* UIP_CONF_ROUTER */
 
   print_local_addresses();
