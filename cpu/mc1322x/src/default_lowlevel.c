423bb460e2a68b4ff1a3d97ae061dd29a8aa62e8
maralvira <maralvira>
2010-06-10 14:49:31 +0000

    initial commit of src files

---
+++ cpu/mc1322x/src/default_lowlevel.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2010, Mariano Alvira <mar@devl.org> and other contributors
+ * to the MC1322x project (http://mc1322x.devl.org)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of libmc1322x: see http://mc1322x.devl.org
+ * for details. 
+ *
+ * $Id: default_lowlevel.c,v 1.1 2010/06/10 14:49:31 maralvira Exp $
+ */
+
+#include <mc1322x.h>
+#include <types.h>
+
+void default_vreg_init(void) {
+	volatile uint32_t i;
+	*CRM_SYS_CNTL = 0x00000018; /* set default state */
+	*CRM_VREG_CNTL = 0x00000f04; /* bypass the buck */
+	for(i=0; i<0x161a8; i++) { continue; } /* wait for the bypass to take */
+//	while((((*(volatile uint32_t *)(0x80003018))>>17) & 1) !=1) { continue; } /* wait for the bypass to take */
+	*CRM_VREG_CNTL = 0x00000ff8; /* start the regulators */
+}
+
+void uart1_init(uint16_t inc, uint16_t mod, uint8_t samp) {
+		
+        /* UART must be disabled to set the baudrate */
+	*UART1_UCON = 0;
+	*UART1_UBRCNT = ( inc << 16 ) | mod; 
+
+	/* see Section 11.5.1.2 Alternate Modes */
+	/* you must enable the peripheral first BEFORE setting the function in GPIO_FUNC_SEL */
+	/* From the datasheet: "The peripheral function will control operation of the pad IF */
+	/* THE PERIPHERAL IS ENABLED. */
+	*UART1_UCON = (1 << 0) | (1 << 1); /* enable receive, transmit */
+	if(samp == UCON_SAMP_16X) 
+		set_bit(*UART1_UCON,UCON_SAMP);
+	*GPIO_FUNC_SEL0 = ( (0x01 << (14*2)) | (0x01 << (15*2)) ); /* set GPIO15-14 to UART (UART1 TX and RX)*/
+       
+	/* interrupt when there are this number or more bytes free in the TX buffer*/
+	*UART1_UTXCON = 16;
+
+	u1_head = 0; u1_tail = 0;
+
+	/* tx and rx interrupts are enabled in the UART by default */
+	/* see status register bits 13 and 14 */
+	/* enable UART1 interrupts in the interrupt controller */
+	enable_irq(UART1);
+}
