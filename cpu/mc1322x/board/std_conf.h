bf9f960bbe323ae227ce342280cd92770a48ddc4
maralvira <maralvira>
2010-06-10 14:48:32 +0000

    add board files.

---
+++ cpu/mc1322x/board/std_conf.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2010, Mariano Alvira <mar@devl.org> and other contributors
+ * to the MC1322x project (http://mc1322x.devl.org)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of libmc1322x: see http://mc1322x.devl.org
+ * for details. 
+ *
+ * $Id: std_conf.h,v 1.1 2010/06/10 14:48:32 maralvira Exp $
+ */
+
+#ifndef STDCONF_H
+#define STDCONF_H
+
+#include <default_lowlevel.h>
+
+#ifndef vreg_init
+#define vreg_init() default_vreg_init()
+#endif
+
+/* XTAL TUNE parameters */
+/* recommended defaults from the datasheet */
+
+/* Coarse tune: add 4pf */
+#ifndef CTUNE_4PF
+#define CTUNE_4PF 1
+#endif
+/* Coarse tune: add 0-7 pf */
+#ifndef CTUNE
+#define CTUNE 5
+#endif
+/* Fine tune: add FTUNE * 156fF (FTUNE is 4bits) */
+#ifndef FTUNE
+#define FTUNE 16
+#endif
+/* datasheet recommends taht you don't change this */
+#ifndef IBIAS
+#define IBIAS 0x1F
+#endif
+
+#endif
