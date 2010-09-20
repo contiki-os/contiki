59cf67fb89d51c922871f165d7c7c665bd3b5b57
kasunch <kasunch>
2010-09-20 17:56:14 +0000

    1. Added uIPv6 configuration options. 2. Fine tuned some configuration options to save RAM.

--- platform/micaz/contiki-conf.h
+++ platform/micaz/contiki-conf.h
@@ -42,18 +42,120 @@
 #ifndef __CONTIKI_CONF_H__
 #define __CONTIKI_CONF_H__
 
-#define NETSTACK_CONF_NETWORK rime_driver
-#define NETSTACK_CONF_MAC     csma_driver
-#define NETSTACK_CONF_RDC     cxmac_driver
-
-#define MAC_CONF_CHANNEL_CHECK_RATE 8
-
-#define RF_CHANNEL 26
-
 #define HAVE_STDINT_H
 #include "avrdef.h"
 
 #include "platform-conf.h"
+
+#if WITH_UIP6
+
+/* Network setup for IPv6 */
+#define NETSTACK_CONF_NETWORK sicslowpan_driver
+#define NETSTACK_CONF_MAC     csma_driver
+#define NETSTACK_CONF_RDC     nullrdc_driver
+#define NETSTACK_CONF_FRAMER  framer_802154
+
+#define CC2420_CONF_AUTOACK              1
+#define MAC_CONF_CHANNEL_CHECK_RATE      8
+#define RIME_CONF_NO_POLITE_ANNOUCEMENTS 0
+#define CXMAC_CONF_ANNOUNCEMENTS         0
+
+#else /* WITH_UIP6 */
+
+/* Network setup for non-IPv6 (rime). */
+
+#define NETSTACK_CONF_NETWORK rime_driver
+#define NETSTACK_CONF_MAC     csma_driver
+#define NETSTACK_CONF_RDC     cxmac_driver
+#define NETSTACK_CONF_FRAMER  framer_802154
+
+#define CC2420_CONF_AUTOACK              1
+#define MAC_CONF_CHANNEL_CHECK_RATE      8
+
+#define COLLECT_CONF_ANNOUNCEMENTS       1
+#define RIME_CONF_NO_POLITE_ANNOUCEMENTS 1
+#define CXMAC_CONF_ANNOUNCEMENTS         0
+#define CXMAC_CONF_COMPOWER              1
+#define CONTIKIMAC_CONF_ANNOUNCEMENTS    0
+#define CONTIKIMAC_CONF_COMPOWER         1
+
+#define COLLECT_NEIGHBOR_CONF_MAX_NEIGHBORS      32
+
+#endif /* WITH_UIP6 */
+
+#define PACKETBUF_CONF_ATTRS_INLINE 1
+
+#ifndef RF_CHANNEL
+#define RF_CHANNEL              26
+#endif /* RF_CHANNEL */
+
+#define CONTIKIMAC_CONF_BROADCAST_RATE_LIMIT 0
+
+#define IEEE802154_CONF_PANID       0xABCD
+
+
+#define AODV_COMPLIANCE
+#define AODV_NUM_RT_ENTRIES 32
+
+#define WITH_ASCII 1
+
+#define PROCESS_CONF_NUMEVENTS 8
+#define PROCESS_CONF_STATS 1
+
+#ifdef WITH_UIP6
+
+#define RIMEADDR_CONF_SIZE              8
+
+#define UIP_CONF_LL_802154              1
+#define UIP_CONF_LLH_LEN                0
+
+#define UIP_CONF_ROUTER                 1
+#define UIP_CONF_IPV6_RPL               1
+
+/* configure number of neighbors and routes */
+#define UIP_CONF_DS6_NBR_NBU     5
+#define UIP_CONF_DS6_ROUTE_NBU   5
+
+#define RPL_CONF_MAX_PARENTS         4
+#define NEIGHBOR_CONF_MAX_NEIGHBORS  8
+
+#define UIP_CONF_ND6_SEND_RA		0
+#define UIP_CONF_ND6_REACHABLE_TIME     600000
+#define UIP_CONF_ND6_RETRANS_TIMER      10000
+
+#define UIP_CONF_IPV6                   1
+#define UIP_CONF_IPV6_QUEUE_PKT         0
+#define UIP_CONF_IPV6_CHECKS            1
+#define UIP_CONF_IPV6_REASSEMBLY        0
+#define UIP_CONF_NETIF_MAX_ADDRESSES    3
+#define UIP_CONF_ND6_MAX_PREFIXES       3
+#define UIP_CONF_ND6_MAX_NEIGHBORS      4
+#define UIP_CONF_ND6_MAX_DEFROUTERS     2
+#define UIP_CONF_IP_FORWARD             0
+#define UIP_CONF_BUFFER_SIZE		    240
+
+#define SICSLOWPAN_CONF_COMPRESSION_IPV6        0
+#define SICSLOWPAN_CONF_COMPRESSION_HC1         1
+#define SICSLOWPAN_CONF_COMPRESSION_HC01        2
+#define SICSLOWPAN_CONF_COMPRESSION             SICSLOWPAN_COMPRESSION_HC06
+#ifndef SICSLOWPAN_CONF_FRAG
+#define SICSLOWPAN_CONF_FRAG                    1
+#define SICSLOWPAN_CONF_MAXAGE                  8
+#endif /* SICSLOWPAN_CONF_FRAG */
+#define SICSLOWPAN_CONF_CONVENTIONAL_MAC	1
+#define SICSLOWPAN_CONF_MAX_ADDR_CONTEXTS       2
+#else /* WITH_UIP6 */
+#define UIP_CONF_IP_FORWARD      1
+#define UIP_CONF_BUFFER_SIZE     128
+#endif /* WITH_UIP6 */
+
+#define UIP_CONF_ICMP_DEST_UNREACH 1
+
+#if !WITH_UIP && !WITH_UIP6
+#define QUEUEBUF_CONF_NUM          8
+#else
+#define QUEUEBUF_CONF_NUM          4
+#endif
 
 #define TIMESYNCH_CONF_ENABLED 1
 #define CC2420_CONF_TIMESTAMPS 1
@@ -64,46 +166,22 @@
 #define CCIF
 #define CLIF
 
-
-#ifdef WITH_UIP6
-
-#define RIMEADDR_CONF_SIZE              8
-
-#define UIP_CONF_LL_802154              1
-#define UIP_CONF_LLH_LEN                0
-
-#define UIP_CONF_IPV6                   1
-#define UIP_CONF_IPV6_QUEUE_PKT         1
-#define UIP_CONF_IPV6_CHECKS            1
-#define UIP_CONF_IPV6_REASSEMBLY        0
-#define UIP_CONF_NETIF_MAX_ADDRESSES    3
-#define UIP_CONF_ND6_MAX_PREFIXES       3
-#define UIP_CONF_ND6_MAX_NEIGHBORS      4
-#define UIP_CONF_ND6_MAX_DEFROUTERS     2
-#define UIP_CONF_IP_FORWARD             0
-
-#define SICSLOWPAN_CONF_COMPRESSION_IPV6        0
-#define SICSLOWPAN_CONF_COMPRESSION_HC1         1
-#define SICSLOWPAN_CONF_COMPRESSION_HC01        2
-#define SICSLOWPAN_CONF_COMPRESSION             SICS_LOWPAN_CONF_COMPRESSION_HC1
-#define SICSLOWPAN_CONF_FRAG                    0
-#define SICSLOWPAN_CONF_CONVENTIONAL_MAC	1
-#else
-#define UIP_CONF_IP_FORWARD      1
-#endif /* WITH_UIP6 */
+/* The process names are not used to save RAM */
+#define PROCESS_CONF_NO_PROCESS_NAMES 1
 
 #define UIP_CONF_ICMP_DEST_UNREACH 1
 
 #define UIP_CONF_DHCP_LIGHT
 #define UIP_CONF_LLH_LEN         0
-#define UIP_CONF_BUFFER_SIZE     110
-#define UIP_CONF_RECEIVE_WINDOW  (UIP_CONF_BUFFER_SIZE - 40)
+#define UIP_CONF_RECEIVE_WINDOW  48
+#define UIP_CONF_TCP_MSS         48
 #define UIP_CONF_MAX_CONNECTIONS 4
 #define UIP_CONF_MAX_LISTENPORTS 8
 #define UIP_CONF_UDP_CONNS       12
-#define UIP_CONF_FWCACHE_SIZE    30
+#define UIP_CONF_FWCACHE_SIZE    15
 #define UIP_CONF_BROADCAST       1
-#define UIP_ARCH_IPCHKSUM        1
+//#define UIP_ARCH_IPCHKSUM        1
+#define UIP_CONF_UDP             1
 #define UIP_CONF_UDP_CHECKSUMS   1
 #define UIP_CONF_PINGADDRCONF    0
 #define UIP_CONF_LOGGING         0
