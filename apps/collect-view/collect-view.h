f82279c299fde76f3bd2d85a620873f92f2b8e19
adamdunkels <adamdunkels>
2010-10-12 11:37:41 +0000

    Made the collect-view application that was preivously hidden inside examples/sky-shell into its own application. This makes it possible for other platforms to use the same nica Java gui from the sky-shell application.

---
+++ apps/collect-view/collect-view.h
@@ -0,0 +1,28 @@
+#ifndef COLLECT_VIEW_H
+#define COLLECT_VIEW_H
+
+#include "contiki-conf.h"
+#include "net/rime/rimeaddr.h"
+
+struct collect_view_data_msg {
+  uint16_t len;
+  uint16_t clock;
+  uint16_t timesynch_time;
+  uint16_t cpu;
+  uint16_t lpm;
+  uint16_t transmit;
+  uint16_t listen;
+  rimeaddr_t parent;
+  uint16_t parent_etx;
+  uint16_t parent_rtmetric;
+  uint16_t num_neighbors;
+  uint16_t beacon_interval;
+
+  uint16_t sensors[10];
+};
+
+void collect_view_init(void);
+
+void collect_view_arch_read_sensors(struct collect_view_data_msg *msg);
+
+#endif /* COLLECT_VIEW_H */
