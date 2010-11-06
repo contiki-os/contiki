3f5a3a9b86989c1f3271421c744e2e5d9ba225c5
adamdunkels <adamdunkels>
2010-11-06 14:43:45 +0000

    Removed left-over powertrace include

--- examples/rime/example-collect.c
+++ examples/rime/example-collect.c
@@ -28,7 +28,7 @@
  *
  * This file is part of the Contiki operating system.
  *
- * $Id: example-collect.c,v 1.13 2010/11/06 10:44:06 adamdunkels Exp $
+ * $Id: example-collect.c,v 1.14 2010/11/06 14:43:45 adamdunkels Exp $
  */
 
 /**
@@ -44,7 +44,6 @@
 #include "net/rime/collect.h"
 #include "dev/leds.h"
 #include "dev/button-sensor.h"
-#include "powertrace.h"
 
 #include "net/netstack.h"
 
@@ -72,14 +71,12 @@ PROCESS_THREAD(example_collect_process, ev, data)
 {
   PROCESS_BEGIN();
 
-
   collect_open(&tc, 130, COLLECT_ROUTER, &callbacks);
 
   if(rimeaddr_node_addr.u8[0] == 1 &&
      rimeaddr_node_addr.u8[1] == 0) {
 	printf("I am sink\n");
 	collect_set_sink(&tc, 1);
-
   }
 
   while(1) {
