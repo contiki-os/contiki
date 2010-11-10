731b29f30e4e211cb085a1df3959a8530268b429
Mariano Alvira <mar@devl.org>
2010-11-10 16:14:20 -0500

    include crm.h

--- src/default_lowlevel.h
+++ src/default_lowlevel.h
@@ -36,6 +36,8 @@
 #ifndef LOWLEVEL_H
 #define LOWLEVEL_H
 
+#include "crm.h"
+
 #define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS);
 void default_vreg_init(void);
 void uart1_init(uint16_t inc, uint16_t mod, uint8_t samp);
