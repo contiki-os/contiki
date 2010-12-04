fdb944a8e2f2bba9304c001bf678ea4741901f3f
dak664 <dak664>
2010-12-04 21:24:29 +0000

    Add status page, change ipv6 address to ::101 and ::102

--- examples/webserver-ipv6-raven/Muninn/makefsdata.h
+++ examples/webserver-ipv6-raven/Muninn/makefsdata.h
@@ -1,7 +1,7 @@
 #include <avr/eeprom.h>
 
-/* Link layer ipv6 address will become fe80::11:22ff:fe33:4456 */
-uint8_t mac_address[8]  EEMEM = {0x02, 0x11, 0x22, 0xff, 0xfe, 0x33, 0x44, 0x56};
+/* Link layer ipv6 address will become fe80::101 */
+uint8_t mac_address[8]  EEMEM = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01};
 uint8_t server_name[16] EEMEM = "muninn";
 uint8_t domain_name[30] EEMEM = "localhost";
 
