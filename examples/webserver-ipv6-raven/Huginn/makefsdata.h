ced88c2effd5bae0b244ad797e92dc09de7f8d71
dak664 <dak664>
2010-12-14 23:22:01 +0000

    Give jackdaw fe80::0 address  and number webservers from fe80::1

--- examples/webserver-ipv6-raven/Huginn/makefsdata.h
+++ examples/webserver-ipv6-raven/Huginn/makefsdata.h
@@ -1,7 +1,6 @@
 #include <avr/eeprom.h>
 
-/* Link layer ipv6 address will become fe80::102 */
-uint8_t mac_address[8]  EEMEM = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x2};
+/* Link layer ipv6 address will become fe80::2 */
+uint8_t mac_address[8]  EEMEM = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02};
 uint8_t server_name[16] EEMEM = "huginn";
 uint8_t domain_name[30] EEMEM = "localhost";
-
