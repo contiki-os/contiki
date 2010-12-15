f55bf050cefdfa775c6c4c6b13ba8357fadf5711
salvopitru <salvopitru>
2010-12-15 15:33:30 +0000

    Added example for the elfloader on the MB851 platform, taken from sky-shell-exec example.

---
+++ examples/mb851/shell-exec/shell-exec-test.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2009, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * $Id: shell-exec-test.c,v 1.1 2010/12/15 15:33:30 salvopitru Exp $
+ */
+
+#include "contiki.h"
+#include "shell.h"
+#include "serial-shell.h"
+
+#include "dev/watchdog.h"
+
+
+/*---------------------------------------------------------------------------*/
+PROCESS(shell_test_process, "Contiki shell");
+AUTOSTART_PROCESSES(&shell_test_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(shell_test_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  serial_shell_init();
+  shell_file_init();
+  shell_coffee_init();
+  shell_text_init();
+  shell_exec_init();
+  shell_base64_init();
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
