3ed04113875946f509093e99203f253cc67da461
oliverschmidt <oliverschmidt>
2011-01-04 11:53:07 +0000

    Adjusted contiki.cfg offset due to increased size of Breadbox64.

--- tools/6502/download.php
+++ tools/6502/download.php
@@ -32,7 +32,7 @@ case 'atari':
     $hex = strtok($_GET['atari-addr-drv'], '-');
     $drv = strtok('-');
     $ext = '-' . $dsk . '.atr';
-    $blk = array(0, 5, 4, 4, 273);
+    $blk = array(0, 5, 4, 4, 275);
     $ofs = 0x00010 + ($blk[$dsk] - 1) * 0x80;
     break;
 }
