f133321b03ba213b9a23e1a521dd7d4a0b7d36a1
Mariano Alvira <mar@devl.org>
2011-01-17 09:55:37 -0500

    remove extra semicolon from macro definition

--- src/default_lowlevel.h
+++ src/default_lowlevel.h
@@ -38,7 +38,8 @@
 
 #include "crm.h"
 
-#define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS);
+#define trim_xtal() pack_XTAL_CNTL(CTUNE_4PF, CTUNE, FTUNE, IBIAS)
+
 void default_vreg_init(void);
 void uart1_init(uint16_t inc, uint16_t mod, uint8_t samp);
 
