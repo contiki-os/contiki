f5ba70cd202300421f8a93fe59071f72d54a45c5
Mariano Alvira <mar@devl.org>
2011-02-06 14:29:01 -0500

    add heap and _sbrk to mc13224v

--- cpu/mc1322x/contiki-misc.c
+++ cpu/mc1322x/contiki-misc.c
@@ -36,6 +36,7 @@
 #include <stdio.h>
 #include "contiki.h"
 #include "mc1322x.h"
+#include <sys/types.h>
 
 int raise(void)
 {
@@ -49,3 +50,25 @@ void srand(unsigned int seed) {
 int rand(void) {
 	return (int)*MACA_RANDOM;
 }
+
+extern int  __HEAP_START;
+extern int  __HEAP_END;
+
+caddr_t _sbrk ( int incr )
+{
+	static unsigned char *heap = NULL;
+	unsigned char *prev_heap;
+
+	if (heap == NULL) {
+		heap = (unsigned char *)&__HEAP_START;
+	}
+	prev_heap = heap;
+	/* check removed to show basic approach */
+
+	if((heap + incr) >= (unsigned char *)&__HEAP_END) return((void *)-1);
+
+	heap += incr;
+
+	return (caddr_t) prev_heap;
+}
+
