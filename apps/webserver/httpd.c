263f7e6ebd4fb126fa8aa7be57493be61c03c57b
dak664 <dak664@embarqmail.com>
2011-02-19 15:43:43 -0500

    Insidious minimal-net segmentation fault if initialized ptr not declared static.

--- apps/webserver/httpd.c
+++ apps/webserver/httpd.c
@@ -185,7 +185,10 @@ PT_THREAD(handle_script(struct httpd_state *s))
 static
 PT_THREAD(send_headers(struct httpd_state *s, const char *statushdr))
 {
-  const char *ptr = NULL;     //gcc warning if not initialized
+/* gcc warning if not initialized. 
+ * If initialized, minimal-net platform segmentation fault if not static...
+ */
+  static const char *ptr = NULL;
 
   PSOCK_BEGIN(&s->sout);
 
