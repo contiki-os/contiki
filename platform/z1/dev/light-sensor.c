701a42e239f16cc2925113d4ffeb584788c1d084
Enric M. Calvo <enric.calvo@gmail.com>
2011-02-27 21:28:25 +0100

    Added dummy light driver. Now skywebsense likes Z1
    The light sensor driver is mapped to the same pin as the potentiometer
    in Z1 Starter Platform. sky-websense is untouched, though.

---
+++ platform/z1/dev/light-sensor.c
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2005-2010, Swedish Institute of Computer Science
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * @(#)$Id: light-sensor.c,v 1.7 2010/08/25 19:30:53 nifi Exp $
+ */
+
+#include "contiki.h"
+#include "lib/sensors.h"
+#include "dev/sky-sensors.h"
+#include "dev/light-sensor.h"
+#include "dev/potentiometer-sensor.h"
+
+#include <io.h>
+
+/* Photodiode 1 (P64) on INCH_4 */
+/* Photodiode 2 (P65) on INCH_5 */
+//Enric#define INPUT_CHANNEL      ((1 << INCH_4) | (1 << INCH_5))
+#ifndef INPUT_CHANNEL
+#define INPUT_CHANNEL      (1 << INCH_4)
+#endif
+
+#ifndef INPUT_REFERENCE
+#define INPUT_REFERENCE     SREF_0
+#endif
+
+#ifdef POTENTIOMETER_MEM
+#define PHOTOSYNTHETIC_MEM  POTENTIOMETER_MEM
+#define TOTAL_SOLAR_MEM     POTENTIOMETER_MEM
+#else
+#define PHOTOSYNTHETIC_MEM  ADC12MEM4
+#define TOTAL_SOLAR_MEM     ADC12MEM4
+#endif
+
+
+const struct sensors_sensor light_sensor;
+
+/*---------------------------------------------------------------------------*/
+static int
+value(int type)
+{
+  switch(type) {
+    /* Photosynthetically Active Radiation. */
+  case LIGHT_SENSOR_PHOTOSYNTHETIC:
+    return PHOTOSYNTHETIC_MEM;
+
+    /* Total Solar Radiation. */
+  case LIGHT_SENSOR_TOTAL_SOLAR:
+    return TOTAL_SOLAR_MEM;
+  }
+  return 0;
+}
+/*---------------------------------------------------------------------------*/
+static int
+status(int type)
+{
+  return sky_sensors_status(INPUT_CHANNEL, type);
+}
+/*---------------------------------------------------------------------------*/
+static int
+configure(int type, int c)
+{
+  return sky_sensors_configure(INPUT_CHANNEL, INPUT_REFERENCE, type, c);
+}
+/*---------------------------------------------------------------------------*/
+SENSORS_SENSOR(light_sensor, "Light", value, configure, status);
