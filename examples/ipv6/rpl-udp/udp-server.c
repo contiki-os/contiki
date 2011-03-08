3685bf254f7cf0b88202d83d1fae2a66e478e397
dak664 <dak664@embarqmail.com>
2011-03-08 14:15:55 -0500

    Server address options giving different 6lowpan header compression

--- examples/ipv6/rpl-udp/udp-server.c
+++ examples/ipv6/rpl-udp/udp-server.c
@@ -108,8 +108,27 @@ PROCESS_THREAD(udp_server_process, ev, data)
   PRINTF("UDP server started\n");
 
 #if UIP_CONF_ROUTER
-  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
-  /* uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr); */
+/* The choice of server address determines its 6LoPAN header compression.
+ * Obviously the choice made here must also be selected in udp-client.c.
+ *
+ * For correct Wireshark decoding using a sniffer, add the /64 prefix to the 6LowPAN protocol preferences,
+ * e.g. set Context 0 to aaaa::.  At present Wireshark copies Context/128 and then overwrites it.
+ * (Setting Context 0 to aaaa::1111:2222:3333:4444 will report a 16 bit compressed address of aaaa::1111:22ff:fe33:xxxx)
+ * Note Wireshark's IPCMV6 checksum verification depends on the correct uncompressed addresses.
+ */
+ 
+#if 0
+/* Mode 1 - 64 bits inline */
+   uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 1);
+#elif 1
+/* Mode 2 - 16 bits inline */
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0x00ff, 0xfe00, 1);
+#else
+/* Mode 3 - derived from link local (MAC) address */
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+#endif
+
   uip_ds6_addr_add(&ipaddr, 0, ADDR_MANUAL);
   root_if = uip_ds6_addr_lookup(&ipaddr);
   if(root_if != NULL) {
