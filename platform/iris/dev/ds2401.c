3da0b124c212ddf511c2b50390d9e9641b1a2386
HATATANI Shinta <gamaguchi@gmail.com>
2011-06-04 21:42:19 +0900

    create iris directory

---
+++ platform/iris/dev/ds2401.c
@@ -0,0 +1,244 @@
+/*
+ * Copyright (c) 2009, University of Colombo School of Computing
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/*
+ * Device driver for the Dallas Semiconductor DS2401 chip. Heavily
+ * based on the application note 126 "1-Wire Communications Through
+ * Software".
+ *
+ * http://www.maxim-ic.com/appnotes.cfm/appnote_number/126
+ */
+
+/*
+ * For now we stuff in Crossbow Technology, Inc's unique OUI.
+ * 00:1A:4C    Crossbow Technology, Inc
+ *
+ * The EUI-64 is a concatenation of the 24-bit OUI value assigned by
+ * the IEEE Registration Authority and a 40-bit extension identifier
+ * assigned by the organization with that OUI assignment.
+ */
+
+#include <avr/io.h>
+#include <string.h>
+#include "contiki.h"
+
+#include "ds2401.h"
+
+unsigned char ds2401_id[8];
+
+/* 1-wire is at PortA.4 */
+#define SERIAL_ID_PIN_READ PINA
+#define SERIAL_ID_PIN_MASK _BV(4)
+#define SERIAL_ID_PxOUT PORTA
+#define SERIAL_ID_PxDIR DDRA
+
+#define SET_PIN_INPUT() (SERIAL_ID_PxDIR &= ~SERIAL_ID_PIN_MASK)
+#define SET_PIN_OUTPUT() (SERIAL_ID_PxDIR |= SERIAL_ID_PIN_MASK)
+
+#define OUTP_0() (SERIAL_ID_PxOUT &= ~SERIAL_ID_PIN_MASK)
+#define OUTP_1() (SERIAL_ID_PxOUT |= SERIAL_ID_PIN_MASK) 
+
+#define PIN_INIT() do{  \
+                     SET_PIN_INPUT();    \
+                     OUTP_0();           \
+                   } while(0)
+
+
+/* Drive the one wire interface low */
+#define OW_DRIVE() do {                    \
+                     SET_PIN_OUTPUT();     \
+                     OUTP_0();             \
+                   } while (0)
+
+/* Release the one wire by turning on the internal pull-up. */
+#define OW_RELEASE() do {                  \
+                       SET_PIN_INPUT();    \
+                       OUTP_1();           \
+                     } while (0)
+
+/* Read one bit. */
+#define INP()  (SERIAL_ID_PIN_READ & SERIAL_ID_PIN_MASK)
+
+/*
+ * Delay times in us.
+ */
+#define tA 6          /* min-5, recommended-6, max-15 */              
+#define tB 64         /* min-59, recommended-64, max-N/A */
+#define tC 60         /* min-60, recommended-60, max-120 */			
+#define tD 10         /* min-5.3, recommended-10, max-N/A */
+#define tE 9          /* min-0.3, recommended-9, max-9.3 */
+#define tF 55         /* min-50, recommended-55, max-N/A */
+#define tG 0          /* min-0, recommended-0, max-0 */
+#define tH 480        /* min-480, recommended-480, max-640 */
+#define tI 70         /* min-60.3, recommended-70, max-75.3 */
+#define tJ 410        /* min-410, recommended-410, max-N/A */
+/*---------------------------------------------------------------------------*/
+/*
+ * The delay caused by calling the delay_loop is given by the following
+ * formula.
+ *           delay(us) = (4n + 1)/XTAL
+ * where n is the number of iterations and XTAL is the clock frequency(in MHz). 
+ * TODO: Moving the delay_loop to dev/clock.c
+ */
+static void
+delay_loop(uint16_t __count)
+{
+  asm volatile ("1: sbiw %0,1" "\n\t"
+                "brne 1b"
+                : "=w" (__count)
+                : "0" (__count)
+               );
+}
+/*---------------------------------------------------------------------------*/
+/*
+ * This macro relies on the compiler doing the arithmetic during compile time
+ * for the needed iterations.!!
+ * In MICAz, XTAL = 7.3728 MHz
+ */
+#define udelay(u) delay_loop(((7.3728F * u)-1)/4)
+/*---------------------------------------------------------------------------*/
+static uint8_t
+reset(void)
+{
+  uint8_t result;
+  OW_DRIVE();
+  udelay(500);     /* 480 < tH < 640 */
+  OW_RELEASE();    /* Releases the bus */
+  udelay(tI);
+  result = INP();
+  udelay(tJ);
+  return result;
+}
+/*---------------------------------------------------------------------------*/
+static void
+write_byte(uint8_t byte)
+{
+  uint8_t i = 7;
+  do {
+    if (byte & 0x01) {
+      OW_DRIVE();
+      udelay(tA);
+      OW_RELEASE();    /* Releases the bus */
+      udelay(tB);
+    } else {
+      OW_DRIVE();
+      udelay(tC);
+      OW_RELEASE();    /* Releases the bus */
+      udelay(tD);
+    }
+    if (i == 0)
+      return;
+    i--;
+    byte >>= 1;
+  } while (1);
+}
+/*---------------------------------------------------------------------------*/
+static unsigned
+read_byte(void)
+{
+  unsigned result = 0;
+  int i = 7;
+  do {
+    OW_DRIVE();
+    udelay(tA);
+    OW_RELEASE();	/* Releases the bus */
+    udelay(tE);
+    if (INP())
+      result |= 0x80;	/* LSbit first */
+    udelay(tF);
+    if (i == 0)
+      return result;
+    i--;
+    result >>= 1;
+  } while (1);
+}
+/*---------------------------------------------------------------------------*/
+/* Polynomial ^8 + ^5 + ^4 + 1 */
+static unsigned
+crc8_add(unsigned acc, unsigned byte)
+{
+  int i;
+  acc ^= byte;
+  for (i = 0; i < 8; i++)
+    if (acc & 1)
+      acc = (acc >> 1) ^ 0x8c;
+    else
+      acc >>= 1;
+
+  return acc;
+}
+/*---------------------------------------------------------------------------*/
+int
+ds2401_init()
+{
+  int i;
+  uint8_t volatile sreg;
+  unsigned family, crc, acc;
+
+  PIN_INIT();
+
+  sreg = SREG;    /* Save status register before disabling interrupts. */
+  cli();    /* Disable interrupts. */
+
+  if (reset() == 0) {
+    write_byte(0x33);    /* Read ROM command. */   
+    family = read_byte();
+    for (i = 7; i >= 2; i--) {
+      ds2401_id[i] = read_byte();
+    }
+    crc = read_byte();
+
+    SREG = sreg;    /* Enable interrupts. */
+
+    if(family != 0x01) {
+      goto fail;
+    }
+    acc = crc8_add(0x0, family);
+    for (i = 7; i >= 2; i--) {
+      acc = crc8_add(acc, ds2401_id[i]);
+    }
+    if (acc == crc) {
+      /* 00:1A:4C    OUI for Crossbow Technology, Inc. */
+      ds2401_id[0] = 0x00;
+      ds2401_id[1] = 0x1A;
+      ds2401_id[2] = 0x4C;
+      return 1;	/* Success! */
+    }
+  } else {
+    SREG = sreg;    /* Enable interrupts. */
+  }
+
+fail:
+  memset(ds2401_id, 0x0, sizeof(ds2401_id));
+  return 0;  /* Fail! */
+}
+/*---------------------------------------------------------------------------*/
---
+++ platform/iris/dev/sensors/mts300.c
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 2009, University of Colombo School of Computing
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ * @(#)$$
+ */
+
+/**
+ * \file
+ *         Device drivers implementation for MTS300 sensor board.
+ * \author
+ *         Kasun Hewage <kasun.ch@gmail.com>
+ */
+
+#include "mts300.h"
+/*---------------------------------------------------------------------------*/
+void
+sounder_on()
+{  SOUNDER_DDR |= SOUNDER_MASK;
+  SOUNDER_PORT &= ~SOUNDER_MASK;
+  SOUNDER_PORT |= SOUNDER_MASK;
+}
+/*---------------------------------------------------------------------------*/
+void 
+sounder_off()
+{
+  SOUNDER_PORT &= ~(SOUNDER_MASK);
+  SOUNDER_DDR &= ~(SOUNDER_MASK);
+}
+/*---------------------------------------------------------------------------*/
+void 
+adc_init()
+{
+  ADMUX = 0;
+  /* AVCC with external capacitor at AREF pin. */
+  //ADMUX |= _BV(REFS0)
+  /* Disable ADC interrupts. */   
+  ADCSRA &= ~( _BV(ADIE) | _BV(ADIF) );
+  /* Set ADC prescaler to 64 and clear interrupt flag. */
+  ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADIE);
+  
+}
+/*---------------------------------------------------------------------------*/
+/* Poll based approach. The interrupt based adc is currently not used.
+   The ADC result is right adjusted. First 8 bits(from left) are in ADCL and
+   other two bits are in ADCH. See Atmega128 datasheet page 228. */
+uint16_t 
+get_adc(int channel)
+{
+  uint16_t reading;
+
+  ADMUX |= (channel & 0x1F);
+
+  /* Disable ADC interrupts. */
+  ADCSRA &= ~_BV(ADIE);
+  /* Clear previous interrupts. */
+  ADCSRA |= _BV(ADIF);
+  /* Enable ADC and start conversion. */
+  ADCSRA |= _BV(ADEN) | _BV(ADSC);
+  /* Wait until conversion is completed. */
+  while ( ADCSRA & _BV(ADSC) );
+  /* Get first 8 bits.  */
+  reading = ADCL;
+  /* Get last two bits. */
+  reading |= (ADCH & 3) << 8;
+  /* Disable ADC. */
+  ADCSRA &= ~_BV(ADEN);
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_light()
+{
+  uint16_t reading;
+
+  /* Enable light sensor. */
+  LIGHT_PORT |= LIGHT_PIN_MASK;       
+  LIGHT_PORT_DDR |= LIGHT_PIN_MASK;
+  /* Disable temperature sensor. */
+  TEMP_PORT_DDR &= ~TEMP_PIN_MASK;
+  TEMP_PORT &= ~TEMP_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(LIGHT_ADC_CHANNEL);
+  /* Disable light sensor. */
+  LIGHT_PORT &= ~LIGHT_PIN_MASK;       
+  LIGHT_PORT_DDR &= ~LIGHT_PIN_MASK;
+    return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_temp()
+{
+  uint16_t reading;
+
+  /* Disable light sensor. */
+  LIGHT_PORT &= ~LIGHT_PIN_MASK;       
+  LIGHT_PORT_DDR &= ~LIGHT_PIN_MASK;
+  /* Enable temperature sensor. */
+  TEMP_PORT_DDR |= TEMP_PIN_MASK;
+  TEMP_PORT |= TEMP_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(TEMP_ADC_CHANNEL);
+  /* Disable temperature sensor. */
+  TEMP_PORT_DDR &= ~TEMP_PIN_MASK;
+  TEMP_PORT &= ~TEMP_PIN_MASK;
+
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_accx()
+{
+  uint16_t reading;
+
+  /* Enable accelerometer. */
+  ACCEL_PORT_DDR |= ACCEL_PIN_MASK;
+  ACCEL_PORT |= ACCEL_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(ACCELX_ADC_CHANNEL);
+  /* Enable accelerometer. */
+  ACCEL_PORT_DDR &= ~ACCEL_PIN_MASK;
+  ACCEL_PORT &= ~ACCEL_PIN_MASK;
+
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_accy()
+{
+  uint16_t reading;
+
+  /* Enable accelerometer. */
+  ACCEL_PORT_DDR |= ACCEL_PIN_MASK;
+  ACCEL_PORT |= ACCEL_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(ACCELY_ADC_CHANNEL);
+  /* Enable accelerometer. */
+  ACCEL_PORT_DDR &= ~ACCEL_PIN_MASK;
+  ACCEL_PORT &= ~ACCEL_PIN_MASK;
+
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_magx()
+{
+  uint16_t reading;
+
+  /* Enable magnetometer. */
+  MAGNET_PORT_DDR |= MAGNET_PIN_MASK;
+  MAGNET_PORT |= MAGNET_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(MAGNETX_ADC_CHANNEL);
+  /* Enable magnetometer. */
+  MAGNET_PORT_DDR &= ~MAGNET_PIN_MASK;
+  MAGNET_PORT &= ~MAGNET_PIN_MASK;
+
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_magy()
+{
+  uint16_t reading;
+
+  /* Enable magnetometer. */
+  MAGNET_PORT_DDR |= MAGNET_PIN_MASK;
+  MAGNET_PORT |= MAGNET_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(MAGNETY_ADC_CHANNEL);
+  /* Enable magnetometer. */
+  MAGNET_PORT_DDR &= ~MAGNET_PIN_MASK;
+  MAGNET_PORT &= ~MAGNET_PIN_MASK;
+
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+uint16_t 
+get_mic()
+{
+  uint16_t reading;
+
+    /* Enable mic. */
+  MIC_PORT_DDR |= MIC_PIN_MASK;
+  MIC_PORT |= MIC_PIN_MASK;
+  /* Read ADC. */
+  reading = get_adc(MIC_ADC_CHANNEL);
+  /* Enable mic. */
+  MIC_PORT_DDR &= ~MIC_PIN_MASK;
+  MIC_PORT &= ~MIC_PIN_MASK;
+
+  return reading;
+}
+/*---------------------------------------------------------------------------*/
+
