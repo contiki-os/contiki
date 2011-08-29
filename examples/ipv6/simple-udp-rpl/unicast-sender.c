277853a0189db168b372622206bd19dc522d99b7
Adam Dunkels <adam@sics.se>
2011-08-29 21:33:23 +0200

    Example code for the simple UDP API

---
+++ examples/ipv6/simple-udp-rpl/unicast-sender.c
@@ -0,0 +1,105 @@
+#include "contiki.h"
+#include "lib/random.h"
+#include "sys/ctimer.h"
+#include "sys/etimer.h"
+#include "net/uip.h"
+#include "net/uip-ds6.h"
+#include "net/uip-debug.h"
+
+#include "node-id.h"
+
+#include "simple-udp.h"
+#include "servreg-hack.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#define UDP_PORT 1234
+#define SERVICE_ID 190
+
+#define SEND_INTERVAL		(60 * CLOCK_SECOND)
+#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
+
+static struct simple_udp_connection unicast_connection;
+
+/*---------------------------------------------------------------------------*/
+PROCESS(unicast_sender_process, "Unicast sender example process");
+AUTOSTART_PROCESSES(&unicast_sender_process);
+/*---------------------------------------------------------------------------*/
+static void
+receiver(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+  printf("Data received on port %d from port %d with length %d\n",
+         receiver_port, sender_port, datalen);
+}
+/*---------------------------------------------------------------------------*/
+static void
+set_global_address(void)
+{
+  uip_ipaddr_t ipaddr;
+  int i;
+  uint8_t state;
+
+  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+  printf("IPv6 addresses: ");
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+      printf("\n");
+    }
+  }
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(unicast_sender_process, ev, data)
+{
+  static struct etimer periodic_timer;
+  static struct etimer send_timer;
+
+  PROCESS_BEGIN();
+
+  servreg_hack_init();
+
+  set_global_address();
+
+  simple_udp_register(&unicast_connection, UDP_PORT,
+                      NULL, UDP_PORT, receiver);
+
+  etimer_set(&periodic_timer, SEND_INTERVAL);
+  while(1) {
+
+    PROCESS_YIELD_UNTIL(ev == PROCESS_EVENT_TIMER);
+    if(data == &periodic_timer) {
+      etimer_reset(&periodic_timer);
+      etimer_set(&send_timer, SEND_TIME);
+    }
+    if(data == &send_timer) {
+      uip_ipaddr_t *addr;
+      addr = servreg_hack_lookup(SERVICE_ID);
+      if(addr != NULL) {
+        char buf[20];
+
+        printf("Sending unicast to ");
+        uip_debug_ipaddr_print(addr);
+        printf("\n");
+        sprintf(buf, "From %d", node_id);
+        simple_udp_sendto(&unicast_connection, buf, strlen(buf) + 1, addr);
+      } else {
+        printf("Service %d not found\n", SERVICE_ID);
+      }
+    }
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
