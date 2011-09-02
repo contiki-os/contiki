4470800aa48b0e1d97a1acd39e4bdb015aa37d7b
Mariano Alvira <mar@devl.org>
2011-09-02 12:32:47 -0400

    change mc1322x rtimers to 32-bit
    hardcode a close default value for the RTC

--- cpu/mc1322x/rtimer-arch.h
+++ cpu/mc1322x/rtimer-arch.h
@@ -53,7 +53,7 @@
 #if USE_32KHZ_XTAL
 #define RTIMER_ARCH_SECOND 32768
 #else
-#define RTIMER_ARCH_SECOND 2000 /* bogus value --- you should set xmac_config after calibration */
+#define RTIMER_ARCH_SECOND 18778 /* close --- should get calibrated */
 #endif
 
 #define rtimer_arch_now() (*CRM_RTC_COUNT)
