a2905aa7af9b08bf977a5c8fef3c8789b3a01230
Adam Dunkels <adam@sics.se>
2011-09-05 11:46:15 +0200

    Made the sky and z1 platform handling of PROJECT_CONF_H the same as the other platforms: the makefile should define PROJECT_CONF_H to be the name of the header file to include. This header file should #undef everything it #defines, as it is included after all the other configuration options in contiki-conf.h

--- platform/sky/contiki-conf.h
+++ platform/sky/contiki-conf.h
@@ -4,10 +4,6 @@
 #ifndef CONTIKI_CONF_H
 #define CONTIKI_CONF_H
 
-#ifdef PROJECT_CONF_H
-#include "project-conf.h"
-#endif /* PROJECT_CONF_H */
-
 #ifdef PLATFORM_CONF_H
 #include PLATFORM_CONF_H
 #else
@@ -209,6 +205,12 @@
 #define UIP_CONF_TCP_SPLIT       0
 
 
+
+/* include the project config */
+/* PROJECT_CONF_H might be defined in the project Makefile */
+#ifdef PROJECT_CONF_H
+#include PROJECT_CONF_H
+#endif /* PROJECT_CONF_H */
 
 
 #endif /* CONTIKI_CONF_H */
