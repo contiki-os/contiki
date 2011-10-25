11a35eb1fb79a797e5394ed84e9993cfd3f66cd0
Mariano Alvira <mar@devl.org>
2011-10-25 10:09:28 -0400

    Add node_id to econotag

--- platform/redbee-econotag/contiki-mc1322x-main.c
+++ platform/redbee-econotag/contiki-mc1322x-main.c
@@ -280,6 +280,8 @@ void oui_to_eui64(rimeaddr_t *eui64, uint32_t oui, uint64_t ext) {
 	eui64->u8[7] =  ext        & 0xff;
 }
 
+unsigned short node_id = 0;
+
 void
 set_rimeaddr(rimeaddr_t *addr) 
 {
@@ -332,6 +334,7 @@ set_rimeaddr(rimeaddr_t *addr)
 		PRINTF("loading rime address from flash.\n\r");
 	}
 
+	node_id = (addr->u8[6] << 8 | addr->u8[7]);
 	rimeaddr_set_node_addr(addr);
 }
 
