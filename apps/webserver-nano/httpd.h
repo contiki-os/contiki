21a8f2af6f4fb7785e0e4599fbba3eb78c839058
David Kopf <dak664@embarqmail.com>
2011-11-03 14:58:33 -0400

    Add streaming ajax page with csv text buffer

--- apps/webserver-nano/httpd.h
+++ apps/webserver-nano/httpd.h
@@ -81,6 +81,7 @@
 #define WEBSERVER_CONF_ROUTES    1
 #define WEBSERVER_CONF_SENSORS   0
 #define WEBSERVER_CONF_TICTACTOE 0   //Needs passquery of at least 10 chars 
+#define WEBSERVER_CONF_AJAX      0
 //#define WEBSERVER_CONF_PASSQUERY 10
 #if WEBSERVER_CONF_PASSQUERY
 extern char httpd_query[WEBSERVER_CONF_PASSQUERY];
@@ -126,6 +127,7 @@ extern char httpd_query[WEBSERVER_CONF_PASSQUERY];
 #define WEBSERVER_CONF_ROUTES    1
 #define WEBSERVER_CONF_SENSORS   1
 #define WEBSERVER_CONF_TICTACTOE 1   //Needs passquery of at least 10 chars 
+#define WEBSERVER_CONF_AJAX      1
 #define WEBSERVER_CONF_PASSQUERY 10
 #if WEBSERVER_CONF_PASSQUERY
 extern char httpd_query[WEBSERVER_CONF_PASSQUERY];
