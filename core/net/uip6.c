e716ce25a184f96186f530e162cab5dbb9f8fad9
Nicolas Tsiftes <nvt@sics.se>
2011-11-16 08:50:01 +0100

    DEBUG off.

--- core/net/uip6.c
+++ core/net/uip6.c
@@ -83,7 +83,7 @@
 /* For Debug, logging, statistics                                            */
 /*---------------------------------------------------------------------------*/
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
