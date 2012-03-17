ccd3f55c9d7114534e109ded5a230ec490f814a6
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2012-03-17 11:47:09 +0100

    Also excluded platform native from static routing.

--- examples/er-rest-example/rest-server-example.c
+++ examples/er-rest-example/rest-server-example.c
@@ -57,7 +57,7 @@
 
 
 
-#if !UIP_CONF_IPV6_RPL && !defined (CONTIKI_TARGET_MINIMAL_NET)
+#if !UIP_CONF_IPV6_RPL && !defined (CONTIKI_TARGET_MINIMAL_NET) && !defined (CONTIKI_TARGET_NATIVE)
 #warning "Compiling with static routing!"
 #include "static-routing.h"
 #endif
