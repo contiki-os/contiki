5c232e7263af3ca0548e992bff83bd7f20e5d21c
Ivan Delamer <delamer@inicotech.com>
2012-03-27 19:48:26 -0600

    Remove compiler warning for remove_ext_header().

--- core/net/tcpip.c
+++ core/net/tcpip.c
@@ -570,6 +570,7 @@ tcpip_ipv6_output(void)
 	  PRINTF("FALLBACK: removing ext hdrs & setting proto %d %d\n", 
 		 uip_ext_len, *((uint8_t *)UIP_IP_BUF + 40));
 	  if(uip_ext_len > 0) {
+	    extern void remove_ext_hdr(void);
 	    uint8_t proto = *((uint8_t *)UIP_IP_BUF + 40);
 	    remove_ext_hdr();
 	    /* This should be copied from the ext header... */
