f328a8a483407e80d0b65d8c5b7a0deab9a2a04b
Niclas Finne <nfi@sics.se>
2012-04-20 21:15:05 +0200

    Stop the CPU when a breakpoint is triggered

--- tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/MspMote.java
+++ tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/MspMote.java
@@ -64,7 +64,6 @@ import se.sics.mspsim.cli.LineListener;
 import se.sics.mspsim.cli.LineOutputStream;
 import se.sics.mspsim.core.EmulationException;
 import se.sics.mspsim.core.MSP430;
-import se.sics.mspsim.core.MSP430Constants;
 import se.sics.mspsim.platform.GenericNode;
 import se.sics.mspsim.ui.JFrameWindowManager;
 import se.sics.mspsim.util.ComponentRegistry;
@@ -138,6 +137,7 @@ public abstract class MspMote extends AbstractEmulatedMote implements Mote, Watc
    */
   public void stopNextInstruction() {
     stopNextInstruction = true;
+    getCPU().stop();
   }
 
   protected MoteInterfaceHandler createMoteInterfaceHandler() {
