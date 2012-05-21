9dd2327f07f318d944adcfcb5e014c87cd008221
Niclas Finne <nfi@sics.se>
2012-05-21 17:33:39 +0200

    Replaced MSPSim interface SkyByteRadio with Msp802154Radio to easier support other 802.15.4 radio chips than CC2420.

--- tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/TyndallMoteType.java
+++ tools/cooja/apps/mspsim/src/se/sics/cooja/mspmote/TyndallMoteType.java
@@ -58,7 +58,7 @@ import se.sics.cooja.interfaces.RimeAddress;
 import se.sics.cooja.mspmote.interfaces.MspClock;
 import se.sics.cooja.mspmote.interfaces.MspDebugOutput;
 import se.sics.cooja.mspmote.interfaces.MspMoteID;
-import se.sics.cooja.mspmote.interfaces.SkyByteRadio;
+import se.sics.cooja.mspmote.interfaces.Msp802154Radio;
 import se.sics.cooja.mspmote.interfaces.TyndallLED;
 import se.sics.cooja.mspmote.interfaces.UsciA0Serial;
 
@@ -188,7 +188,7 @@ public class TyndallMoteType extends MspMoteType {
         MoteAttributes.class,
         MspClock.class,
         MspMoteID.class,
-        SkyByteRadio.class,
+        Msp802154Radio.class,
         UsciA0Serial.class,
         TyndallLED.class,
         MspDebugOutput.class
