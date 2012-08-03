48d99f15c8b71dcd4359c7391b415b4266de02ed
Philippe Rétornaz <philippe.retornaz@epfl.ch>
2012-08-03 17:04:11 +0200

    cc253x: fix dma interrupt enable flag

--- cpu/cc253x/dev/dma.c
+++ cpu/cc253x/dev/dma.c
@@ -46,7 +46,7 @@ dma_init(void)
   DMA1CFGL = tmp_ptr;
 #endif
 
-  IEN1_DMAIE = 1;	/* Enable DMA interrupts */
+  DMAIE = 1;	/* Enable DMA interrupts */
 }
 /*---------------------------------------------------------------------------*/
 /*
@@ -62,7 +62,7 @@ dma_associate_process(struct process * p, uint8_t c)
 
   if(p) {
     dma_conf[c].inc_prio |= 8; /* Enable interrupt generation */
-    IEN1_DMAIE = 1; /* Make sure DMA interrupts are acknowledged */
+    DMAIE = 1; /* Make sure DMA interrupts are acknowledged */
   }
   dma_callback[c] = p;
 }
