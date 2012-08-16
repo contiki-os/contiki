d795e4cb5d28399650c51ab5f48ec88b6b21e4f3
George Oikonomou <g.oikonomou@bristol.ac.uk>
2012-08-16 14:56:35 +0100

    cc253x: Renamed the P2 ISR
    This is mainly a naming convention thing, we want to have 'isr'
    as part of the name, instead of 'int'. We also want port_2 instead
    of p2 because we already had port_1

--- cpu/cc253x/dev/p2-intr.h
+++ cpu/cc253x/dev/p2-intr.h
@@ -48,7 +48,7 @@ void cc253x_p2_unregister_handler(struct cc253x_p2_handler *h);
 /* When the ISR is not needed, hide its prototype from the module containing
  * main() to prevent linking it */
 #if PORT_2_ISR_ENABLED
-void cc253x_p2_int(void) __interrupt(P2INT_VECTOR);
+void port_2_isr(void) __interrupt(P2INT_VECTOR);
 #endif
 
 #define cc253x_p2_irq_disable(flag) do { flag = IEN2 & 0x2; IEN2 &= ~0x2; } while(0)
