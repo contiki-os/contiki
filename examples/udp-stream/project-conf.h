1bb055bae6633d520477a133e5d866416c4dfa40
simonduq <simonduq@sics.se>
2012-08-30 13:12:26 +0200

    Fixed the udp-stream example: use the new RPL interface, reduce RAM usage

--- examples/udp-stream/project-conf.h
+++ examples/udp-stream/project-conf.h
@@ -35,13 +35,13 @@
 /* Free some code and RAM space */
 #define UIP_CONF_TCP                    0
 #undef UIP_CONF_DS6_NBR_NBU
-#define UIP_CONF_DS6_NBR_NBU            12
+#define UIP_CONF_DS6_NBR_NBU            8
 #undef UIP_CONF_DS6_ROUTE_NBU
-#define UIP_CONF_DS6_ROUTE_NBU          12
+#define UIP_CONF_DS6_ROUTE_NBU          8
 
 /* The total number of queuebuf */
 #undef QUEUEBUF_CONF_NUM
-#define QUEUEBUF_CONF_NUM               140
+#define QUEUEBUF_CONF_NUM               128
 /* The number of queuebuf actually stored in RAM. If
    not set or equal to the total number of queuebuf,
    swapping is disabled, and CFS not linked. */
