189407c04e9bc75b0681963c8a24a19314a0103b
Mariano Alvira <mar@devl.org>
2012-10-20 22:29:16 -0400

    Newer and cleaner TARGET=econotag as well as robust mc13224v configuration system.
    (also deprecate TARGET=redbee-econotag)
    
    - mc13224v now automatically probes hardware config for buck converter
    and 32kHz crystal as well as automatically monitors battery voltage
    and manages the buck accordingly.
    
    - new flashed based config system for mc13224v parameters such has
      radio modes (demod, autoack), nvmtype, mac address, channel and
      power.
    
    - considerably cleaned up econotag platform code (suffered from severe
      case of bit-rot)

---
+++ cpu/mc1322x/config.c
@@ -0,0 +1,83 @@
+/* MC1322x flash config system */
+
+#include <mc1322x.h>
+#include "config.h"
+
+/* debug */
+#define DEBUG DEBUG_FULL
+#include "net/uip-debug.h"
+
+mc1322xConfig mc1322x_config;
+
+void dump_bytes(uint32_t addr, uint16_t num);
+
+/* takes an mc1322xConf and initializes to default values */
+void mc1322x_config_set_default(mc1322xConfig *c) {
+	nvmType_t type;
+	c->magic = MC1322X_CONFIG_MAGIC;
+	c->version = MC1322X_CONFIG_VERSION;
+	c->eui = 0;
+	c->channel = RF_CHANNEL - 11;
+	c->power = 0x11;
+	c->flags.demod = DEMOD_DCD;
+	c->flags.autoack = AUTOACK;
+	nvm_detect(gNvmInternalInterface_c, &type);
+	c->flags.nvmtype = type;
+}
+
+/* write out config to flash */
+void mc1322x_config_save(mc1322xConfig *c) {
+	nvmErr_t err;
+	err = nvm_erase(gNvmInternalInterface_c, c->flags.nvmtype, 1 << MC1322X_CONFIG_PAGE/4096);
+	err = nvm_write(gNvmInternalInterface_c, c->flags.nvmtype, (uint8_t *)c, MC1322X_CONFIG_PAGE, sizeof(mc1322xConfig));
+}
+
+/* load the config from flash to the pass conf structure */
+void mc1322x_config_restore(mc1322xConfig *c) {
+	nvmErr_t err;
+	nvmType_t type;
+	if (c->flags.nvmtype == 0) { nvm_detect(gNvmInternalInterface_c, &type); }
+	c->flags.nvmtype = type;
+	err = nvm_read(gNvmInternalInterface_c, c->flags.nvmtype, c, MC1322X_CONFIG_PAGE, sizeof(mc1322xConfig));
+}
+
+/* check the flash for magic number and proper version */
+int mc1322x_config_valid(mc1322xConfig *c) {
+	if (c->magic == MC1322X_CONFIG_MAGIC &&
+	    c->version == MC1322X_CONFIG_VERSION) {
+		return 1;
+	} else {
+#if DEBUG
+		if (c->magic != MC1322X_CONFIG_MAGIC) { PRINTF("config bad magic %04x\n\r", c->magic); }
+		if (c->version != MC1322X_CONFIG_MAGIC) { PRINTF("config bad version %04x\n\r", c->version); }
+#endif
+		return -1;
+	}
+}
+
+void mc1322x_config_print(void) {
+	uint64_t eui64;
+	PRINTF("mc1322x config:\n\r");
+	PRINTF("  magic:    %04x\n\r", mc1322x_config.magic);
+	PRINTF("  version:  %d\n\r",   mc1322x_config.version);
+	PRINTF("  eui:      %08x%08x\n\r", (uint32_t)(mc1322x_config.eui>>32), (uint32_t)(mc1322x_config.eui & 0xffffffff));
+	PRINTF("  channel:  %d\n\r",   mc1322x_config.channel);
+	PRINTF("  power:    %d\n\r",   mc1322x_config.power);
+	PRINTF("  flags:    %08x\n\r",   mc1322x_config.flags);
+	PRINTF("     demod:    %d\n\r",   mc1322x_config.flags.demod);
+	PRINTF("     autoack:  %d\n\r",   mc1322x_config.flags.autoack);
+	PRINTF("     nvm type: %d\n\r",   mc1322x_config.flags.nvmtype);
+}
+
+void dump_bytes(uint32_t addr, uint16_t num) {
+	uint32_t buf[num/4];
+	nvmErr_t err;
+	uint16_t i;
+
+	err = nvm_read(gNvmInternalInterface_c, mc1322x_config.flags.nvmtype, (uint8_t *)buf, addr, num);
+	PRINTF("nvm_read returned: 0x%02x\r\n", err);
+
+	for(i=0; i < num/4; i++) {
+		printf("0x%08x\r\n", (unsigned int)buf[i]);
+	}
+}
---
+++ platform/econotag/main.c
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2012, Mariano Alvira <mar@devl.org> and other contributors
+ * to the MC1322x project (http://mc1322x.devl.org) and Contiki.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+#include <string.h>
+
+/* debug */
+#define DEBUG DEBUG_FULL
+#include "net/uip-debug.h"
+
+/* contiki */
+#include "contiki.h"
+#include "dev/button-sensor.h"
+#include "net/rime/rimeaddr.h"
+#include "net/netstack.h"
+
+/* mc1322x */
+#include "mc1322x.h"
+#include "config.h"
+#include "contiki-uart.h"
+
+/* econotag */
+#include "platform_prints.h"
+
+SENSORS(&button_sensor);
+
+#ifndef M12_CONF_SERIAL
+#define M12_SERIAL 0x000000
+#else
+#define M12_SERIAL M12_CONF_SERIAL
+#endif
+
+int main(void) {
+
+	mc1322x_init();
+
+	/* m12_init() flips the mux switch */
+
+	/* trims the main crystal load capacitance */
+	if (!FORCE_ECONOTAG_I && CRM->SYS_CNTLbits.XTAL32_EXISTS) {
+		/* M12 based econotag */
+		PRINTF("trim xtal for M12\n\r");
+		CRM->XTAL_CNTLbits.XTAL_CTUNE = (M12_CTUNE_4PF << 4) | M12_CTUNE;
+		CRM->XTAL_CNTLbits.XTAL_FTUNE = M12_FTUNE;
+	} else {
+		/* econotag I */
+		PRINTF("trim xtal for Econotag I\n\r");
+		CRM->XTAL_CNTLbits.XTAL_CTUNE = (ECONOTAG_CTUNE_4PF << 4) | ECONOTAG_CTUNE;
+		CRM->XTAL_CNTLbits.XTAL_FTUNE = ECONOTAG_FTUNE;
+	}
+
+	/* create mac address if blank*/
+	if (mc1322x_config.eui == 0) {
+		/* mac address is blank */
+		/* construct a new mac address based on IAB or OUI definitions */
+
+		/* if an M12_SERIAL number is not defined */
+		/* generate a random extension in the Redwire experimental IAB */
+		/* The Redwire IAB (for development only) is: */
+		/* OUI: 0x0050C2 IAB: 0xA8C */
+		/* plus a random 24-bit extension */
+		/* Otherwise, construct a mac based on the M12_SERIAL */
+		/* Owners of an Econotag I (not M12 based) can request a serial number from Redwire */
+		/* to use here */
+
+		/* M12 mac is of the form "EC473C4D12000000" */
+		/* Redwire's OUI: EC473C */
+		/* M12: 4D12 */
+		/* next six nibbles are the M12 serial number as hex */
+		/* e.g. if the barcode reads: "12440021" = BDD1D5 */
+		/* full mac is EC473C4D12BDD1D5 */
+
+#if (M12_SERIAL == 0)
+                /* use random mac from experimental range */
+		mc1322x_config.eui = (0x0050C2A8Cull << 24) | (*MACA_RANDOM & (0xffffff));
+#else
+		/* construct mac from serial number */
+		mc1322x_config.eui = (0xEC473C4D12ull << 24) | M12_SERIAL;
+#endif
+		mc1322x_config_save(&mc1322x_config);		
+	} 
+	
+	/* configure address on maca hardware and RIME */
+	contiki_maca_set_mac_address(mc1322x_config.eui);
+
+#if WITH_UIP6
+	memcpy(&uip_lladdr.addr, &rimeaddr_node_addr.u8, sizeof(uip_lladdr.addr));
+	queuebuf_init();
+	NETSTACK_RDC.init();
+	NETSTACK_MAC.init();
+	NETSTACK_NETWORK.init();
+  #if DEBUG_ANNOTATE
+	print_netstack();
+  #endif
+	process_start(&tcpip_process, NULL);
+  #if DEBUG_ANNOTATE
+	print_lladdrs();
+  #endif
+#endif /* endif WITH_UIP6 */
+
+	process_start(&sensors_process, NULL);
+
+	print_processes(autostart_processes); 
+	autostart_start(autostart_processes);
+
+	/* Main scheduler loop */
+	while(1) {
+		check_maca();
+
+		if(uart1_input_handler != NULL) {
+			if(uart1_can_get()) {
+				uart1_input_handler(uart1_getc());
+			}
+		}
+		
+		process_run();
+	}
+	
+	return 0;
+}
