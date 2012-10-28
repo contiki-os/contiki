67bf9ec10e328ebf1efe7649649182218860cc85
George Oikonomou <g.oikonomou@bristol.ac.uk>
2012-10-28 19:42:16 +0000

    DOS EOL and indentation fixes (platform files)
    - Removed some DOS EOLs
    - Changed some tabs to spaces
    - Removed some trailing whitespaces
    
    Closes #6

--- platform/micaz/dev/sensors/mts300.c
+++ platform/micaz/dev/sensors/mts300.c
@@ -47,30 +47,30 @@ sounder_on()
   SOUNDER_PORT |= SOUNDER_MASK;
 }
 /*---------------------------------------------------------------------------*/
-void 
+void
 sounder_off()
 {
   SOUNDER_PORT &= ~(SOUNDER_MASK);
   SOUNDER_DDR &= ~(SOUNDER_MASK);
 }
 /*---------------------------------------------------------------------------*/
-void 
+void
 adc_init()
 {
   ADMUX = 0;
   /* AVCC with external capacitor at AREF pin. */
   //ADMUX |= _BV(REFS0)
-  /* Disable ADC interrupts. */   
+  /* Disable ADC interrupts. */
   ADCSRA &= ~( _BV(ADIE) | _BV(ADIF) );
   /* Set ADC prescaler to 64 and clear interrupt flag. */
   ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADIE);
-  
+
 }
 /*---------------------------------------------------------------------------*/
 /* Poll based approach. The interrupt based adc is currently not used.
    The ADC result is right adjusted. First 8 bits(from left) are in ADCL and
    other two bits are in ADCH. See Atmega128 datasheet page 228. */
-uint16_t 
+uint16_t
 get_adc(int channel)
 {
   uint16_t reading;
@@ -94,13 +94,13 @@ get_adc(int channel)
   return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_light()
 {
   uint16_t reading;
 
   /* Enable light sensor. */
-  LIGHT_PORT |= LIGHT_PIN_MASK;       
+  LIGHT_PORT |= LIGHT_PIN_MASK;
   LIGHT_PORT_DDR |= LIGHT_PIN_MASK;
   /* Disable temperature sensor. */
   TEMP_PORT_DDR &= ~TEMP_PIN_MASK;
@@ -108,18 +108,18 @@ get_light()
   /* Read ADC. */
   reading = get_adc(LIGHT_ADC_CHANNEL);
   /* Disable light sensor. */
-  LIGHT_PORT &= ~LIGHT_PIN_MASK;       
+  LIGHT_PORT &= ~LIGHT_PIN_MASK;
   LIGHT_PORT_DDR &= ~LIGHT_PIN_MASK;
     return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_temp()
 {
   uint16_t reading;
 
   /* Disable light sensor. */
-  LIGHT_PORT &= ~LIGHT_PIN_MASK;       
+  LIGHT_PORT &= ~LIGHT_PIN_MASK;
   LIGHT_PORT_DDR &= ~LIGHT_PIN_MASK;
   /* Enable temperature sensor. */
   TEMP_PORT_DDR |= TEMP_PIN_MASK;
@@ -133,7 +133,7 @@ get_temp()
   return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_accx()
 {
   uint16_t reading;
@@ -150,7 +150,7 @@ get_accx()
   return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_accy()
 {
   uint16_t reading;
@@ -167,7 +167,7 @@ get_accy()
   return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_magx()
 {
   uint16_t reading;
@@ -184,7 +184,7 @@ get_magx()
   return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_magy()
 {
   uint16_t reading;
@@ -201,7 +201,7 @@ get_magy()
   return reading;
 }
 /*---------------------------------------------------------------------------*/
-uint16_t 
+uint16_t
 get_mic()
 {
   uint16_t reading;
