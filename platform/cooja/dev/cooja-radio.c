02746cfd7292d612e6cd742ffcea8a2367bdcadf
Yvonne-Anne Pignolet <yvonneanne.pignolet@gmail.com>
2012-10-30 11:42:07 +0100

    Cooja: Add access to LQI and RSSI to Contiki motes

--- platform/cooja/dev/cooja-radio.c
+++ platform/cooja/dev/cooja-radio.c
@@ -62,6 +62,7 @@ int simSignalStrength = -100;
 int simLastSignalStrength = -100;
 char simPower = 100;
 int simRadioChannel = 26;
+int simLQI = 105;
 
 static const void *pending_data;
 
@@ -91,6 +92,12 @@ int
 radio_signal_strength_current(void)
 {
   return simSignalStrength;
+}
+/*---------------------------------------------------------------------------*/
+int
+radio_LQI(void)
+{
+	return simLQI;
 }
 /*---------------------------------------------------------------------------*/
 static int
@@ -145,6 +152,9 @@ radio_read(void *buf, unsigned short bufsize)
 
   memcpy(buf, simInDataBuffer, simInSize);
   simInSize = 0;
+  packetbuf_set_attr(PACKETBUF_ATTR_RSSI, simSignalStrength);
+  packetbuf_set_attr(PACKETBUF_ATTR_LINK_QUALITY, simLQI);
+
   return tmp;
 }
 /*---------------------------------------------------------------------------*/
