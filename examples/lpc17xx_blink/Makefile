bbb5a44879de3d8bf94f4c37322b7f4d7edf9dd7
Instant Contiki <user@instant-contiki.(none)>
2012-11-15 16:55:42 +0100

    add a blink test for lpc17xx

---
+++ examples/lpc17xx_blink/Makefile
@@ -0,0 +1,7 @@
+CONTIKI_PROJECT = hello-world
+all: $(CONTIKI_PROJECT)
+
+#UIP_CONF_IPV6=1
+
+CONTIKI = ../..
+include $(CONTIKI)/Makefile.include
---
+++ examples/lpc17xx_blink/hello-world.c
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2006, Swedish Institute of Computer Science.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         A very simple Contiki application showing how Contiki programs look
+ * \author
+ *         Adam Dunkels <adam@sics.se>
+ */
+
+#include "contiki.h"
+
+#include <stdio.h> /* For printf() */
+#include "LPC17xx.h"
+
+volatile uint32_t temp;
+void _delay(uint32_t del)
+{
+	uint32_t i;
+	for(i=0;i<del;i++)
+		temp = i;
+}
+/*---------------------------------------------------------------------------*/
+PROCESS(hello_world_process, "Hello world process");
+AUTOSTART_PROCESSES(&hello_world_process);
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(hello_world_process, ev, data)
+{
+  PROCESS_BEGIN();
+
+  LPC_SC->PCONP |= ( 1 << 15 ); // power up GPIO
+  LPC_GPIO1->FIODIR |= 1 << 29; // puts P1.29 into output
+  while (1) {
+    LPC_GPIO1->FIOPIN |= 1 << 29; // make P1.29 high
+    _delay( 1 << 24 );
+    LPC_GPIO1->FIOPIN &= ~( 1 << 29 ); // make P1.29 low
+    _delay( 1 << 24 );
+
+
+  }
+//  printf("Hello, world\n");
+  
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
