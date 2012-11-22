eda571684c41106ed2dbca34bc19df61383ebd8c
Adam Dunkels <adam@thingsquare.com>
2012-11-22 11:55:01 +0100

    Added NULL pointer guards

--- core/net/rime/collect.c
+++ core/net/rime/collect.c
@@ -814,8 +814,10 @@ handle_ack(struct collect_conn *tc)
        chance that another parent will be chosen. */
     if(msg.flags & ACK_FLAGS_CONGESTED) {
       PRINTF("ACK flag indicated parent was congested.\n");
-      collect_neighbor_set_congested(n);
-      collect_neighbor_tx(n, tc->max_rexmits * 2);
+      if(n != NULL) {
+	collect_neighbor_set_congested(n);
+	collect_neighbor_tx(n, tc->max_rexmits * 2);
+      }
       update_rtmetric(tc);
     }
     if((msg.flags & ACK_FLAGS_DROPPED) == 0) {
