4d5275a523df0bfca11a6409bc7d5a32ed489663
Laurent Deru <laurent.deru@cetic.be>
2013-02-15 00:58:44 +0100

    Support channel configuration

--- examples/6lbr-demo/shell-6lbr.c
+++ examples/6lbr-demo/shell-6lbr.c
@@ -20,6 +20,11 @@ SHELL_COMMAND(start6lbr_command,
 	      "start6lbr",
 	      "start6lbr: Start IP stack and 6LBR apps",
 	      &shell_start_6lbr_process);
+PROCESS(shell_rfchannel_process, "rfchannel");
+SHELL_COMMAND(rfchannel_command,
+	      "rfchannel",
+	      "rfchannel <channel>: change CC2420 radio channel (11 - 26)",
+	      &shell_rfchannel_process);
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(shell_fast_reboot_process, ev, data)
 {
@@ -49,10 +54,26 @@ PROCESS_THREAD(shell_start_6lbr_process, ev, data)
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+PROCESS_THREAD(shell_rfchannel_process, ev, data)
+{
+  uint16_t channel;
+  const char *newptr;
+  PROCESS_BEGIN();
+
+  channel = shell_strtolong(data, &newptr);
+
+  if(newptr != data) {
+    cc2420_set_channel(channel);
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
 void
 shell_6lbr_init(void)
 {
   shell_register_command(&fast_reboot_command);
   shell_register_command(&start6lbr_command);
+  shell_register_command(&rfchannel_command);
 }
 /*---------------------------------------------------------------------------*/
