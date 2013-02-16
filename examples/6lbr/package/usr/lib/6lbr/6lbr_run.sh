6aa545a0f417c9774c0b949f779292e220fd6510
Laurent Deru <laurent.deru@cetic.be>
2013-02-16 12:26:22 +0000

    Wait if the slip-radio is not detected at startup

--- examples/6lbr/package/usr/lib/6lbr/6lbr_run.sh
+++ examples/6lbr/package/usr/lib/6lbr/6lbr_run.sh
@@ -5,6 +5,12 @@ LOGS=/var/log
 BIN_6LBR=/usr/bin/6lbr
 CONF=/etc/6lbr/6lbr.conf
 
+. $CONF
+
+if [ ! -e $DEV_RADIO ]; then
+	sleep 10
+fi
+
 $BIN_6LBR $CONF >> $LOGS/6lbr.log 2>>$LOGS/6lbr.err &
 	
 echo $! > $PIDFILE
