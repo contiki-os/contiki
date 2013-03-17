843d2361dcc0247309eac4be430ccc814599ae28
Adam Dunkels <adam@thingsquare.com>
2013-03-17 23:53:46 +0100

    Include the rimestats variable only if rimestats are enabled by the configuration

--- core/net/rime/rimestats.h
+++ core/net/rime/rimestats.h
@@ -55,8 +55,11 @@ struct rimestats {
   unsigned long lltx, llrx;
 };
 
+#if RIMESTATS_CONF_ENABLED
 extern struct rimestats rimestats;
-
 #define RIMESTATS_ADD(x) rimestats.x++
+#else /* RIMESTATS_CONF_ENABLED */
+#define RIMESTATS_ADD(x)
+#endif /* RIMESTATS_CONF_ENABLED */
 
 #endif /* __RIMESTATS_H__ */
