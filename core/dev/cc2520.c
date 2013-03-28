1e7d1cee2529337c0d3dfadc65520d6bd3889b07
Kévin Roussel <Kevin.Roussel@inria.fr>
2013-03-28 13:50:16 +0100

    Implemented radio API extensions for CC2x20 radio drivers. This is not a stub, but an actually useful extension, allowing upper layers of the network stack to switch channels, set emission power and change node addresses in a standardized way.

--- core/dev/cc2520.c
+++ core/dev/cc2520.c
@@ -115,7 +115,6 @@ static int cc2520_cca(void);
 signed char cc2520_last_rssi;
 uint8_t cc2520_last_correlation;
 
-<<<<<<< HEAD
 #if RADIO_CONF_EXTENDED_API
 
 static radio_conf_result_t cc2520_get_config_const(radio_const_t cst_id, void *value);
@@ -123,8 +122,6 @@ static radio_conf_result_t cc2520_set_param(radio_param_t param_id, void *value)
 static radio_conf_result_t cc2520_get_param(radio_param_t param_id, void *value);
 
 #endif /* RADIO_CONF_EXTENDED_API */
-=======
->>>>>>> Refactor CC2x20 radio drivers.
 
 const struct radio_driver cc2520_driver =
   {
