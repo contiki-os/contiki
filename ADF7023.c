a1bc241c0684144a5c37ff1fe0a4efceea905856
Dragos Bogdan <Dragos.Bogdan@Analog.com>
2013-05-09 18:31:37 +0300

    Added the PmodRF3 demo for Renesas RL78G14 board.

---
+++ ADF7023.c
@@ -0,0 +1,284 @@
+/***************************************************************************//**
+ *   @file   ADF7023.c
+ *   @brief  Implementation of ADF7023 Driver.
+ *   @author DBogdan (Dragos.Bogdan@analog.com)
+********************************************************************************
+ * Copyright 2013(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "ADF7023.h"
+#include "ADF7023_Config.h"
+#include "Communication.h"
+
+/******************************************************************************/
+/*************************** Macros Definitions *******************************/
+/******************************************************************************/
+#define ADF7023_CS_ASSERT   CS_PIN_LOW
+#define ADF7023_CS_DEASSERT CS_PIN_HIGH
+#define ADF7023_MISO        MISO_PIN
+
+/******************************************************************************/
+/************************ Variables Definitions *******************************/
+/******************************************************************************/
+struct ADF7023_BBRAM ADF7023_BBRAMCurrent;
+
+/***************************************************************************//**
+ * @brief Transfers one byte of data.
+ *
+ * @param writeByte - Write data.
+ * @param readByte - Read data.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_WriteReadByte(unsigned char writeByte,
+                           unsigned char* readByte)
+{
+    unsigned char data = 0;
+    
+    data = writeByte;
+    SPI_Read(0, &data, 1);
+    if(readByte)
+    {
+        *readByte = data;
+    }
+}
+
+/***************************************************************************//**
+ * @brief Initializes the ADF7023.
+ *
+ * @return retVal - Result of the initialization procedure.
+ *                  Example: 0 - if initialization was successful;
+ *                           -1 - if initialization was unsuccessful.
+*******************************************************************************/
+char ADF7023_Init(void)
+{
+    char           retVal  = 0;
+    unsigned char  miso    = 0;
+    unsigned short timeout = 0;
+    unsigned char  status  = 0;
+    
+    ADF7023_BBRAMCurrent = ADF7023_BBRAMDefault;
+    SPI_Init(0,         // MSB first.
+             1000000,   // Clock frequency.
+             0,         // Idle state for clock is a high level; active state is a low level.
+             1);        // Serial output data changes on transition from idle clock state to active clock state.
+    ADF7023_CS_ASSERT;
+    while ((miso == 0) && (timeout < 1000))
+    {
+        miso = ADF7023_MISO;
+        timeout++;
+    }
+    if(timeout == 1000)
+    {
+        retVal = -1;
+    }
+    while(!(status & STATUS_CMD_READY))
+    {
+        ADF7023_GetStatus(&status);
+    }
+    ADF7023_SetRAM(0x100, 64, (unsigned char*)&ADF7023_BBRAMCurrent);
+    ADF7023_SetCommand(CMD_CONFIG_DEV);
+    
+    return retVal;
+}
+
+/***************************************************************************//**
+ * @brief Reads the status word of the ADF7023.
+ *
+ * @param status - Status word.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_GetStatus(unsigned char* status)
+{
+    ADF7023_CS_ASSERT;
+    ADF7023_WriteReadByte(SPI_NOP, 0);
+    ADF7023_WriteReadByte(SPI_NOP, status);
+    ADF7023_CS_DEASSERT;
+}
+
+/***************************************************************************//**
+ * @brief Initiates a command.
+ *
+ * @param command - Command.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_SetCommand(unsigned char command)
+{
+    ADF7023_CS_ASSERT;
+    ADF7023_WriteReadByte(command, 0);
+    ADF7023_CS_DEASSERT;
+}
+
+/***************************************************************************//**
+ * @brief Sets a FW state and waits until the device enters in that state.
+ *
+ * @param fwState - FW state.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_SetFwState(unsigned char fwState)
+{
+    unsigned char status = 0;
+    
+    switch(fwState)
+    {
+        case FW_STATE_PHY_OFF:
+            ADF7023_SetCommand(CMD_PHY_OFF);
+            break;
+        case FW_STATE_PHY_ON:
+            ADF7023_SetCommand(CMD_PHY_ON);
+            break;
+        case FW_STATE_PHY_RX:
+            ADF7023_SetCommand(CMD_PHY_RX);
+            break;
+        case FW_STATE_PHY_TX:
+            ADF7023_SetCommand(CMD_PHY_TX);
+            break;
+        default:
+            ADF7023_SetCommand(CMD_PHY_SLEEP);
+    }
+    while((status & STATUS_FW_STATE) != fwState)
+    {
+        ADF7023_GetStatus(&status);
+    }
+}
+
+/***************************************************************************//**
+ * @brief Reads data from the RAM.
+ *
+ * @param address - Start address.
+ * @param length - Number of bytes to write.
+ * @param data - Read buffer.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_GetRAM(unsigned long address,
+                    unsigned long length,
+                    unsigned char* data)
+{
+    ADF7023_CS_ASSERT;
+    ADF7023_WriteReadByte(SPI_MEM_RD | ((address & 0x700) >> 8), 0);
+    ADF7023_WriteReadByte(address & 0xFF, 0);
+    ADF7023_WriteReadByte(SPI_NOP, 0);
+    while(length--)
+    {
+        ADF7023_WriteReadByte(SPI_NOP, data++);
+    }
+    ADF7023_CS_DEASSERT;
+}
+
+/***************************************************************************//**
+ * @brief Writes data to RAM.
+ *
+ * @param address - Start address.
+ * @param length - Number of bytes to write.
+ * @param data - Write buffer.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_SetRAM(unsigned long address,
+                    unsigned long length,
+                    unsigned char* data)
+{
+    ADF7023_CS_ASSERT;
+    ADF7023_WriteReadByte(SPI_MEM_WR | ((address & 0x700) >> 8), 0);
+    ADF7023_WriteReadByte(address & 0xFF, 0);
+    while(length--)
+    {
+        ADF7023_WriteReadByte(*(data++), 0);
+    }
+    ADF7023_CS_DEASSERT;
+}
+
+/***************************************************************************//**
+ * @brief Receives one packet.
+ *
+ * @param packet - Data buffer.
+ * @param length - Number of received bytes.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_ReceivePacket(unsigned char* packet, unsigned char* length)
+{
+    unsigned char interruptReg = 0;
+    
+    ADF7023_SetFwState(FW_STATE_PHY_ON);
+    ADF7023_SetFwState(FW_STATE_PHY_RX);
+    while(!(interruptReg & BBRAM_INTERRUPT_MASK_0_INTERRUPT_CRC_CORRECT))
+    {
+        ADF7023_GetRAM(MCR_REG_INTERRUPT_SOURCE_0,
+                       0x1,
+                       &interruptReg);
+    }
+    ADF7023_SetRAM(MCR_REG_INTERRUPT_SOURCE_0,
+                   0x1,
+                   &interruptReg);
+    ADF7023_GetRAM(0x10, 1, length);
+    ADF7023_GetRAM(0x12, *length - 2, packet);
+}
+
+/***************************************************************************//**
+ * @brief Transmits one packet.
+ *
+ * @param packet - Data buffer.
+ * @param length - Number of bytes to transmit.
+ *
+ * @return None.
+*******************************************************************************/
+void ADF7023_TransmitPacket(unsigned char* packet, unsigned char length)
+{
+    unsigned char interruptReg = 0;
+    unsigned char header[2]    = {0, 0};
+    
+    header[0] = 2 + length;
+    header[1] = ADF7023_BBRAMCurrent.addressMatchOffset;
+    ADF7023_SetRAM(0x10, 2, header);
+    ADF7023_SetRAM(0x12, length, packet);
+    ADF7023_SetFwState(FW_STATE_PHY_ON);
+    ADF7023_SetFwState(FW_STATE_PHY_TX);
+    while(!(interruptReg & BBRAM_INTERRUPT_MASK_0_INTERRUPT_TX_EOF))
+    {
+        ADF7023_GetRAM(MCR_REG_INTERRUPT_SOURCE_0,
+                       0x1,
+                       &interruptReg);
+    }
+}
---
+++ ADF7023_Config.h
@@ -0,0 +1,191 @@
+/***************************************************************************//**
+ *   @file   ADF7023_Config.h
+ *   @brief  Configuration file of ADF7023 Driver.
+ *   @author DBogdan (Dragos.Bogdan@analog.com)
+********************************************************************************
+ * Copyright 2013(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+#ifndef __ADF7023_CONFIG_H__
+#define __ADF7023_CONFIG_H__
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "ADF7023.h"
+
+/******************************************************************************/
+/************************* Variables Declarations *****************************/
+/******************************************************************************/
+struct ADF7023_BBRAM ADF7023_BBRAMDefault =
+{
+    /* interruptMask0 - 0x100 */
+    BBRAM_INTERRUPT_MASK_0_INTERRUPT_TX_EOF |
+    BBRAM_INTERRUPT_MASK_0_INTERRUPT_CRC_CORRECT,
+    /* interruptMask1 - 0x101 */
+    0x00,
+    /* numberOfWakeups0 - 0x102 */
+    0x00,
+    /* numberOfWakeups1 - 0x103 */
+    0x00,
+    /* numberOfWakeupsIrqThreshold0 - 0x104 */
+    0xFF,
+    /* numberOfWakeupsIrqThreshold1 - 0x105 */
+    0xFF,
+    /* rxDwellTime - 0x106 */
+    0x00,
+    /* parmtimeDivider - 0x107 */
+    0x33,
+    /* swmRssiThresh - 0x108 */
+    0x31,
+    /* channelFreq0 - 0x109 */
+    0x3B,
+    /* channelFreq1 - 0x10A */
+    0x31,
+    /* channelFreq2 - 0x10B */
+    0x23,
+    /* radioCfg0 - 0x10C */
+    BBRAM_RADIO_CFG_0_DATA_RATE_7_0(0xF4),
+    /* radioCfg1 - 0x10D */
+    BBRAM_RADIO_CFG_1_FREQ_DEVIATION_11_8(1) |
+    BBRAM_RADIO_CFG_1_DATA_RATE_11_8(1),
+    /* radioCfg2 - 0x10E */
+    BBRAM_RADIO_CFG_2_FREQ_DEVIATION_7_0(0xF4),
+    /* radioCfg3 - 0x10F */
+    0x31,
+    /* radioCfg4 - 0x110 */
+    0x16,
+    /* radioCfg5 - 0x111 */
+    0x00,
+    /* radioCfg6 - 0x112 */
+    BBRAM_RADIO_CFG_6_DISCRIM_PHASE(0x2),
+    /* radioCfg7 - 0x113 */
+    BBRAM_RADIO_CFG_7_AGC_LOCK_MODE(3),
+    /* radioCfg8 - 0x114 */
+    BBRAM_RADIO_CFG_8_PA_SINGLE_DIFF_SEL |
+    BBRAM_RADIO_CFG_8_PA_LEVEL(0xF) |
+    BBRAM_RADIO_CFG_8_PA_RAMP(1),
+    /* radioCfg9 - 0x115 */
+    BBRAM_RADIO_CFG_9_IFBW(2),
+    /* radioCfg10 - 0x116 */
+    BBRAM_RADIO_CFG_10_AFC_SCHEME(2) |
+    BBRAM_RADIO_CFG_10_AFC_LOCK_MODE(3),
+    /* radioCfg11 - 0x117 */
+    BBRAM_RADIO_CFG_11_AFC_KP(3) |
+    BBRAM_RADIO_CFG_11_AFC_KI(7),
+    /* imageRejectCalPhase - 0x118 */
+    0x00,
+    /* imageRejectCalAmplitude - 0x119 */
+    0x00,
+    /* modeControl - 0x11A */
+    BBRAM_MODE_CONTROL_BB_CAL,
+    /* preambleMatch - 0x11B */
+    0x0C,
+    /* symbolMode - 0x11C */
+    BBRAM_SYMBOL_MODE_MANCHESTER_ENC,
+    /* preambleLen - 0x11D */
+    0x20,
+    /* crcPoly0 - 0x11E */
+    0x00,
+    /* crcPoly1 - 0x11F */
+    0x00,
+    /* syncControl - 0x120 */
+    BBRAM_SYNC_CONTROL_SYNC_WORD_LENGTH(8),
+    /* syncByte0 - 0x121 */
+    0x00,
+    /* syncByte1 - 0x122 */
+    0x00,
+    /* syncByte2 - 0x123 */
+    0x12,
+    /* txBaseAdr - 0x124 */
+    ADF7023_TX_BASE_ADR,
+    /* rxBaseAdr - 0x125 */
+    ADF7023_RX_BASE_ADR,
+    /* packetLengthControl - 0x126 */
+    0x24,
+    /* packetLengthMax - 0x127 */
+    0xF0,
+    /* staticRegFix - 0x128 */
+    0x00,
+    /* addressMatchOffset - 0x129 */
+    0x01,
+    /* addressLength - 0x12A */
+    0x02,
+    /* addressFiltering0 - 0x12B */
+    0x01,
+    /* addressFiltering1 - 0x12C */
+    0xFF,
+    /* addressFiltering2 - 0x12D */
+    0xFF,
+    /* addressFiltering3 - 0x12E */
+    0xFF,
+    /* addressFiltering4 - 0x12F */
+    0x02,
+    /* addressFiltering5 - 0x130 */
+    0x0F,
+    /* addressFiltering6 - 0x131 */
+    0xFF,
+    /* addressFiltering7 - 0x132 */
+    0x0F,
+    /* addressFiltering8 - 0x133 */
+    0xFF,
+    /* addressFiltering9 - 0x134 */
+    0x00,
+    /* addressFiltering10 - 0x135 */
+    0x00,
+    /* addressFiltering11 - 0x136 */
+    0x00,
+    /* addressFiltering12 - 0x137 */
+    0x00,
+    /* rssiWaitTime - 0x138 */
+    0x00,
+    /* testmodes - 0x139 */
+    0x00,
+    /* transitionClockDiv - 0x13A */
+    0x00,
+    /* reserved0 - 0x13B */
+    0x00,
+    /* reserved1 - 0x13C */
+    0x00,
+    /* reserved2 - 0x13D */
+    0x00,
+    /* rxSynthLockTime - 0x13E */
+    0x00,
+    /* txSynthLockTime - 0x13F */
+    0x00,
+};
+
+#endif // __ADF7023_CONFIG_H__
