41dad10fc43b1891eeb3f95b0fb86cd325f73da4
Adam Dunkels <adam@thingsquare.com>
2013-07-27 13:48:54 +0200

    Added regression tests that test IPv6 fragmentation

---
+++ regression-tests/11-ipv6/fragmentation-should-receive-none.js
@@ -0,0 +1,22 @@
+TIMEOUT(200000, log.log("last message: " + msg + "\n"));
+
+data = 0;
+alive = 0;
+while(true) {
+    YIELD();
+    if(msg.startsWith('Data')) {
+        data++;
+        log.log("Heard " + data + " data messages\n");
+    }
+    if(msg.startsWith('Alive')) {
+        alive++;
+        log.log("Heard " + alive + " alive messages\n");
+    }
+    if(data == 0 && alive == 10) {
+        if(msg.startsWith('Alive 10')) {
+            log.testOK();
+        } else {
+            log.testError();
+        }
+    }
+}
