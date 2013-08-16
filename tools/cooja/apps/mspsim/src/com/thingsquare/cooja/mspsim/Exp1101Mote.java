7c125ced9a89bc19a1cbc64473b9e83592ab8c0a
Fredrik Osterlind <fredrik@thingsquare.com>
2013-08-16 12:02:16 +0200

    a set of new cooja-mspsim mote types

---
+++ tools/cooja/apps/mspsim/src/com/thingsquare/cooja/mspsim/Exp1101Mote.java
@@ -0,0 +1,26 @@
+package com.thingsquare.cooja.mspsim;
+
+import java.io.File;
+
+import se.sics.cooja.Simulation;
+import se.sics.cooja.mspmote.Exp5438Mote;
+import se.sics.cooja.mspmote.MspMoteType;
+
+/**
+ * @author Fredrik Osterlind
+ */
+public class Exp1101Mote extends Exp5438Mote {
+  private String desc = "";
+  
+  public Exp1101Mote(MspMoteType moteType, Simulation sim) {
+    super(moteType, sim);
+  }
+
+  protected boolean initEmulator(File fileELF) {
+    return super.initEmulator(fileELF);
+  }
+
+  public String toString() {
+    return desc + " " + getID();
+  }
+}
---
+++ tools/cooja/apps/mspsim/src/com/thingsquare/cooja/mspsim/Trxeb1120MoteType.java
@@ -0,0 +1,169 @@
+package com.thingsquare.cooja.mspsim;
+
+import java.awt.Container;
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
+import se.sics.cooja.AbstractionLevelDescription;
+import se.sics.cooja.ClassDescription;
+import se.sics.cooja.MoteInterface;
+import se.sics.cooja.MoteType;
+import se.sics.cooja.Simulation;
+import se.sics.cooja.dialogs.CompileContiki;
+import se.sics.cooja.dialogs.MessageList;
+import se.sics.cooja.dialogs.MessageList.MessageContainer;
+import se.sics.cooja.interfaces.IPAddress;
+import se.sics.cooja.interfaces.Mote2MoteRelations;
+import se.sics.cooja.interfaces.MoteAttributes;
+import se.sics.cooja.interfaces.Position;
+import se.sics.cooja.interfaces.RimeAddress;
+import se.sics.cooja.mspmote.Exp5438MoteType;
+import se.sics.cooja.mspmote.MspCompileDialog;
+import se.sics.cooja.mspmote.interfaces.Msp802154Radio;
+import se.sics.cooja.mspmote.interfaces.MspClock;
+import se.sics.cooja.mspmote.interfaces.MspDebugOutput;
+import se.sics.cooja.mspmote.interfaces.MspMoteID;
+import se.sics.cooja.mspmote.interfaces.UsciA1Serial;
+
+@ClassDescription("Trxeb1120")
+@AbstractionLevelDescription("Emulated level")
+public class Trxeb1120MoteType extends Exp5438MoteType {
+  private static Logger logger = Logger.getLogger(Trxeb1120MoteType.class);
+
+  public boolean configureAndInit(Container parentContainer, Simulation simulation, boolean visAvailable)
+  throws MoteTypeCreationException {
+
+    /* If visualized, show compile dialog and let user configure */
+    if (visAvailable) {
+
+      /* Create unique identifier */
+      if (getIdentifier() == null) {
+        int counter = 0;
+        boolean identifierOK = false;
+        while (!identifierOK) {
+          identifierOK = true;
+
+          counter++;
+          setIdentifier("trxeb1120#" + counter);
+
+          for (MoteType existingMoteType : simulation.getMoteTypes()) {
+            if (existingMoteType == this) {
+              continue;
+            }
+            if (existingMoteType.getIdentifier().equals(getIdentifier())) {
+              identifierOK = false;
+              break;
+            }
+          }
+        }
+      }
+
+      /* Create initial description */
+      if (getDescription() == null) {
+        setDescription("Trxeb1120 Mote Type " + getIdentifier());
+      }
+
+      return MspCompileDialog.showDialog(parentContainer, simulation, this, "trxeb1120");
+    }
+
+    /* Not visualized: Compile Contiki immediately */
+    if (getIdentifier() == null) {
+      throw new MoteTypeCreationException("No identifier");
+    }
+
+    final MessageList compilationOutput = new MessageList();
+
+    if (getCompileCommands() != null) {
+      /* Handle multiple compilation commands one by one */
+      String[] arr = getCompileCommands().split("\n");
+      for (String cmd: arr) {
+        if (cmd.trim().isEmpty()) {
+          continue;
+        }
+
+        try {
+          CompileContiki.compile(
+              cmd,
+              null,
+              null /* Do not observe output firmware file */,
+              getContikiSourceFile().getParentFile(),
+              null,
+              null,
+              compilationOutput,
+              true
+          );
+        } catch (Exception e) {
+          MoteTypeCreationException newException =
+            new MoteTypeCreationException("Mote type creation failed: " + e.getMessage());
+          newException = (MoteTypeCreationException) newException.initCause(e);
+          newException.setCompilationOutput(compilationOutput);
+
+          /* Print last 10 compilation errors to console */
+          MessageContainer[] messages = compilationOutput.getMessages();
+          for (int i=messages.length-10; i < messages.length; i++) {
+            if (i < 0) {
+              continue;
+            }
+            logger.fatal(">> " + messages[i]);
+          }
+
+          logger.fatal("Compilation error: " + e.getMessage());
+          throw newException;
+        }
+      }
+    }
+
+    if (getContikiFirmwareFile() == null ||
+        !getContikiFirmwareFile().exists()) {
+      throw new MoteTypeCreationException("Contiki firmware file does not exist: " + getContikiFirmwareFile());
+    }
+    return true;
+  }
+
+  public Class<? extends MoteInterface>[] getDefaultMoteInterfaceClasses() {
+	    return new Class[] {
+	            Position.class,
+	            RimeAddress.class,
+	            IPAddress.class,
+	            Mote2MoteRelations.class,
+	            MoteAttributes.class,
+	            MspClock.class,
+	            MspMoteID.class,
+	            CC1120Radio.class,
+	            UsciA1Serial.class,
+	            TrxebLEDs.class,
+	            /*Exp5438LCD.class,*/ /* TODO */
+	            MspDebugOutput.class
+	        };
+  }
+  public Class<? extends MoteInterface>[] getAllMoteInterfaceClasses() {
+    return new Class[] {
+        Position.class,
+        RimeAddress.class,
+        IPAddress.class,
+        Mote2MoteRelations.class,
+        MoteAttributes.class,
+        MspClock.class,
+        MspMoteID.class,
+        Msp802154Radio.class,
+        CC1120Radio.class,
+        UsciA1Serial.class,
+        TrxebLEDs.class,
+        /*Exp5438LCD.class,*/ /* TODO */
+        MspDebugOutput.class
+    };
+  }
+
+  public File getExpectedFirmwareFile(File source) {
+    File parentDir = source.getParentFile();
+    String sourceNoExtension = source.getName().substring(0, source.getName().length()-2);
+
+    return new File(parentDir, sourceNoExtension + ".trxeb1120");
+  }
+
+  protected String getTargetName() {
+  	return "trxeb1120";
+  }
+
+}
