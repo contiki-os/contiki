496d7c52c6099cba44fa4b9a3c599006e943b6bf
Ari Suutari <ari@stonepile.fi>
2013-09-05 20:37:01 +0300

    Fix UDP statistics.

--- core/net/uip.c
+++ core/net/uip.c
@@ -1136,6 +1136,7 @@ uip_process(uint8_t flag)
     }
   }
   UIP_LOG("udp: no matching connection found");
+  UIP_STAT(++uip_stat.udp.drop);
 #if UIP_CONF_ICMP_DEST_UNREACH && !UIP_CONF_IPV6
   /* Copy fields from packet header into payload of this ICMP packet. */
   memcpy(&(ICMPBUF->payload[0]), ICMPBUF, UIP_IPH_LEN + 8);
@@ -1169,6 +1170,7 @@ uip_process(uint8_t flag)
 #endif /* UIP_CONF_ICMP_DEST_UNREACH */
   
  udp_found:
+  UIP_STAT(++uip_stat.udp.recv);
   uip_conn = NULL;
   uip_flags = UIP_NEWDATA;
   uip_sappdata = uip_appdata = &uip_buf[UIP_LLH_LEN + UIP_IPUDPH_LEN];
@@ -1213,6 +1215,7 @@ uip_process(uint8_t flag)
   }
 #endif /* UIP_UDP_CHECKSUMS */
   
+  UIP_STAT(++uip_stat.udp.sent);
   goto ip_send_nolen;
 #endif /* UIP_UDP */
   
