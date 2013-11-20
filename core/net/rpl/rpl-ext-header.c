b0460317b889361845e10f0116c1f9e2557d9f0f
Adam Dunkels <adam@thingsquare.com>
2013-11-20 15:14:11 +0100

    Moved variable declaration to the start of the function block

--- core/net/rpl/rpl-ext-header.c
+++ core/net/rpl/rpl-ext-header.c
@@ -69,6 +69,7 @@ rpl_verify_header(int uip_ext_opt_offset)
   rpl_instance_t *instance;
   int down;
   uint8_t sender_closer;
+  uip_ds6_route_t *route;
 
   if(UIP_EXT_HDR_OPT_RPL_BUF->opt_len != RPL_HDR_OPT_LEN) {
     PRINTF("RPL: Bad header option! (wrong length)\n");
@@ -88,7 +89,6 @@ rpl_verify_header(int uip_ext_opt_offset)
        the packet to be forwareded in the first place. We drop any
        routes that go through the neighbor that sent the packet to
        us. */
-    uip_ds6_route_t *route;
     route = uip_ds6_route_lookup(&UIP_IP_BUF->destipaddr);
     if(route != NULL) {
       uip_ds6_route_rm(route);
