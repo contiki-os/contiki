662eb4db1432407c88cb3f8d8ffe1b315e3cb562
Martin Heusse <martin.heusse@imag.fr>
2014-01-12 10:29:31 +0100

    Fix compilation error when DEBUG enabled in core/net/mac/framer-nullmac.c

--- core/net/mac/framer-nullmac.c
+++ core/net/mac/framer-nullmac.c
@@ -67,7 +67,7 @@ create(void)
     rimeaddr_copy(&(hdr->receiver), packetbuf_addr(PACKETBUF_ADDR_RECEIVER));
     return sizeof(struct nullmac_hdr);
   }
-  PRINTF("PNULLMAC-UT: too large header: %u\n", len);
+  PRINTF("PNULLMAC-UT: too large header: %u\n", sizeof(struct nullmac_hdr));
   return FRAMER_FAILED;
 }
 /*---------------------------------------------------------------------------*/
@@ -83,7 +83,7 @@ parse(void)
     PRINTF("PNULLMAC-IN: ");
     PRINTADDR(packetbuf_addr(PACKETBUF_ADDR_SENDER));
     PRINTADDR(packetbuf_addr(PACKETBUF_ADDR_RECEIVER));
-    PRINTF("%u (%u)\n", packetbuf_datalen(), len);
+    PRINTF("%u (%u)\n", packetbuf_datalen(), sizeof(struct nullmac_hdr));
 
     return sizeof(struct nullmac_hdr);
   }
