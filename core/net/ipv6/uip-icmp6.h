tarakanov/contiki-sensinode

Define a new ICMPv6 message type for Trickle Multicast
Pending IANA allocation, we currently use private experimentation

core/net/uip-icmp6.h
@@ -65,6 +65,11 @@
 #define ICMP6_REDIRECT                  137  /**< Redirect */
 
 #define ICMP6_RPL                       155  /**< RPL */
+#define ICMP6_PRIV_EXP_100              100  /**< Private Experimentation */
+#define ICMP6_PRIV_EXP_101              101  /**< Private Experimentation */
+#define ICMP6_PRIV_EXP_200              200  /**< Private Experimentation */
+#define ICMP6_PRIV_EXP_201              201  /**< Private Experimentation */
+#define ICMP6_TRICKLE_MCAST ICMP6_PRIV_EXP_200 /**< ROLL Trickle Multicast */
 /** @} */
 
 

contiki-os/contiki

core/net/ipv6/uip-icmp6.h
@@ -65,6 +65,11 @@
 #define ICMP6_REDIRECT                  137  /**< Redirect */
 
 #define ICMP6_RPL                       155  /**< RPL */
+#define ICMP6_PRIV_EXP_100              100  /**< Private Experimentation */
+#define ICMP6_PRIV_EXP_101              101  /**< Private Experimentation */
+#define ICMP6_PRIV_EXP_200              200  /**< Private Experimentation */
+#define ICMP6_PRIV_EXP_201              201  /**< Private Experimentation */
+#define ICMP6_ROLL_TM    ICMP6_PRIV_EXP_200  /**< ROLL Trickle Multicast */
 /** @} */
 
 