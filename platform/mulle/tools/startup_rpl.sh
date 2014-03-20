af72b9cf90ffd18b458ad5f38fd4a1536f403c6b
Henrik Mäkitaavola <henrik.makitaavola@gmail.com>
2014-03-20 13:25:23 +0100

    platform/mulle: Added prefix from commandline for rpl startup script.

--- platform/mulle/tools/startup_rpl.sh
+++ platform/mulle/tools/startup_rpl.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo "Usage: sudo `basename $0` <serial interface>"
+  echo "Usage: sudo `basename $0` <serial interface> <ipv6 prefix>"
+  echo "Example: sudo `basename $0` /dev/ttyUSB0 fdfd:1:2:3"
   exit 65
 fi
 
 sysctl -w net.ipv6.conf.all.forwarding=1
-./../../../tools/tunslip6 -t tun1 -B 115200 -s $1 fdfd::ff/64
+./../../../tools/tunslip6 -t tun1 -B 115200 -s $1 $2::ff/64
