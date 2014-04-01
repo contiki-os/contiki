5179ab3909e13054a7250743a18d1605f0ccf378
Joakim Gebart <joakim.gebart@eistec.se>
2014-04-01 12:37:56 +0200

    cpu/arm/k60: Split UART SBR, BRFA from uart.c
    Defined as macro functions UART_SBR, UART_BRFA in K60.h now.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- cpu/arm/k60/K60.h
+++ cpu/arm/k60/K60.h
@@ -61,3 +61,47 @@
 #else
 #error K60_CPU_REV must be set to the correct CPU revision!
 #endif
+
+/*
+ * Baud rate generator is driven by the System clock (UART0, UART1) or bus
+ * clock (UART2-4) divided by SBR in UARTx_BDL, UARTx_BDH (13 bits)
+ * The receiver needs to sample the input at 16 times the line baud rate.
+ *
+ * From the reference manual:
+ * UART baud rate = UART module clock / (16 * (SBR[12:0] + BRFD))
+ *
+ * So if we want 115200 baud, we need to have a UART clock of (after dividing)
+ * 115200*16=1843200
+ * If we are running a system clock of 96 MHz we will need to divide the clock
+ * by 96000000/1843200=52.083333
+ * We set the clock divisor SBR to 52 and the BRFA fine adjust to 3 (BRFD = 0.09375)
+ * This yields a baud rate of 115176.9646.
+ * Alternatively, we can run at BRFA = 2 yielding a baud rate of 115246.0984
+ */
+/**
+ * UART module SBR parameter based on module frequency f and desired baud rate b.
+ */
+#define UART_SBR(f, b) ((f)/(b * 16))
+
+/*
+ * The constant numbers will be computed compile time by most (all?) compilers.
+ * The suffix ull on 64ull is in order to avoid overflows in the variable in
+ * the compiler when computing the number. Without ull suffix the number will
+ * be truncated to a 32 bit integer before the division yielding the wrong
+ * fine adjust value.
+ */
+/*
+ * The below calculation will yield a fine adjust value rounded to the nearest
+ * configurable fraction.
+ */
+/**
+ * UART module fine adjust parameter based on module frequency f and desired baud rate b.
+ */
+/**
+ * \todo Verify proper rounding on UART1 fine adjust calculation
+ *
+ * \todo Verify the UART1 fine-adjust calculations if F_SYS*32 > 2^32 <=> F_SYS > 2^27 (== 134217728)
+ */
+#define UART_BRFA(f, b) ((((64ull * (f))/((b) * 16)+1)/2) % 32)
+
+
--- cpu/arm/k60/drivers/uart.c
+++ cpu/arm/k60/drivers/uart.c
@@ -21,38 +21,9 @@ void uart_init(void)
   /* SIM_SCGC4 */
   SIM_SCGC4 |= SIM_SCGC4_UART1_MASK;
 
-  /*
-   * Baud rate generator is driven by the System clock (UART0, UART1) or bus
-   * clock (UART2-4) divided by SBR in UARTx_BDL, UARTx_BDH (13 bits)
-   * The receiver needs to sample the input at 16 times the line baud rate.
-   *
-   * From the reference manual: UART baud rate = UART module clock / (16 * (SBR[12:0] + BRFD))
-   *
-   * So if we want 115200 baud, we need to have a UART clock of (after dividing)
-   * 115200*16=1843200
-   * If we are running a system clock of 96 MHz we will need to divide the clock
-   * by 96000000/1843200=52.083333
-   * We set the clock divisor SBR to 52 and the BRFA fine adjust to 3 (BRFD = 0.09375)
-   * This yields a baud rate of 115176.9646.
-   * Alternatively, we can run at BRFA = 2 yielding a baud rate of 115246.0984
-   */
-  UART1_BDH    = UART_BDH_SBR(((F_SYS)/(K60_DEBUG_BAUD * 16)) / 256);
-  UART1_BDL    = UART_BDL_SBR(((F_SYS)/(K60_DEBUG_BAUD * 16)) % 256);
-  /**
-   * \todo Verify the UART1 fine-adjust calculations if F_SYS*32 > 2^32 <=> F_SYS > 2^27 (== 134217728)
-   */
-  /*
-   * The below calculation will yield a fine adjust value rounded to the nearest
-   * configurable fraction.
-   */
-  /** \todo Verify proper rounding on UART1 fine adjust calculation */
-  /*
-   * The number will be computed compile time by most (all?) compilers.
-   * The suffix ull on 64ull is in order to avoid overflows in the variable in
-   * the compiler when computing the number. Without ull suffix the number will
-   * be truncated to a 32 bit integer before the division yielding the wrong
-   * fine adjust value. */
-  UART1_C4     = UART_C4_BRFA((((64ull * (F_SYS))/((K60_DEBUG_BAUD) * 16)+1)/2) % 32);
+  UART1_BDH    = UART_BDH_SBR(UART_SBR(F_SYS, K60_DEBUG_BAUD) / 256);
+  UART1_BDL    = UART_BDL_SBR(UART_SBR(F_SYS, K60_DEBUG_BAUD) % 256);
+  UART1_C4     = UART_C4_BRFA(UART_BRFA(F_SYS, K60_DEBUG_BAUD));
   /* Enable transmitter and receiver and enable receive interrupt */
   UART1_C2     = UART_C2_TE_MASK | UART_C2_RE_MASK | UART_C2_RIE_MASK;
 }
