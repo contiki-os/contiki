16a286212cdeb2825bca97d58d1ccc37ec935a35
Joakim Gebart <joakim.gebart@eistec.se>
2014-05-12 17:20:15 +0200

    platform/mulle: Add empty compatibility uart1.h
    This file exists simply to make some broken examples (rpl-border-router
    etc.) that are dependent on platform specific headers compile correctly.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

---
+++ platform/mulle/dev/uart1.h
@@ -0,0 +1,9 @@
+/*
+  * This file exists simply to make some broken examples (rpl-border-router
+  * etc.) that are dependent on platform specific headers compile correctly.
+ */
+
+#ifndef DEV_UART1_H
+#define DEV_UART1_H
+
+#endif
