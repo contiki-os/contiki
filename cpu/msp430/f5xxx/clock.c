fe30192f4deb7671be9fe685e115f9fac7e5cfae
Atis Elsts <atis.elsts@gmail.com>
2014-08-21 18:09:54 +0200

    Fix time accounting on msp430 Series 5 MCU based platforms (wismote)
    See https://github.com/contiki-os/contiki/pull/727 for explanation

--- cpu/msp430/f5xxx/clock.c
+++ cpu/msp430/f5xxx/clock.c
@@ -41,11 +41,25 @@
 
 #define MAX_TICKS (~((clock_time_t)0) / 2)
 
+#define CLOCK_LT(a, b) ((int16_t)((a)-(b)) < 0)
+
 static volatile unsigned long seconds;
 
 static volatile clock_time_t count = 0;
 /* last_tar is used for calculating clock_fine, last_ccr might be better? */
 static volatile uint16_t last_tar = 0;
+/*---------------------------------------------------------------------------*/
+static inline uint16_t
+read_tar(void)
+{
+  /* Same as clock_counter(), but can be inlined */
+  uint16_t t1, t2;
+  do {
+    t1 = TA1R;
+    t2 = TA1R;
+  } while(t1 != t2);
+  return t1;
+}
 /*---------------------------------------------------------------------------*/
 ISR(TIMER1_A1, timera1)
 {
@@ -59,8 +73,9 @@ ISR(TIMER1_A1, timera1)
      * Occurs when timer state is toggled between STOP and CONT. */
     while(TA1CTL & MC1 && TA1CCR1 - TA1R == 1);
 
+    last_tar = read_tar();
     /* Make sure interrupt time is future */
-    do {
+    while(!CLOCK_LT(last_tar, TA1CCR1)) {
       TA1CCR1 += INTERVAL;
       ++count;
 
@@ -76,9 +91,8 @@ ISR(TIMER1_A1, timera1)
         ++seconds;
         energest_flush();
       }
-    } while((TA1CCR1 - TA1R) > INTERVAL);
-
-    last_tar = TA1R;
+      last_tar = read_tar();
+    }
 
     if(etimer_pending() &&
        (etimer_next_expiration_time() - count - 1) > MAX_TICKS) {
