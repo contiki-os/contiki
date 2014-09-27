c42b23a0344b1001e12213e7f43d44810c82a30b
Joakim Gebart <joakim.gebart@eistec.se>
2014-09-27 14:58:06 +0200

    platform/mulle: Refactor udelay.
     - Fix from da62ba3bd13 for zero us delay by Henrik Mäkitaavola.
     - Added comments for readability.
     - Use bit banding when only setting a single bit.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- platform/mulle/udelay.c
+++ platform/mulle/udelay.c
@@ -11,15 +11,38 @@
 void
 udelay_init(void)
 {
+  /* Enable peripheral clock */
   SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
+  /* Reset logic to a known state */
   PIT->MCR = 0x00;
 }
+
+/**
+ * Microsecond busy wait.
+ *
+ * \param [in] us number of microseconds to sleep.
+ */
 void
 udelay(uint16_t us)
 {
-  PIT->CHANNEL[0].LDVAL = PIT_LDVAL_TSV((F_BUS / 1000000) * (uint32_t)us - 1);
-  PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;
+  /* Don't hang on zero µs sleep. */
+  if (us == 0) return;
+
+  /* Set up timer */
+  PIT->CHANNEL[0].LDVAL = PIT_LDVAL_TSV((F_BUS / 1000000) * (uint32_t)us);
+
+  /* Disable timer to load a new value */
+  BITBAND_REG(PIT->CHANNEL[0].TCTRL, PIT_TCTRL_TEN_SHIFT) = 0;
+
+  /* Clear interrupt flag */
+  BITBAND_REG(PIT->CHANNEL[0].TFLG, PIT_TFLG_TIF_SHIFT) = 1;
+
+  /* Enable timer, no interrupt, no chaining */
   PIT->CHANNEL[0].TCTRL = PIT_TCTRL_TEN_MASK;
+
+  /* Wait for completion */
   while(!(PIT->CHANNEL[0].TFLG & PIT_TFLG_TIF_MASK));
+
+  /* Disable everything */
   PIT->CHANNEL[0].TCTRL = 0x00;
 }
