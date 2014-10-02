831a8477b66baee0e56aecf3dc2a4578508c9376
Joakim Gebart <joakim.gebart@eistec.se>
2014-10-02 12:48:01 +0200

    platform/mulle: Add comments to slip_arch_init.
    Added comments and todo for later reference.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- platform/mulle/dev/slip_arch_uart.c
+++ platform/mulle/dev/slip_arch_uart.c
@@ -8,8 +8,13 @@
 void
 slip_arch_init(unsigned long ubr)
 {
+  /* ubr is the desired baud rate, but the name comes from the msp430 platform
+   * where baud is converted to some platform specific "UBR" parameter. */
+  /** \todo (MULLE) Handle baud rate requests in slip_arch_init */
   uart_set_rx_callback(slip_input_byte);
   uart_enable_rx_interrupt();
+  /* Don't allow LLS since it will disable the UART module clock, which prevents
+   * any incoming bytes from being detected. */
   LLWU_INHIBIT_LLS();
 }
 /*---------------------------------------------------------------------------*/
