6ce74b942c5d8d225cf31b621cd5e72d29f2c0c9
Joakim Gebart <joakim.gebart@eistec.se>
2014-10-16 08:21:16 +0200

    cpu/arm/k60: system_MK60D10: Add license.
    Added license header and minor documentation text changes.
    
    Signed-off-by: Joakim Gebart <joakim.gebart@eistec.se>

--- cpu/arm/k60/include/system_MK60D10.h
+++ cpu/arm/k60/include/system_MK60D10.h
@@ -1,16 +1,46 @@
-
+/*
+ * Copyright (c) 2014, Eistec AB.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This file is part of the Mulle platform port of the Contiki operating system.
+ *
+ */
 
 /**
  * \file
  * \brief Device specific configuration file for MK60D10 (header file)
  *
  * Provides a system configuration function and a global variable that contains
- * the system frequency. It configures the device and initializes the oscillator
- * (PLL) that is part of the microcontroller device.
+ * the system frequency. It configures the device clocks and initializes the
+ * oscillator that is part of the microcontroller device.
  */
 
 #ifndef SYSTEM_MK60D10_H_
-#define SYSTEM_MK60D10_H_                        /**< Symbol preventing repeated inclusion */
+#define SYSTEM_MK60D10_H_
 
 #include <stdint.h>
 
@@ -37,7 +67,7 @@ extern uint32_t SystemSysClock;
 /**
  * \brief Current bus clock frequency
  *
- * MCGOUTCLK divided by OUTDIV2 clocks the bus slaves and peripheral (excluding
+ * MCGOUTCLK divided by OUTDIV2 clocks the bus slaves and peripherals (excluding
  * memories).
  */
 extern uint32_t SystemBusClock;
