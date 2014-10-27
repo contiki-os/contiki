221b9a5c65d225b707dc3ea3c59b85e23c3558b1
jared <jaredestroud@gmail.com>
2014-10-26 23:16:20 -0400

    Insecure use of strcpy allowed for potential buffer overflow vulnerabilities. Several of these changes have been replaced with the more secure strncpy. Due to the Contiki being the largest IoT platform, security should be a primary concerned.

--- apps/webbrowser/www.c
+++ apps/webbrowser/www.c
@@ -342,7 +342,7 @@ open_url(void)
   /* XXX: Here we should find the port part of the URL, but this isn't
      currently done because of laziness from the programmer's side
      :-) */
-  
+
   /* Find file part of the URL. */
   while(*urlptr != '/' && *urlptr != 0) {
     ++urlptr;
@@ -609,15 +609,15 @@ set_url(char *host, uint16_t port, char *file)
   char *urlptr;
 
   memset(url, 0, WWW_CONF_MAX_URLLEN);
-  
+
   if(strncmp(file, http_http, 7) == 0) {
     strncpy(url, file, sizeof(url));
   } else {
     strncpy(url, http_http, 7);
     urlptr = url + 7;
-    strcpy(urlptr, host);
+    strncpy(urlptr, host, sizeof(urlptr));
     urlptr += strlen(host);
-    strcpy(urlptr, file);
+    strncpy(urlptr, file, sizeof(urlptr));
   }
 
   show_url();
@@ -698,12 +698,15 @@ webclient_datahandler(char *data, uint16_t len)
 #if CTK_CONF_WINDOWS
       ctk_dialog_open(&wgetdialog);
 #else /* CTK_CONF_WINDOWS */
-      strcpy(webpage + WWW_CONF_WEBPAGE_WIDTH * 5,
-	     (80 - WWW_CONF_WEBPAGE_WIDTH) / 2 +
-	     "                       This web page cannot be displayed.");
-      strcpy(webpage + WWW_CONF_WEBPAGE_WIDTH * 6,
-	     (80 - WWW_CONF_WEBPAGE_WIDTH) / 2 +
-	     "                       Would you like to download instead?");
+      strncpy(webpage + WWW_CONF_WEBPAGE_WIDTH * 5,
+	     			 (80 - WWW_CONF_WEBPAGE_WIDTH) / 2 + "\nThis web page cannot be displayed.",
+			 			 sizeof(webpage));
+
+      strncpy(webpage + WWW_CONF_WEBPAGE_WIDTH * 6,
+	     			 (80 - WWW_CONF_WEBPAGE_WIDTH) / 2 +
+	     			 "Would you like to download instead?",
+							sizeof(webpage));
+
       CTK_WIDGET_ADD(&mainwindow, &wgetnobutton);
       CTK_WIDGET_ADD(&mainwindow, &wgetyesbutton);
       redraw_window();
@@ -933,7 +936,7 @@ add_query(char delimiter, char *string)
   }
 
   *query++ = delimiter;
-  strcpy(query, string);
+  strncpy(query, string, sizeof(query));
   if(delimiter == ISO_eq) {
     char *space = query;
 
