74f6ae3751a49ee2bf513db7196bdb180855058f
Simon Duquennoy <simonduq@sics.se>
2014-12-01 21:04:25 +0100

    slip-radio: remove unneeded definition of NETSTACK_CONF_WITH_RPL (now set from makefile with CONTIKI_WITH_RPL)

--- examples/ipv6/slip-radio/project-conf.h
+++ examples/ipv6/slip-radio/project-conf.h
@@ -39,9 +39,6 @@
 #undef UIP_CONF_ROUTER
 #define UIP_CONF_ROUTER                 0
 
-#undef UIP_CONF_IPV6_RPL
-#define UIP_CONF_IPV6_RPL               0
-
 #define CMD_CONF_OUTPUT slip_radio_cmd_output
 
 /* add the cmd_handler_cc2420 + some sensors if TARGET_SKY */
