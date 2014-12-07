f89aff0e56371d676a364214df220dd14c9784a7
Adam Dunkels <adam@thingsquare.com>
2014-12-07 21:50:30 +0100

    When selecting the best parent for RPL, disregard parents that have the maximum ETX, which typically indicates that they are no longer present as a neighbor

--- core/net/rpl/rpl-dag.c
+++ core/net/rpl/rpl-dag.c
@@ -721,7 +721,8 @@ best_parent(rpl_dag_t *dag)
 
   p = nbr_table_head(rpl_parents);
   while(p != NULL) {
-    if(p->dag != dag || p->rank == INFINITE_RANK) {
+    if(p->dag != dag || p->rank == INFINITE_RANK ||
+       (dag->instance && p->link_metric >= dag->instance->max_rankinc)) {
       /* ignore this neighbor */
     } else if(best == NULL) {
       best = p;
