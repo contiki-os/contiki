3d567aba5b010d84fdb88019b4e20365ef0ed193
Pere Tuset <peretuset@gmail.com>
2015-03-20 23:49:02 +0100

    Added ENC28J60 Ethernet driver.

---
+++ dev/enc28j60/enc28j60.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2012-2013, Thingsquare, http://www.thingsquare.com/.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef ENC28J60_H
+#define ENC28J60_H
+
+void enc28j60_init(uint8_t *mac_addr);
+
+int enc28j60_send(uint8_t *data, uint16_t datalen);
+
+int enc28j60_read(uint8_t *buffer, uint16_t bufsize);
+
+/* ENC28J60 architecture-specific SPI functions that are called by the
+   enc28j60 driver and must be implemented by the platform code */
+
+void enc28j60_arch_spi_init(void);
+uint8_t enc28j60_arch_spi_write(uint8_t data);
+uint8_t enc28j60_arch_spi_read(void);
+void enc28j60_arch_spi_select(void);
+void enc28j60_arch_spi_deselect(void);
+
+
+#endif /* ENC28J60_H */
