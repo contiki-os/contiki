4a85d49a09a6eb7200dc3ebe441848f283633a7a
Adam Dunkels <adam@thingsquare.com>
2015-03-24 14:43:27 +0100

    Include a subplatform-conf.h, which may override configuration parameters set by the cooja/contiki-conf.h

--- platform/cooja/contiki-conf.h
+++ platform/cooja/contiki-conf.h
@@ -33,6 +33,10 @@
 #ifndef CONTIKI_CONF_H_
 #define CONTIKI_CONF_H_
 
+#ifdef INCLUDE_SUBPLATFORM_CONF
+#include "subplatform-conf.h"
+#endif /* INCLUDE_SUBPLATFORM_CONF */
+
 #define PROFILE_CONF_ON 0
 #define ENERGEST_CONF_ON 0
 #define LOG_CONF_ENABLED 1
@@ -135,9 +139,17 @@
 
 #define TCPIP_CONF_ANNOTATE_TRANSMISSIONS 1
 
+#ifndef UIP_CONF_ND6_SEND_RA
 #define UIP_CONF_ND6_SEND_RA		0
+#endif
+
+#ifndef UIP_CONF_ND6_REACHABLE_TIME
 #define UIP_CONF_ND6_REACHABLE_TIME     600000
+#endif
+
+#ifndef UIP_CONF_ND6_RETRANS_TIMER
 #define UIP_CONF_ND6_RETRANS_TIMER      10000
+#endif
 
 #define LINKADDR_CONF_SIZE            8
 #define UIP_CONF_NETIF_MAX_ADDRESSES  3
@@ -149,9 +161,6 @@
 #define UIP_CONF_IPV6_REASSEMBLY        0
 #define UIP_CONF_NETIF_MAX_ADDRESSES    3
 #define UIP_CONF_IP_FORWARD             0
-#ifndef UIP_CONF_BUFFER_SIZE
-#define UIP_CONF_BUFFER_SIZE		240
-#endif
 
 #define SICSLOWPAN_CONF_COMPRESSION             SICSLOWPAN_COMPRESSION_HC06
 #ifndef SICSLOWPAN_CONF_FRAG
@@ -168,7 +177,9 @@
 
 #define PACKETBUF_CONF_ATTRS_INLINE 1
 
+#ifndef QUEUEBUF_CONF_NUM
 #define QUEUEBUF_CONF_NUM 16
+#endif
 
 #define CC_CONF_REGISTER_ARGS          1
 #define CC_CONF_FUNCTION_POINTER_ARGS  1
@@ -202,12 +213,6 @@ typedef unsigned long rtimer_clock_t;
 
 #define UIP_CONF_DHCP_LIGHT
 #define UIP_CONF_LLH_LEN         0
-#ifndef  UIP_CONF_RECEIVE_WINDOW
-#define UIP_CONF_RECEIVE_WINDOW  48
-#endif
-#ifndef  UIP_CONF_TCP_MSS
-#define UIP_CONF_TCP_MSS         48
-#endif
 #define UIP_CONF_MAX_CONNECTIONS 4
 #define UIP_CONF_MAX_LISTENPORTS 8
 #define UIP_CONF_UDP_CONNS       12
@@ -230,10 +235,27 @@ typedef unsigned long rtimer_clock_t;
 
 #define CFS_CONF_OFFSET_TYPE	long
 
+#ifndef UIP_CONF_BUFFER_SIZE
+#define UIP_CONF_BUFFER_SIZE            1600
+#endif
+
+#ifndef UIP_CONF_TCP_MSS
+#define UIP_CONF_TCP_MSS                (UIP_CONF_BUFFER_SIZE - 70)
+#endif
+
+#ifndef UIP_CONF_RECEIVE_WINDOW
+#define UIP_CONF_RECEIVE_WINDOW         (UIP_CONF_BUFFER_SIZE - 70)
+#endif
+
+#define RF_CHANNEL                     26
+#define IEEE802154_CONF_PANID          0xABCD
+#define NETSTACK_RADIO_MAX_PAYLOAD_LEN 125
+
 /* include the project config */
 /* PROJECT_CONF_H might be defined in the project Makefile */
 #ifdef PROJECT_CONF_H
 #include PROJECT_CONF_H
 #endif /* PROJECT_CONF_H */
+
 
 #endif /* CONTIKI_CONF_H_ */
