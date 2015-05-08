053546129146bce4394c7a804e5994e4127b37b3
Jesus Sanchez-Palencia <jesus.sanchez-palencia@intel.com>
2015-05-08 09:34:32 -0300

    x86: Isolate SoC specific cpu_init code
    This commit turns cpu_init() into a SoC-agnostic function by
    removing any SoC specific calls and isolating them into their
    own SoC implementation.
    
    We start this by isolating all IRQs initialization code from
    the legacy-pc target, pic_init() and spurious IRQ7 registration,
    into a new interface: irq_init() from irq.h. Future SoCs will have
    to provide their own implementation of this interface.
    
    This model is to be followed by future initialization code that
    we may need to add and which is not common to all x86 SoCs.

--- cpu/x86/init/common/irq.h
+++ cpu/x86/init/common/irq.h
@@ -28,28 +28,9 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "gdt.h"
-#include "helpers.h"
-#include "idt.h"
-#include "interrupt.h"
-#include "irq.h"
+#ifndef IRQ_H
+#define IRQ_H
 
-static void
-double_fault_handler(struct interrupt_context context)
-{
-  halt();
-}
-/*---------------------------------------------------------------------------*/
-void
-cpu_init(void)
-{
-  gdt_init();
-  idt_init();
+void irq_init(void);
 
-  /* Set an interrupt handler for Double Fault exception. This way, we avoid
-   * the system to triple fault, leaving no trace about what happened.
-   */
-  SET_INTERRUPT_HANDLER(8, 1, double_fault_handler);
-
-  irq_init();
-}
+#endif /* IRQ_H */
--- cpu/x86/init/legacy_pc/irq.c
+++ cpu/x86/init/legacy_pc/irq.c
@@ -28,28 +28,33 @@
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "gdt.h"
-#include "helpers.h"
-#include "idt.h"
+#include "drivers/legacy_pc/pic.h"
 #include "interrupt.h"
 #include "irq.h"
 
+#define IRQ7_INT  PIC_INT(7)
+
 static void
-double_fault_handler(struct interrupt_context context)
+spurious_irq7_handler(void)
 {
-  halt();
+  /*
+   * NOTE: Originally IRQ7 was used for the parallel port interrupts. Nowadays,
+   * though, it is only used if some other IRQ (i.e.: a PCIx interrupt) is
+   * mapped to it. In this case we will have to check the PIC ISR register in
+   * order to confirm this was a real interrupt.
+   *
+   * In case of a spurious interrupt, we should NEVER send an EOI here so the PIC
+   * doesn't trigger the next queued interrupt.
+   */
 }
 /*---------------------------------------------------------------------------*/
 void
-cpu_init(void)
+irq_init(void)
 {
-  gdt_init();
-  idt_init();
+  pic_init();
 
-  /* Set an interrupt handler for Double Fault exception. This way, we avoid
-   * the system to triple fault, leaving no trace about what happened.
+  /* Set a 'fake' handler for the Spurious IRQ7 interrupts.
+   * Refer to http://wiki.osdev.org/PIC .
    */
-  SET_INTERRUPT_HANDLER(8, 1, double_fault_handler);
-
-  irq_init();
+  SET_INTERRUPT_HANDLER(IRQ7_INT, 0, spurious_irq7_handler);
 }
