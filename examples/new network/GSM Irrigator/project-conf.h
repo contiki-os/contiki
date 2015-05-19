41f18344fb5f030ed8aa00b2fb3d290f03febe45
mattonfarm <matt@onfarmsolutions.com>
2015-05-19 15:40:07 +1200

    Add GPS function to GSM device

---
+++ examples/new network/GSM Irrigator/project-conf.h
@@ -0,0 +1,14 @@
+#define NETSTACK_CONF_RDC nullrdc_driver
+#define NETSTACK_CONF_MAC nullmac_driver
+
+#define UART0_CONF_BAUD_RATE 38400
+
+#define UART1_RX_PORT            GPIO_D_NUM
+#define UART1_RX_PIN             4
+#define UART1_TX_PORT            GPIO_D_NUM
+#define UART1_TX_PIN             5
+
+#define UART1_CONF_BAUD_RATE 38400
+#define UART1_CONF_UART 1
+#define DBG_CONF_UART 0
+#define LPM_CONF_ENABLE 0
