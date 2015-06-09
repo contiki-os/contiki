ddb4614cabb5f02df127e144ef9005cdee95e5ed
Justin King-Lacroix <justin.king-lacroix@cs.ox.ac.uk>
2015-06-09 13:51:59 +0100

    fixed minor warning in cc2530-rf driver

--- cpu/cc253x/dev/cc2530-rf.c
+++ cpu/cc253x/dev/cc2530-rf.c
@@ -486,7 +486,7 @@ transmit(unsigned short transmit_len)
 }
 /*---------------------------------------------------------------------------*/
 static int
-send(void *payload, unsigned short payload_len)
+send(const void *payload, unsigned short payload_len)
 {
   prepare(payload, payload_len);
   return transmit(payload_len);
