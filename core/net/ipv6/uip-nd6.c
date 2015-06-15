1784338b2ec69109b19ef237b2463b04e72946aa
Laurent Deru <laurent.deru@cetic.be>
2015-06-15 10:25:58 +0200

    Add uip_clear_buf() macro and replace all instances of uip_len = 0; with it

--- core/net/ipv6/uip-nd6.c
+++ core/net/ipv6/uip-nd6.c
@@ -321,7 +321,7 @@ create_na:
   return;
 
 discard:
-  uip_len = 0;
+  uip_clear_buf();
   return;
 }
 #endif /* UIP_ND6_SEND_NA */
@@ -360,7 +360,7 @@ uip_nd6_ns_output(uip_ipaddr_t * src, uip_ipaddr_t * dest, uip_ipaddr_t * tgt)
     }
     if (uip_is_addr_unspecified(&UIP_IP_BUF->srcipaddr)) {
       PRINTF("Dropping NS due to no suitable source address\n");
-      uip_len = 0;
+      uip_clear_buf();
       return;
     }
     UIP_IP_BUF->len[1] =
@@ -557,7 +557,7 @@ na_input(void)
 #endif /*UIP_CONF_IPV6_QUEUE_PKT */
 
 discard:
-  uip_len = 0;
+  uip_clear_buf();
   return;
 }
 #endif /* UIP_ND6_SEND_NA */
@@ -646,7 +646,7 @@ rs_input(void)
   uip_ds6_send_ra_sollicited();
 
 discard:
-  uip_len = 0;
+  uip_clear_buf();
   return;
 }
 
@@ -1029,7 +1029,7 @@ ra_input(void)
 #endif /*UIP_CONF_IPV6_QUEUE_PKT */
 
 discard:
-  uip_len = 0;
+  uip_clear_buf();
   return;
 }
 #endif /* !UIP_CONF_ROUTER */
