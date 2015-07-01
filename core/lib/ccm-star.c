f56a4dd117147ac625859b47eebf03488900a780
Simon Duquennoy <simonduq@sics.se>
2015-07-01 15:31:51 +0200

    Add missing CCM* files

---
+++ core/lib/ccm-star.c
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2013, Hasso-Plattner-Institut.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         AES_128-based CCM* implementation.
+ * \author
+ *         Original: Konrad Krentz <konrad.krentz@gmail.com>
+ *         Generified version: Justin King-Lacroix <justin.kinglacroix@gmail.com>
+ */
+
+#include "ccm-star.h"
+#include "lib/aes-128.h"
+#include <string.h>
+
+/* see RFC 3610 */
+#define CCM_STAR_AUTH_FLAGS(Adata, M) ((Adata ? (1u << 6) : 0) | (((M - 2u) >> 1) << 3) | 1u)
+#define CCM_STAR_ENCRYPTION_FLAGS     1
+#define CCM_STAR_NONCE_MAX_IV_LENGTH  13
+
+/*---------------------------------------------------------------------------*/
+static void
+set_nonce(uint8_t *nonce,
+    uint8_t flags,
+    const uint8_t *iv, uint8_t iv_len,
+    uint8_t counter)
+{
+  /* 1 byte||          8 bytes        ||    4 bytes    || 1 byte  || 2 bytes */
+  /* flags || extended_source_address || frame_counter || sec_lvl || counter */
+
+  if(iv_len > CCM_STAR_NONCE_MAX_IV_LENGTH)
+    iv_len = CCM_STAR_NONCE_MAX_IV_LENGTH;
+
+  nonce[0] = flags;
+  memcpy(nonce + 1, iv, iv_len);
+  memset(nonce + iv_len + 1, 0, 16 - (1 + 1 + iv_len));
+  nonce[15] = counter;
+}
+/*---------------------------------------------------------------------------*/
+/* XORs the block m[pos] ... m[pos + 15] with K_{counter} */
+static void
+ctr_step(const uint8_t *iv, uint8_t iv_len,
+    uint8_t pos,
+    uint8_t *m,
+    uint8_t *output,
+    uint8_t m_len,
+    uint8_t counter)
+{
+  uint8_t a[AES_128_BLOCK_SIZE];
+  uint8_t i;
+  
+  set_nonce(a, CCM_STAR_ENCRYPTION_FLAGS, iv, iv_len, counter);
+  AES_128.encrypt(a);
+  
+  for(i = 0; (pos + i < m_len) && (i < AES_128_BLOCK_SIZE); i++) {
+    output[pos + i] = m[pos + i] ^ a[i];
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+mic(const uint8_t* m,  uint8_t m_len,
+    const uint8_t* iv, uint8_t iv_len,
+    const uint8_t* a,  uint8_t a_len,
+    uint8_t *result,
+    uint8_t mic_len)
+{
+  uint8_t x[AES_128_BLOCK_SIZE];
+  uint8_t pos;
+  uint8_t i;
+  
+  set_nonce(x,
+      CCM_STAR_AUTH_FLAGS(a_len, mic_len),
+      iv, iv_len,
+      m_len);
+  AES_128.encrypt(x);
+  
+  if(a_len > 0) {
+    x[1] = x[1] ^ a_len;
+    for(i = 2; (i - 2 < a_len) && (i < AES_128_BLOCK_SIZE); i++) {
+      x[i] ^= a[i - 2];
+    }
+    
+    AES_128.encrypt(x);
+    
+    pos = 14;
+    while(pos < a_len) {
+      for(i = 0; (pos + i < a_len) && (i < AES_128_BLOCK_SIZE); i++) {
+        x[i] ^= a[pos + i];
+      }
+      pos += AES_128_BLOCK_SIZE;
+      AES_128.encrypt(x);
+    }
+  }
+  
+  if(m_len > 0) {
+    m = a + a_len;
+    pos = 0;
+    while(pos < m_len) {
+      for(i = 0; (pos + i < m_len) && (i < AES_128_BLOCK_SIZE); i++) {
+        x[i] ^= m[pos + i];
+      }
+      pos += AES_128_BLOCK_SIZE;
+      AES_128.encrypt(x);
+    }
+  }
+  
+  ctr_step(iv, iv_len, 0, x, x, AES_128_BLOCK_SIZE, 0);
+  
+  memcpy(result, x, mic_len);
+}
+/*---------------------------------------------------------------------------*/
+static void
+ctr(uint8_t* m, uint8_t* output, uint8_t m_len, const uint8_t* iv, uint8_t iv_len)
+{
+  uint8_t pos;
+  uint8_t counter;
+
+  pos = 0;
+  counter = 1;
+  while(pos < m_len) {
+    ctr_step(iv, iv_len, pos, m, output, m_len, counter++);
+    pos += AES_128_BLOCK_SIZE;
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void
+ctr_and_mic(const uint8_t* m,  uint8_t* output, uint8_t m_len,
+    const uint8_t* iv, uint8_t iv_len,
+    const uint8_t* a,  uint8_t a_len,
+    uint8_t *result,
+    uint8_t mic_len, int encrypt)
+{
+  if(encrypt) {
+    CCM_STAR.mic(m, m_len,
+          iv, 13,
+          a, a_len,
+          result, mic_len);
+    CCM_STAR.ctr(m, output, m_len, iv, 13);
+  } else {
+    CCM_STAR.ctr(m, output, m_len, iv, 13);
+    CCM_STAR.mic(m, m_len,
+          iv, 13,
+          a, a_len,
+          result, mic_len);
+  }
+}
+/*---------------------------------------------------------------------------*/
+static void set_key(const uint8_t* key) {
+    AES_128.set_key((uint8_t*)key);
+}
+/*---------------------------------------------------------------------------*/
+const struct ccm_star_driver ccm_star_driver = {
+  mic,
+  ctr,
+  ctr_and_mic,
+  set_key
+};
+/*---------------------------------------------------------------------------*/
---
+++ core/lib/ccm-star.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2013, Hasso-Plattner-Institut.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         CCM* header file.
+ * \author
+ *         Original: Konrad Krentz <konrad.krentz@gmail.com>
+ *         Generified version: Justin King-Lacroix <justin.kinglacroix@gmail.com>
+ */
+#ifndef CCM_STAR_H_
+#define CCM_STAR_H_
+
+#include "contiki.h"
+
+#ifdef CCM_STAR_CONF
+#define CCM_STAR CCM_STAR_CONF
+#else /* CCM_STAR_CONF */
+#define CCM_STAR ccm_star_driver
+#endif /* CCM_STAR_CONF */
+
+/**
+ * Structure of CCM* drivers.
+ */
+struct ccm_star_driver {
+  
+   /**
+    * \brief             Generates a MIC over the data supplied.
+    * \param data        The data buffer to read.
+    * \param data_length The data buffer length.
+    * \param iv          The IV to use.
+    * \param iv_length   The IV's length.
+    * \param result      The generated MIC will be put here
+    * \param mic_len     The size of the MIC to be generated. <= 16.
+    */
+  void (* mic)(const uint8_t* data, uint8_t data_length,
+      const uint8_t* iv,   uint8_t iv_len,
+      const uint8_t* add,  uint8_t add_len,
+      uint8_t *result,
+      uint8_t mic_len);
+  
+  /**
+   * \brief XORs the frame in the packetbuf with the key stream.
+   * \param data        The data buffer to read.
+   * \param data_length The data buffer length.
+   * \param iv          The IV to use.
+   * \param iv_length   The IV's length.
+   * \param mic         Output buffer to hold the MIC to be generated.
+   * \param mic_len     The size of the MIC to be generated. <= 16.
+   */
+  void (* ctr)(uint8_t* data, uint8_t* output, uint8_t data_length,
+               const uint8_t* iv,   uint8_t iv_len);
+
+  void (* ctr_and_mic)(const uint8_t* data, uint8_t* output,
+        uint8_t data_length,
+        const uint8_t* iv,   uint8_t iv_len,
+        const uint8_t* add,  uint8_t add_len,
+        uint8_t *result,
+        uint8_t mic_len, int encrypt);
+
+  /**
+   * \brief Sets the key in use. Default implementation calls AES_128.set_key()
+   * \param key The key to use.
+   */
+  void (* set_key)(const uint8_t* key);
+};
+
+extern const struct ccm_star_driver CCM_STAR;
+
+#endif /* CCM_STAR_H_ */
