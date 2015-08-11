3a071d058e765a17c1fc90970ca1b5fa7adacb74
Atis Elsts <atis.elsts@gmail.com>
2015-08-11 13:45:20 +0200

    update the ContikiMAC test app

--- examples/jn516x/contikimac/node.c
+++ examples/jn516x/contikimac/node.c
@@ -35,6 +35,14 @@
 #include "contiki-conf.h"
 #include "net/rime/rime.h"
 
+#if 0
+#define RX_ADDR1 140
+#define RX_ADDR2 228
+#else
+#define RX_ADDR1 7
+#define RX_ADDR2 0
+#endif
+
 #define MAX_RETRANSMISSIONS 4
 
 /*---------------------------------------------------------------------------*/
@@ -44,24 +52,24 @@ AUTOSTART_PROCESSES(&unicast_test_process);
 static void
 recv_runicast(struct runicast_conn *c, const linkaddr_t *from, uint8_t seqno)
 {
-  printf("runicast message received from %d.%d, seqno %d\n",
-	 from->u8[0], from->u8[1], seqno);
+  printf("runicast message received from %d.%d, seqno %d, len %d: '%s'\n",
+         from->u8[0], from->u8[1], seqno, packetbuf_datalen(), (char *)packetbuf_dataptr());
 }
 static void
 sent_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
 {
   printf("runicast message sent to %d.%d, retransmissions %d\n",
-	 to->u8[0], to->u8[1], retransmissions);
+         to->u8[0], to->u8[1], retransmissions);
 }
 static void
 timedout_runicast(struct runicast_conn *c, const linkaddr_t *to, uint8_t retransmissions)
 {
   printf("runicast message timed out when sending to %d.%d, retransmissions %d\n",
-	 to->u8[0], to->u8[1], retransmissions);
+         to->u8[0], to->u8[1], retransmissions);
 }
 static const struct runicast_callbacks runicast_callbacks = {recv_runicast,
-							     sent_runicast,
-							     timedout_runicast};
+                                                             sent_runicast,
+                                                             timedout_runicast};
 static struct runicast_conn runicast;
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(unicast_test_process, ev, data)
@@ -73,10 +81,9 @@ PROCESS_THREAD(unicast_test_process, ev, data)
   runicast_open(&runicast, 144, &runicast_callbacks);
 
   /* Receiver node: do nothing */
-  if(linkaddr_node_addr.u8[0] == 140 &&
-     linkaddr_node_addr.u8[1] == 228) {
+  if(linkaddr_node_addr.u8[0] == RX_ADDR1 &&
+     linkaddr_node_addr.u8[1] == RX_ADDR2) {
     puts("wait forever");
-    PROCESS_WAIT_EVENT_UNTIL(0);
   }
 
   while(1) {
@@ -87,18 +94,25 @@ PROCESS_THREAD(unicast_test_process, ev, data)
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
+    if(linkaddr_node_addr.u8[0] == RX_ADDR1 &&
+        linkaddr_node_addr.u8[1] == RX_ADDR2) {
+      puts("tick...");
+      continue;
+    }
+
     if(!runicast_is_transmitting(&runicast)) {
       linkaddr_t recv = {0};
+      static char buffer[100] = "hello";
 
-      packetbuf_copyfrom("Hello", 5);
-      recv.u8[0] = 140;
-      recv.u8[1] = 228;
+      packetbuf_copyfrom(buffer, sizeof(buffer));
+      recv.u8[0] = RX_ADDR1;
+      recv.u8[1] = RX_ADDR2;
 
       printf("%u.%u: sending runicast to address %u.%u\n",
-	     linkaddr_node_addr.u8[0],
-	     linkaddr_node_addr.u8[1],
-	     recv.u8[0],
-	     recv.u8[1]);
+             linkaddr_node_addr.u8[0],
+             linkaddr_node_addr.u8[1],
+             recv.u8[0],
+             recv.u8[1]);
 
       packetbuf_set_attr(PACKETBUF_ATTR_PACKET_ID, ++seqno);
       runicast_send(&runicast, &recv, MAX_RETRANSMISSIONS);
