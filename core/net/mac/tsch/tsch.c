9315bd2716bf90438a0470116214c44736c31c56
Simon Duquennoy <simonduq@sics.se>
2015-09-09 13:44:09 +0200

    TSCH: fix log message

--- core/net/mac/tsch/tsch.c
+++ core/net/mac/tsch/tsch.c
@@ -221,7 +221,7 @@ keepalive_packet_sent(void *ptr, int status, int transmissions)
 #ifdef TSCH_LINK_NEIGHBOR_CALLBACK
   TSCH_LINK_NEIGHBOR_CALLBACK(packetbuf_addr(PACKETBUF_ADDR_RECEIVER), status, transmissions);
 #endif
-  PRINTF("TSCH: KA sent to %u, st %d %d\n",
+  PRINTF("TSCH: KA sent to %u, st %d-%d\n",
       TSCH_LOG_ID_FROM_LINKADDR(packetbuf_addr(PACKETBUF_ADDR_RECEIVER)), status, transmissions);
   tsch_schedule_keepalive();
 }
