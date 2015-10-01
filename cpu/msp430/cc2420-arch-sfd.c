55767adb87f299091e16aa0f9e83fc15877f3b89
Simon Duquennoy <simonduq@sics.se>
2015-10-01 11:59:38 +0200

    cc2420 and cc2430: removed compiler warning on unused tbiv

--- cpu/msp430/cc2420-arch-sfd.c
+++ cpu/msp430/cc2420-arch-sfd.c
@@ -45,6 +45,7 @@ ISR(TIMERB1, cc2420_timerb1_interrupt)
   ENERGEST_ON(ENERGEST_TYPE_IRQ);
   /* always read TBIV to clear IFG */
   tbiv = TBIV;
+  (void)tbiv;
   if(CC2420_SFD_IS_1) {
     cc2420_sfd_counter++;
     cc2420_sfd_start_time = TBCCR1;
