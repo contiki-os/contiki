01ca2af3f9caf73979bdca4b0a937c384ebe6cce
Billy Kozak <bkozak@scanimetrics.com>
2015-10-02 08:27:06 -0600

    Added the PT_YIELD_ON macro
    Allows the caller to yield from the protothread then execute a
    given statement. Necessarry if the given statement can cause
    re-entry to the proto-thread in any way.

--- core/sys/pt.h
+++ core/sys/pt.h
@@ -296,6 +296,34 @@ struct pt {
   } while(0)
 
 /**
+ * \brief Yield from the current protothread then execute a statement
+ *
+ * This function will yield the protothread, thereby allowing other
+ * processing to take place in the system.
+ *
+ * This macro is intended to be used when there is a possibility that the
+ * given statement will cause recursion into the protothread either directly
+ * or through an ISR. Without this construction, the protothread would not
+ * correctly continue from the yield point.
+ *
+ * Note that proto-threads are never truly reentrent.
+ *
+ * \param pt A pointer to the protothread control structure.
+ * \param stmnt code statement to execute before yielding
+ *
+ * \hideinitializer
+ */
+#define PT_YIELD_ON(pt,stmnt)                         \
+  do{                                                 \
+    PT_YIELD_FLAG = 0;                                \
+    LC_SET((pt)->lc);                                 \
+    if(PT_YIELD_FLAG == 0) {                          \
+      { stmnt ;}                                      \
+      return PT_YIELDED;                              \
+    }                                                 \
+  }while(0)
+
+/**
  * \brief      Yield from the protothread until a condition occurs.
  * \param pt   A pointer to the protothread control structure.
  * \param cond The condition.
