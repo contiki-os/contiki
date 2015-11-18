4c2fd9a03ff99a48887b07c33985d428b9d7669a
Michael LeMay <michael.lemay@intel.com>
2015-11-18 09:19:04 -0800

    x86: Add support for 32-bit build hosts in build_uefi.sh
    This patch adds support for building the UEFI EDK2 tools on 32-bit
    hosts.  It also revises the script to support rebuilding the tools
    when the script is re-invoked after the EDK2 repository has previously
    been downloaded.

--- cpu/x86/uefi/build_uefi.sh
+++ cpu/x86/uefi/build_uefi.sh
@@ -5,10 +5,17 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # This script will always run on its own basepath, no matter where you call it from.
 pushd ${SCRIPT_DIR}
 
+if [ "$(uname -m)" = "x86_64" ]; then
+    export ARCH=X64
+fi
+
 # Download the UEFI tool and library sources:
-git clone --depth=1 https://github.com/tianocore/edk2 || exit
-# This script only supports building the tools on 64-bit hosts:
-export ARCH=X64
+if [ -e edk2 ]; then
+    make -C edk2/BaseTools/Source/C/Common clean
+    make -C edk2/BaseTools/Source/C/GenFw clean
+else
+    git clone --depth=1 https://github.com/tianocore/edk2 || exit
+fi
 # Build common sources required by the GenFw tool:
 make -C edk2/BaseTools/Source/C/Common || exit
 # Build the GenFw tool that is used to generate UEFI binaries:
