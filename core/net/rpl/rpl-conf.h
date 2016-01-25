64679642f283a675cc428cbf93709d5d5004ac5b
Simon Duquennoy <simon.duquennoy@inria.fr>
2016-01-25 13:51:10 +0100

    Saving ROM and RAM

--- core/net/rpl/rpl-conf.h
+++ core/net/rpl/rpl-conf.h
@@ -67,7 +67,7 @@
 #ifdef RPL_CONF_SUPPORTED_OFS
 #define RPL_SUPPORTED_OFS RPL_CONF_SUPPORTED_OFS
 #else /* RPL_CONF_SUPPORTED_OFS */
-#define RPL_SUPPORTED_OFS {&rpl_of0, &rpl_mrhof}
+#define RPL_SUPPORTED_OFS {&rpl_mrhof}
 #endif /* RPL_CONF_SUPPORTED_OFS */
 
 /*
