e25af70906908dbf3158656135f11b5478b43b99
Afonso Oliveira <afonso.oliveira@tecnico.ulisboa.pt>
2016-02-22 20:16:54 +0000

    seed incrementation

--- examples/rpl-mobility/org_simulations.sh
+++ examples/rpl-mobility/org_simulations.sh
@@ -9,14 +9,18 @@ sh script.conf "$@"
 cd $1
 increment=${16}
 
+echo "Partial:" > ../results/$1.log
+
 while [ $i -lt $max ]
 do
 cooja $1.csc
 sh ../traffic_volume.sh
 sh ../used_time.sh
+increment=$(($increment+10))
 eval `echo rename "'s/log_([A-Za-z0-9]*).log/log_"'$1'"_$i.log/'" *.log`
 eval `echo rename "'s/([A-Za-z]*).pcap/"'$1'"_$i.pcap/'" *.pcap`
-sed -e "s/<generation_seed>.*<\/generation_seed>/<generation_seed>$(($increment+10))<\/generation_seed>/g" -i ${1}.csc
+sed -e "s/<generation_seed>.*<\/generation_seed>/<generation_seed>$increment<\/generation_seed>/g" -i ${1}.csc
 i=$((i+1))
+awk -f ../final.awk final.log >> ../results/$1.log
 done
 awk -f ../final.awk final.log > ../results/$1.log
