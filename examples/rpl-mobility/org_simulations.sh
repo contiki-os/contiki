0c0d4bb293f3798e7a7a8a3ba7d73a190c14d85c
Afonso Oliveira <afonso.oliveira@tecnico.ulisboa.pt>
2016-03-01 15:26:17 +0000

    One seed to rule them all: iteration fix

--- examples/rpl-mobility/org_simulations.sh
+++ examples/rpl-mobility/org_simulations.sh
@@ -8,19 +8,19 @@ i="0"
 max=${20-20}
 sh script.conf "$@"
 cd $1
-increment=${16}
+increment=${11}
 
 echo "Partial:" > ../results/$1.log
 
 while [ $i -lt $max ]
 do
+sed -e s/SCRIPT_SEED/"$increment"/g < ${1}-preseed.csc > ${1}.csc
 cooja $1.csc
 sh ../traffic_volume.sh
 sh ../used_time.sh
 increment=$(($increment+10))
 eval `echo rename "'s/log_([A-Za-z0-9]*).log/log_"'$1'"_$i.log/'" *.log`
 eval `echo rename "'s/([A-Za-z]*).pcap/"'$1'"_$i.pcap/'" *.pcap`
-sed -e "s/<generation_seed>.*<\/generation_seed>/<generation_seed>$increment<\/generation_seed>/g" -i ${1}.csc
 i=$((i+1))
 awk -f ../final.awk final.log >> ../results/$1.log
 done
