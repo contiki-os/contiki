66453f2f3a243bc86cc837c9132cae402db6a8a1
Laurent Deru <laurent.deru@cetic.be>
2016-03-30 17:05:25 +0200

    Always use uip_ds6_select_src when sending back an icmp error

--- core/net/ipv6/uip-icmp6.c
+++ core/net/ipv6/uip-icmp6.c
@@ -239,12 +239,8 @@ uip_icmp6_error_output(uint8_t type, uint8_t code, uint32_t param) {
       return;
     }
   } else {
-#if UIP_CONF_ROUTER
     /* need to pick a source that corresponds to this node */
     uip_ds6_select_src(&UIP_IP_BUF->srcipaddr, &tmp_ipaddr);
-#else
-    uip_ipaddr_copy(&UIP_IP_BUF->srcipaddr, &tmp_ipaddr);
-#endif
   }
 
   UIP_ICMP_BUF->type = type;
