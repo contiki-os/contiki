b3afd65b1dbc9feed793883b18fb2afab46eb4be
Atis Elsts <atis.elsts@gmail.com>
2016-04-25 17:13:46 +0300

    fix a few comments in TSCH

--- core/net/mac/tsch/tsch-rpl.c
+++ core/net/mac/tsch/tsch-rpl.c
@@ -73,7 +73,7 @@ tsch_rpl_callback_leaving_network(void)
 }
 /*---------------------------------------------------------------------------*/
 /* Set TSCH EB period based on current RPL DIO period.
- * To use, set #define RPL_CALLBACK_PARENT_SWITCH tsch_rpl_callback_new_dio_interval */
+ * To use, set #define RPL_CALLBACK_NEW_DIO_INTERVAL tsch_rpl_callback_new_dio_interval */
 void
 tsch_rpl_callback_new_dio_interval(uint8_t dio_interval)
 {
