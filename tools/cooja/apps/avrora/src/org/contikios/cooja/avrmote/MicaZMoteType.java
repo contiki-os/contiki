127153d00302e6d2b85543fe26bf1c195dbebf1c
Joakim Eriksson <joakime@sics.se>
2016-04-26 20:00:21 +0200

    made a text-based version of the MessageList for headless execution

--- tools/cooja/apps/avrora/src/org/contikios/cooja/avrmote/MicaZMoteType.java
+++ tools/cooja/apps/avrora/src/org/contikios/cooja/avrmote/MicaZMoteType.java
@@ -63,7 +63,9 @@ import org.contikios.cooja.avrmote.interfaces.MicaZLED;
 import org.contikios.cooja.avrmote.interfaces.MicaZRadio;
 import org.contikios.cooja.dialogs.CompileContiki;
 import org.contikios.cooja.dialogs.MessageList;
-import org.contikios.cooja.dialogs.MessageList.MessageContainer;
+import org.contikios.cooja.dialogs.MessageListText;
+import org.contikios.cooja.dialogs.MessageListUI;
+import org.contikios.cooja.dialogs.MessageContainer;
 import org.contikios.cooja.interfaces.Mote2MoteRelations;
 import org.contikios.cooja.interfaces.MoteAttributes;
 import org.contikios.cooja.interfaces.Position;
@@ -364,7 +366,7 @@ public class MicaZMoteType implements MoteType {
       throw new MoteTypeCreationException("No identifier");
     }
 
-    final MessageList compilationOutput = new MessageList();
+    final MessageList compilationOutput = visAvailable ? new MessageListUI() : new MessageListText();
 
     if (getCompileCommands() != null) {
       /* Handle multiple compilation commands one by one */
