119291d083deb73e9bc053e38ff6d6482b5f38ab
Jens Dede <jd@comnets.uni-bremen.de>
2016-05-02 19:13:26 +0200

    Fix putchar include when using IPv4

--- cpu/msp430/dev/uart0-putchar.c
+++ cpu/msp430/dev/uart0-putchar.c
@@ -1,9 +1,12 @@
-#include "dev/uart0.h"
 #include <stdio.h>
+#include "dev/uart0.h"
 
+#ifndef NETSTACK_CONF_WITH_IPV4
+/* In case of IPv4: putchar() is defined by the SLIP driver */
 int
 putchar(int c)
 {
   uart0_writeb((char)c);
   return c;
 }
+#endif /* NETSTACK_CONF_WITH_IPV4 */
--- cpu/msp430/dev/uart1-putchar.c
+++ cpu/msp430/dev/uart1-putchar.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "dev/uart1.h"
 
+#ifndef NETSTACK_CONF_WITH_IPV4
+/* In case of IPv4: putchar() is defined by the SLIP driver */
 int
 putchar(int c)
 {
   uart1_writeb((char)c);
   return c;
 }
+#endif /* NETSTACK_CONF_WITH_IPV4 */
