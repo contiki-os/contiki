26ae3344142be184f704cec140e4dab52def6498
Mark Solters <msolters@driblet.io>
2016-05-17 15:26:09 -0400

    Hex merge now uses exclusively -crop (no -exclude)

--- examples/cc26xx-ota-bootloader/make-all.sh
+++ examples/cc26xx-ota-bootloader/make-all.sh
@@ -9,4 +9,4 @@ make clean
 make ota-image-example
 
 cd ..
-srec_cat bootloader/bootloader.hex -intel -exclude 0x1000 0x1FFA8 ota-image-example/ota-image-example.hex -intel -crop 0x1000 0x1FFA8 -o firmware.hex -intel
+srec_cat bootloader/bootloader.hex -intel -crop 0x0 0x1000 0x1FFA8 0x20000 ota-image-example/ota-image-example.hex -intel -crop 0x1000 0x1FFA8 -o firmware.hex -intel
