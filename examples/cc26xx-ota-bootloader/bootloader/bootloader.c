c34a326bf4c4c4ee4a347931ee941dd23b56f211
Mark Solters <msolters@gmail.com>
2016-05-21 20:30:11 -0400

    Removed unnecessary code from ota-bootloader

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -1,40 +1,8 @@
-#include "ti-lib.h"
-#include <stdint.h>
-#include <string.h>
-#include <stdbool.h>
-#include "driverlib/gpio.h"
-
 int
 main(void)
 {
-  //  (1) Enable GPIO
-  /* Disable global interrupts */
-  bool int_disabled = ti_lib_int_master_disable();
 
-  /* Turn on the PERIPH PD */
-  ti_lib_prcm_power_domain_on(PRCM_DOMAIN_PERIPH);
-
-  /* Wait for domains to power on */
-  while((ti_lib_prcm_power_domain_status(PRCM_DOMAIN_PERIPH)
-        != PRCM_DOMAIN_POWER_ON));
-
-  /* Enable GPIO peripheral */
-  ti_lib_prcm_peripheral_run_enable(PRCM_PERIPH_GPIO);
-
-  /* Apply settings and wait for them to take effect */
-  ti_lib_prcm_load_set();
-  while(!ti_lib_prcm_load_get());
-
-  /* Re-enable interrupt if initially enabled. */
-  if(!int_disabled) {
-    ti_lib_int_master_enable();
-  }
-
-  //  (2) Turn on red LED
-  GPIODirModeSet(GPIO_PIN_10, GPIO_DIR_MODE_OUT);
-  GPIOPinWrite(GPIO_PIN_10, 1);
-
-  //  (3) Load address of reset function from the fixed location
+  //  (1) Load address of reset function from the fixed location
   //      of the image's reset vector and jump.
   __asm("LDR R0, =0x1004"); //  RESET vector of target image
   __asm("LDR R1, [R0]");    //  Get the branch address
