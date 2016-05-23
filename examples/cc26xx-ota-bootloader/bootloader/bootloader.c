f4f967ce328f4c6aa41ff23f8b22647fca691e33
Mark Solters <msolters@gmail.com>
2016-05-23 13:59:24 -0400

    OTA bootloader jump_to_image() compensates for RESET vector offset

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -3,15 +3,14 @@
 /**
  *    OTA Image Memory Map
  */
-#define OTA_IMAGE_1_OFFSET        0x1000  //  The "Golden Image" initial OTA firmware provided on the chip
-#define OTA_IMAGE_2_OFFSET        0x8000  //  Our first OTA version upgrade
+#define OTA_IMAGE_1        0x2000  //  The "Golden Image" initial OTA firmware provided on the chip
+#define OTA_IMAGE_2        0x8000  //  Our first OTA version upgrade
 
 /**
  *    OTA defines
  */
 #define OTA_IMAGE_HEADER_LENGTH   0x10    //  Length of OTA image metadata (CRC, version, data, etc.)
 #define OTA_IMAGE_RESET_VECTOR    0x4     //  RESET ISR Vector (see )
-
 
 /**
  *  Load address of reset function from the fixed location
@@ -20,6 +19,7 @@
 void
 jump_to_image(uint32_t destination_address)
 {
+  destination_address += OTA_IMAGE_HEADER_LENGTH + OTA_IMAGE_RESET_VECTOR;
   __asm("LDR R0, [%[dest]]"::[dest]"r"(destination_address)); //  Load the destination address
   __asm("LDR R1, [R0]");                                      //  Get the branch address
   __asm("ORR R1, #1");                                        //  Make sure the Thumb State bit is set.
@@ -29,6 +29,6 @@ jump_to_image(uint32_t destination_address)
 int
 main(void)
 {
-  jump_to_image(OTA_IMAGE_1_OFFSET + OTA_IMAGE_HEADER_LENGTH + OTA_IMAGE_RESET_VECTOR);
+  jump_to_image( OTA_IMAGE_1 );
   return 0;
 }
