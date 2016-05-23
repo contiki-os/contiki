9cf6a8216167d36f6a3d92d3a7630514caf6f43a
Mark Solters <msolters@gmail.com>
2016-05-22 23:43:24 -0400

    Bootloader now allows jumping to flash address calculated at run-time

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -1,13 +1,34 @@
+#include <stdint.h>
+
+/**
+ *    OTA Image Memory Map
+ */
+#define OTA_IMAGE_1_OFFSET        0x1000  //  The "Golden Image" initial OTA firmware provided on the chip
+#define OTA_IMAGE_2_OFFSET        0x8000  //  Our first OTA version upgrade
+
+/**
+ *    OTA defines
+ */
+#define OTA_IMAGE_HEADER_LENGTH   0x10    //  Length of OTA image metadata (CRC, version, data, etc.)
+#define OTA_IMAGE_RESET_VECTOR    0x4     //  RESET ISR Vector (see )
+
+
+/**
+ *  Load address of reset function from the fixed location
+ *  of the image's reset vector and jump.
+ */
+void
+jump_to_image(uint32_t destination_address)
+{
+  __asm("LDR R0, [%[dest]]"::[dest]"r"(destination_address)); //  Load the destination address
+  __asm("LDR R1, [R0]");                                      //  Get the branch address
+  __asm("ORR R1, #1");                                        //  Make sure the Thumb State bit is set.
+  __asm("BX R1");                                             //  Branch execution
+}
+
 int
 main(void)
 {
-
-  //  (1) Load address of reset function from the fixed location
-  //      of the image's reset vector and jump.
-  __asm("LDR R0, =0x1004"); //  RESET vector of target image
-  __asm("LDR R1, [R0]");    //  Get the branch address
-  __asm("ORR R1, #1");      //  Make sure the Thumb State bit is set.
-  __asm("BX R1");           //  Branch execution
-
+  jump_to_image(OTA_IMAGE_1_OFFSET + OTA_IMAGE_HEADER_LENGTH + OTA_IMAGE_RESET_VECTOR);
   return 0;
 }
