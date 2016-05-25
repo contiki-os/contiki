661c248a1a320fc29d4ce90a9152821c93c03cb6
Mark Solters <msolters@driblet.io>
2016-05-25 10:52:13 -0400

    Added GPIO/ext-flash initialization to bootloader

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include "ti-lib.h"
 #include "ota.h"
 
 /**
@@ -13,16 +14,51 @@ jump_to_image(uint32_t destination_address)
   __asm("BX R0");                                             //  Branch execution
 }
 
+void
+initialize_peripherals() {
+  /* Disable global interrupts */
+  bool int_disabled = ti_lib_int_master_disable();
+
+  //power_domains_on();
+
+  /* Enable GPIO peripheral */
+  ti_lib_prcm_peripheral_run_enable(PRCM_PERIPH_GPIO);
+
+  /* Apply settings and wait for them to take effect */
+  ti_lib_prcm_load_set();
+  while(!ti_lib_prcm_load_get());
+
+  /* I2C controller */
+  //board_i2c_wakeup();
+
+  //buzzer_init();
+
+  /* Make sure the external flash is in the lower power mode */
+  ext_flash_init();
+
+  //lpm_register_module(&sensortag_module);
+
+  /* For unsupported peripherals, select a default pin configuration */
+  //configure_unused_pins();
+
+  /* Re-enable interrupt if initially enabled. */
+  if(!int_disabled) {
+    ti_lib_int_master_enable();
+  }
+}
+
 int
 main(void)
 {
+  initialize_peripherals();
+
   OTAMetadata_t example_metadata = {
     0, 0, 0x27, 0x1, 0x2, 0x2000
   };
   if ( FlashProgram((uint8_t*)&example_metadata, 0x1000, OTA_METADATA_LENGTH) == FAPI_STATUS_SUCCESS ) {
-    return 0;
+  //  return 0;
   } else {
-    return -1;
+  //  return -1;
   }
 
   generate_fake_metadata();
