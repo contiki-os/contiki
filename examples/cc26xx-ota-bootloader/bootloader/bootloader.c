c442ddd53d1dcb4e8d4802d8aad83faf80eb6905
Mark Solters <msolters@driblet.io>
2016-05-25 16:17:55 -0400

    Bootloader now has full R/W to both internal and external flash

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include "ti-lib.h"
+
 #include "ota.h"
 
 /**
@@ -14,12 +15,22 @@ jump_to_image(uint32_t destination_address)
   __asm("BX R0");                                             //  Branch execution
 }
 
+static void
+power_domains_on(void) {
+  /* Turn on the PERIPH PD */
+  ti_lib_prcm_power_domain_on(PRCM_DOMAIN_PERIPH);
+
+  /* Wait for domains to power on */
+  while((ti_lib_prcm_power_domain_status(PRCM_DOMAIN_PERIPH)
+        != PRCM_DOMAIN_POWER_ON));
+}
+
 void
 initialize_peripherals() {
   /* Disable global interrupts */
   bool int_disabled = ti_lib_int_master_disable();
 
-  //power_domains_on();
+  power_domains_on();
 
   /* Enable GPIO peripheral */
   ti_lib_prcm_peripheral_run_enable(PRCM_PERIPH_GPIO);
@@ -28,18 +39,8 @@ initialize_peripherals() {
   ti_lib_prcm_load_set();
   while(!ti_lib_prcm_load_get());
 
-  /* I2C controller */
-  //board_i2c_wakeup();
-
-  //buzzer_init();
-
   /* Make sure the external flash is in the lower power mode */
   ext_flash_init();
-
-  //lpm_register_module(&sensortag_module);
-
-  /* For unsupported peripherals, select a default pin configuration */
-  //configure_unused_pins();
 
   /* Re-enable interrupt if initially enabled. */
   if(!int_disabled) {
@@ -55,11 +56,11 @@ main(void)
   OTAMetadata_t example_metadata = {
     0, 0, 0x27, 0x1, 0x2, 0x2000
   };
-  if ( FlashProgram((uint8_t*)&example_metadata, 0x1000, OTA_METADATA_LENGTH) == FAPI_STATUS_SUCCESS ) {
+  //if ( FlashProgram((uint8_t*)&example_metadata, 0x1000, OTA_METADATA_LENGTH) == FAPI_STATUS_SUCCESS ) {
   //  return 0;
-  } else {
+  //} else {
   //  return -1;
-  }
+  //}
 
   generate_fake_metadata();
 
