ea8c361b0ef96b062bd23e3b1ba0f24b4ad55cbf
Mark Solters <msolters@driblet.io>
2016-05-25 16:17:21 -0400

    Bootloader now takes up 2 pages instead of 1

--- examples/cc26xx-ota-bootloader/make-master-hex.sh
+++ examples/cc26xx-ota-bootloader/make-master-hex.sh
@@ -13,4 +13,4 @@ make clean
 make ota-image-example
 
 cd ..
-srec_cat bootloader/bootloader.hex -intel -crop 0x0 0x1000 0x1FFA8 0x20000 ota-image-example/ota-image-example.hex -intel -crop 0x1000 0x1FFA8 -o firmware.hex -intel
+srec_cat bootloader/bootloader.hex -intel -crop 0x0 0x2000 0x1FFA8 0x20000 ota-image-example/ota-image-example.hex -intel -crop 0x2000 0x1FFA8 -o firmware.hex -intel
