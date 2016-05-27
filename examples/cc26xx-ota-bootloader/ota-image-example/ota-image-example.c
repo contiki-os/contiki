5f2c53fea1c6456ee851fd8474a65677bbdfcd7a
Mark Solters <msolters@driblet.io>
2016-05-27 18:20:17 -0400

    ota-image-example starts ota_download thread

--- examples/cc26xx-ota-bootloader/ota-image-example/ota-image-example.c
+++ examples/cc26xx-ota-bootloader/ota-image-example/ota-image-example.c
@@ -17,11 +17,7 @@
 #include "sys/ctimer.h"
 #include "sys/timer.h"
 
-#include "contiki-net.h"
-#include "http-socket.h"
-#include "ip64-addr.h"
-
-#include "ota.h"
+#include "ota-download.h"
 
 #define BLINKER_PIN	BOARD_LED_1
 
@@ -41,37 +37,6 @@ blink_looper()
 
 OTAMetadata_t current_firmware;
 
-static struct http_socket s;
-static int bytes_received = 0;
-static void
-callback(struct http_socket *s, void *ptr,
-         http_socket_event_t e,
-         const uint8_t *data, uint16_t datalen)
-{
-  if(e == HTTP_SOCKET_ERR) {
-    printf("HTTP socket error\n");
-  } else if(e == HTTP_SOCKET_TIMEDOUT) {
-    printf("HTTP socket error: timed out\n");
-  } else if(e == HTTP_SOCKET_ABORTED) {
-    printf("HTTP socket error: aborted\n");
-  } else if(e == HTTP_SOCKET_HOSTNAME_NOT_FOUND) {
-    printf("HTTP socket error: hostname not found\n");
-  } else if(e == HTTP_SOCKET_CLOSED) {
-    printf("HTTP socket closed, %d bytes received\n", bytes_received);
-  } else if(e == HTTP_SOCKET_DATA) {
-    bytes_received += datalen;
-    printf("HTTP socket received %d bytes of data\n", datalen);
-  }
-}
-
-struct ctimer http_timer;
-
-void
-http_looper() {
-  http_socket_get(&s, "http://[bbbb::1]:3003/", 0, 0, callback, NULL);
-  ctimer_reset( &http_timer );
-}
-
 PROCESS_THREAD(blinker_test_loop, ev, data)
 {
   PROCESS_BEGIN();
@@ -83,30 +48,22 @@ PROCESS_THREAD(blinker_test_loop, ev, data)
 	GPIODirModeSet( BLINKER_PIN, GPIO_DIR_MODE_OUT);
   ctimer_set( &blink_timer, (CLOCK_SECOND/2), blink_looper, NULL);
 
-  //  (3) HTTP testing!
-  uip_ip6addr_t ip6addr;
-  uip_ip6addr(&ip6addr, 0xbbbb, 0, 0, 0, 0, 0, 0, 0x1);
-  http_socket_init(&s);
-  ctimer_set(&http_timer, (CLOCK_SECOND * 5), http_looper, NULL);
-
-
-
-  //  (1) Get metadata about the current firmware version
+  //  (3) Get metadata about the current firmware version
   FlashRead( (uint8_t *)&current_firmware, (CURRENT_FIRMWARE<<12), OTA_METADATA_LENGTH );
   printf("\nCurrent Firmware\n");
   print_metadata( &current_firmware );
 
+  //  (4) OTA Download!
   ext_flash_init();
-
-
+  process_start(ota_download_th_p, NULL);
 
   //generate_fake_metadata();
 
   //  (2) Get metadata about OTA firmwares stored in the external flash.
-  for (int img=0; img<sizeof(ota_images); img++) {
+/*  for (int img=0; img<sizeof(ota_images); img++) {
     OTAMetadata_t ota_firmare;
 
-    /* Make sure the external flash is in the lower power mode */
+    // Make sure the external flash is in the lower power mode
     int eeprom_access = ext_flash_open();
 
     if(!eeprom_access) {
@@ -126,7 +83,7 @@ PROCESS_THREAD(blinker_test_loop, ev, data)
 
     printf("\nOTA Firmware Slot %u\n", img);
     print_metadata( &ota_firmare );
-  }
+  }*/
 
   PROCESS_END();
 }
