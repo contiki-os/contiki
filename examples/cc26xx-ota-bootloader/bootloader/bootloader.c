edcd5b8f2b53706c3d7d73f306095a8fcf314db0
Mark Solters <msolters@driblet.io>
2016-06-01 19:41:17 -0400

    Tried some new ways to reboot/indicate current firmware slot

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -41,7 +41,44 @@ main(void)
 {
   initialize_peripherals();
 
-  update_firmware( 1 );
+  //  (1) Get the metadata of whatever firmware is currently installed
+  OTAMetadata_t current_firmware = get_current_metadata();
+  //  (2) Are there any newer firmware images in ext-flash?
+  uint8_t newest_ota_slot = find_newest_ota_image();
+  OTAMetadata_t newest_firmware = get_ota_slot_metadata( newest_ota_slot );
+/*
+  erase_ota_image( 1 );
+  erase_ota_image( 2 );
+  erase_ota_image( 3 );
+  return 0;
+*/
+  uint32_t led_pin;
+  switch (newest_ota_slot)
+  {
+    case 0:
+      led_pin = IOID_25;
+      break;
+    case 1:
+      led_pin = IOID_27;
+      break;
+    case 2:
+      led_pin = IOID_7;
+      break;
+    case 3:
+      led_pin = IOID_6;
+      break;
+  }
+  GPIODirModeSet( (1 << led_pin), GPIO_DIR_MODE_OUT);
+  GPIOPinWrite( (1 << led_pin), true );
+
+  //  (3) If there's any newer images in storage, update the current firmware!
+  if ( newest_ota_slot && (newest_firmware.version > current_firmware.version) ) {
+    update_firmware( newest_ota_slot );
+  }
+
+  GPIOPinWrite( (1 << led_pin), false );
   jump_to_image( (CURRENT_FIRMWARE<<12) );
+
+  //  ( ) Boot to the current firmware
   return 0;
 }
