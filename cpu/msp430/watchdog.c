812e63d25fc4c93b7430e0d1699fbc613430705f
Atis Elsts <atis.elsts@gmail.com>
2016-06-02 16:16:59 +0300

    fix watchdog compilation for z1x nodes

--- cpu/msp430/watchdog.c
+++ cpu/msp430/watchdog.c
@@ -106,7 +106,7 @@ watchdog_init(void)
      initialization. */
   counter = 0;
   watchdog_stop();
-#if CONTIKI_TARGET_WISMOTE
+#if CONTIKI_TARGET_WISMOTE || CONTIKI_TARGET_Z1X
   SFRIFG1 &= ~WDTIFG;
   SFRIE1 |= WDTIE;
 #else
