85bce08d53f368015fcb20865b7a784892e335d5
Mark Solters <msolters@driblet.io>
2016-06-03 18:29:58 -0400

    make-master-hex inserts true CRC16 value into master hex's metadata

--- examples/cc26xx-ota-bootloader/generate-metadata.js
+++ examples/cc26xx-ota-bootloader/generate-metadata.js
@@ -3,14 +3,15 @@ var fs = require('fs');
 var firmware_bin_file = process.argv[2];
 var firmware_version = parseInt( process.argv[3] );
 var firmware_uuid = parseInt( process.argv[4] );
+var firmware_crc = parseInt( process.argv[5] )
 
 var firmware_binary = fs.readFileSync( firmware_bin_file );
 
 output_file = new Buffer( 256 + firmware_binary.length );
 
 //  Write OTA metadata
-output_file.writeUInt16LE(0x1, 0); // crc
-output_file.writeUInt16LE(0x1, 2); // crc shadow
+output_file.writeUInt16LE(firmware_crc, 0); // crc
+output_file.writeUInt16LE(0x0, 2); // crc shadow
 output_file.writeUInt32LE(firmware_binary.length, 4); // binary size
 output_file.writeUInt32LE(firmware_uuid, 8); // uuid
 output_file.writeUInt16LE(firmware_version, 12); // version
