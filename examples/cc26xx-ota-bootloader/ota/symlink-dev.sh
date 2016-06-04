bfacbc61fd78e3a17ce55be04b77fb32a96b6d11
Mark Solters <msolters@gmail.com>
2016-06-04 01:35:38 -0400

    OTA bootloader/example makefile no longer require symlinked OTA source; now just define OTA_SOURCE directory location.

--- examples/cc26xx-ota-bootloader/ota/symlink-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Symlink OTA resources into both application folders
-# Useful for debugging
-
-here="$(dirname $(pwd))"
-
-ln -s $here/ota/ota.* $here/bootloader
-ln -s $here/ota/ota.* $here/ota-image-example
-ln -s $here/ota/ota-* $here/ota-image-example
