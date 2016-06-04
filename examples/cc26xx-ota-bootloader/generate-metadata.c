d7bfef412dd4665c29a5323963d914748ea3bb1f
Mark Solters <msolters@gmail.com>
2016-06-04 14:25:53 -0400

    New generate-metadata program will automatically generate OTA metadata; no NodeJS needed.

--- examples/cc26xx-ota-bootloader/generate-metadata.c
+++ examples/cc26xx-ota-bootloader/generate-metadata.c
@@ -2,7 +2,21 @@
 #include <stdint.h>
 
 FILE *firmware_bin;
+FILE *firmware_out;
 #define HAL_WORD_SIZE 4
+
+const int i = 1;
+#define is_bigendian() ( (*(char*)&i) == 0 )
+
+uint32_t firmware_size = 0;
+typedef struct OTAMetadata {
+  uint16_t crc;             //
+  uint16_t crc_shadow;      //
+  uint32_t size;            //  Size of firmware image
+  uint32_t uuid;             //  Integer representing unique firmware ID
+  uint16_t version;         //  Integer representing firmware version
+} OTAMetadata_t;
+
 
 /*********************************************************************
  * @fn          crc16
@@ -49,6 +63,7 @@ crcCalcWord(uint8_t *_word, uint16_t imageCRC)
     //printf("%#x ", _word[idx]);
     imageCRC = crc16(imageCRC, _word[idx]);
   }
+  firmware_size += 4;
   return imageCRC;
 }
 
@@ -89,14 +104,40 @@ crcCalc(void)
   return imageCRC;
 }
 
+inline uint16_t
+swap_endian_16(uint16_t val)
+{
+	uint16_t swapped_val = (val<<8) | (val>>8);
+  return swapped_val;
+}
+
+inline uint32_t
+swap_endian_32(uint32_t val)
+{
+	uint32_t swapped_val = (val<<24) | ((val<<8) & 0x00ff0000) |
+		  ((val>>8) & 0x0000ff00) | (val>>24);
+  return swapped_val;
+}
+
 int
 main(int argc, char *argv[]) {
 
-  //  (1) Open the firmware .bin file
   if ( !argv[1] ) {
     printf("Please provide a .bin file to compute the CRC on as the first argument.\n");
     return -1;
   }
+
+  if ( !argv[2] ) {
+    printf("Please provide a 16-bit hex firmware version integer as the second argument.\n");
+    return -1;
+  }
+
+  if ( !argv[3] ) {
+    printf("Please provide a 32-bit hex UUID integer as the third argument.\n");
+    return -1;
+  }
+
+  //  (1) Open the firmware .bin file
   firmware_bin = fopen( argv[1], "rb" );
 
   //  (2) Run the CRC16 calculation over the file.  Print result.
@@ -105,6 +146,34 @@ main(int argc, char *argv[]) {
 
   //  (3) Close the .bin file.
   fclose( firmware_bin );
+
+  //  (4) Generate OTA image metadata
+  OTAMetadata_t metadata;
+  metadata.crc = crc_result;
+  metadata.crc_shadow = crc_result;
+  metadata.size = firmware_size;
+  sscanf( argv[2], "%xu", &(metadata.version) );
+  sscanf( argv[3], "%xu", &(metadata.uuid) );
+
+  //swap_endian_32( &metadata.uuid );
+
+  uint8_t output_buffer[ sizeof(OTAMetadata_t) ];
+  memcpy( output_buffer, (uint8_t *)&metadata, sizeof(OTAMetadata_t) );
+
+  //  (4) Open the output firmware .bin file
+  firmware_out = fopen( "firmware-metadata.bin", "wb" );
+
+  //  (5) Write the metadata
+  fwrite(output_buffer, sizeof(output_buffer), 1, firmware_out);
+
+  //  (6) 0xff spacing until firmware binary starts
+  uint8_t blank_buffer[240];
+  for (int b=0; b<240; b++) {
+    blank_buffer[ b ] = 0xff;
+  }
+  fwrite( blank_buffer, 240, 1, firmware_out);
+
+  fclose( firmware_out );
 
   return 0;
 }
--- examples/cc26xx-ota-bootloader/make-master-hex.sh
+++ examples/cc26xx-ota-bootloader/make-master-hex.sh
@@ -14,10 +14,13 @@ make ota-image-example
 
 printf "\nAdding metadata to OTA Example Image\n"
 cd ..
-rm -f firmware-with-metadata.bin
-make generate-crc
-CRC=`./generate-crc ota-image-example/ota-image-example.bin`
-node generate-metadata.js ota-image-example/ota-image-example.bin 0x0 0xabcd $CRC
+rm -f firmware-metadata.bin
+rm -f generate-metadata
+make generate-metadata
+#CRC=`./generate-crc ota-image-example/ota-image-example.bin`
+#node generate-metadata.js ota-image-example/ota-image-example.bin 0x0 0xabcd $CRC
+./generate-metadata ota-image-example/ota-image-example.bin 0x1 0xabcd
+srec_cat firmware-metadata.bin -binary ota-image-example/ota-image-example.bin -binary -offset 0x100 -o firmware-with-metadata.bin -binary
 
 printf "\nMerging OTA Example Image with Bootloader\n"
 srec_cat bootloader/bootloader.hex -intel -crop 0x0 0x2000 0x1FFA8 0x20000 firmware-with-metadata.bin -binary -offset 0x2000 -crop 0x2000 0x1B000 -o firmware.hex -intel
