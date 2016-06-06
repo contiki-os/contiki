2ecc2b7095738dedb30777b0378637da79bd60d5
Mark Solters <msolters@driblet.io>
2016-06-06 19:02:30 -0400

    Added Sensortag ext-flash pinouts to bootloader

--- examples/cc26xx-ota-bootloader/bootloader/ext-flash/spi-pins.h
+++ examples/cc26xx-ota-bootloader/bootloader/ext-flash/spi-pins.h
@@ -8,8 +8,20 @@
 
 #define BOARD_IOID_FLASH_CS       IOID_14
 #define BOARD_FLASH_CS            (1 << BOARD_IOID_FLASH_CS)
+
+/**
+ *  For srf06 board:
+ */
+ /*
 #define BOARD_IOID_SPI_CLK_FLASH  IOID_10
 #define BOARD_IOID_SPI_MOSI       IOID_9
 #define BOARD_IOID_SPI_MISO       IOID_8
+*/
 
+/**
+ *  For Sensortag:
+ */
+#define BOARD_IOID_SPI_CLK_FLASH  IOID_17
+#define BOARD_IOID_SPI_MOSI       IOID_19
+#define BOARD_IOID_SPI_MISO       IOID_18
 #endif
