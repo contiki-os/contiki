4f87dddc5436fc33984ca9887b93c5c28638f3b8
Mark Solters <msolters@driblet.io>
2016-06-08 17:24:55 -0400

    Bootloader will now repair the device with the latest valid firmware even when the current firmware is corrupt

--- examples/cc26xx-ota-bootloader/bootloader/bootloader.c
+++ examples/cc26xx-ota-bootloader/bootloader/bootloader.c
@@ -55,31 +55,33 @@ main(void)
   OTAMetadata_t current_firmware;
   get_current_metadata( &current_firmware );
 
-  //  (2) Are there any newer firmware images in ext-flash?
+  //  (2) Verify the current firmware! (Recompute the CRC over the internal flash image)
+  verify_current_firmware( &current_firmware );
+
+  //  (3) Are there any newer firmware images in ext-flash?
   uint8_t newest_ota_slot = find_newest_ota_image();
   OTAMetadata_t newest_firmware;
   while( get_ota_slot_metadata( newest_ota_slot, &newest_firmware ) );
 
-  //  (3) If there's a newer image, install that version!
-  if ( ( newest_ota_slot > 0 ) && (newest_firmware.version > current_firmware.version) ) {
-    update_firmware( newest_ota_slot );
-    get_current_metadata( &current_firmware ); // update the current firmware metadata!
-  }
-
-  //  (4) Verify the current firmware! (Recompute the CRC over the internal flash image)
-  verify_current_firmware( &current_firmware );
-
-  //  (5) If the current image is valid, jump to it.
+  //  (4) Is the current image valid?
   if ( validate_ota_metadata( &current_firmware ) ) {
-    jump_to_image( (CURRENT_FIRMWARE<<12) );
+    if ( ( newest_ota_slot > 0 ) && (newest_firmware.version > current_firmware.version) ) {
+      //  If there's a newer firmware image than the current firmware, install
+      //  the newer version!
+      update_firmware( newest_ota_slot );
+      ti_lib_sys_ctrl_system_reset(); // reboot
+    } else {
+      //  If our image is valid, and there's nothing newer, then boot the firmware.
+      jump_to_image( (CURRENT_FIRMWARE<<12) );
+    }
+  } else {
+    //  If our image is not valid, install the newest valid image we have.
+    //  Note: This can be the Golden Image, when newest_ota_slot = 0.
+    update_firmware( newest_ota_slot );
+    ti_lib_sys_ctrl_system_reset(); // reboot
   }
 
-  //  (6) If we've gotten here, something's wrong.  Install the latest valid firmware!
-  //      We no longer care if it's newer than what we have now, because we assume it is
-  //      invalid data.
-  update_firmware( newest_ota_slot );
-  ti_lib_sys_ctrl_system_reset(); // reboot
-
-  //  This function should never return
+  //  main() *should* never return - we should have rebooted or branched
+  //  to other code by now.
   return 0;
 }
