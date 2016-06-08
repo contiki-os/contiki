0d9cfcc0be618513fecefa6bb2631729bebde5cc
Mark Solters <msolters@driblet.io>
2016-06-08 17:26:08 -0400

    project-conf.h contains SPI definitions for using ext-flash with srf06 board

--- examples/cc26xx-ota-bootloader/ota-image-example/project-conf.h
+++ examples/cc26xx-ota-bootloader/ota-image-example/project-conf.h
@@ -3,3 +3,15 @@
 
 #undef REST_MAX_CHUNK_SIZE
 #define REST_MAX_CHUNK_SIZE            256
+
+
+/**
+*  For srf06 board we must specify SPI connections
+*/
+/*
+#define BOARD_IOID_FLASH_CS       IOID_14
+#define BOARD_FLASH_CS            (1 << BOARD_IOID_FLASH_CS)
+#define BOARD_IOID_SPI_CLK_FLASH  IOID_10
+#define BOARD_IOID_SPI_MOSI       IOID_9
+#define BOARD_IOID_SPI_MISO       IOID_8
+*/
