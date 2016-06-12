373e5653e64ef1756a411bbeb4b7a58b4b5278db
Pablo Corbalán <paul.corbalan@gmail.com>
2016-06-12 16:09:45 +0100

    uIPv6: Make sure dest address is not multicast before refreshing the state of a neighbor

--- core/net/ipv6/uip6.c
+++ core/net/ipv6/uip6.c
@@ -1156,7 +1156,9 @@ uip_process(uint8_t flag)
 
   /* Refresh neighbor state after receiving a unicast message */
 #if UIP_ND6_SEND_NA
-  uip_ds6_nbr_refresh_reachable_state(&UIP_IP_BUF->srcipaddr);
+  if(!uip_is_addr_mcast(&UIP_IP_BUF->destipaddr)) {
+    uip_ds6_nbr_refresh_reachable_state(&UIP_IP_BUF->srcipaddr);
+  }
 #endif /* UIP_ND6_SEND_NA */
 
 #if UIP_CONF_ROUTER
