1dc671be7e06a482c6433f035ab3afc8a7a17498
Robert Olsson <robert@herjulf.se>
2016-06-17 14:16:45 +0200

    Added authors and proposed Copyright.

--- platform/avr-rss2/apps/pdr-test/pdr.c
+++ platform/avr-rss2/apps/pdr-test/pdr.c
@@ -1,3 +1,58 @@
+/*
+ * Copyright (c) 2016, 
+ * Authors:
+ *   Atis Elsts       <atis.elsts@bristol.ac.uk>
+ *   Christian Rohner <christian.rohner@it.uu.se>
+ *   Robert Olsson    <roolss@kth.se> 
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/* Main program for Packet Delivery Ratio, (PDR) testing. 
+
+   Data file format:
+   * timestamp unix time format
+   * tx node model (platform)
+   * tx node id
+   * tx node temperature (multiplied by 100)
+   * rx node model (platform)
+   * rx node id
+   * rx node temperature
+   * channel
+   * Tx setting/power
+   * number of packets with good checksum (from 100 total in a test)
+   * number of all packets received
+   * rssi level, average for 100 packets
+   * lqi level
+
+Example:
+1466035452.0 > avr-rss2 49771 1168 | avr-rss2 49551 1075 | 11 -7 | 100 100 -10 255
+
+*/
+
 #include "pdr.h"
 #include "sys/process.h"
 #include "dev/serial-line.h"
