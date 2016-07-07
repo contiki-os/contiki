d1ed85653343d3db7b1128f9f27f91b4fffe1a1b
Mohamed Seliem <mseliem11@gmail.com>
2016-07-07 14:23:47 +0200

    Update project-conf.h

--- examples/ipv6/multicast/project-conf.h
+++ examples/ipv6/multicast/project-conf.h
@@ -62,6 +62,7 @@
 /* Code/RAM footprint savings so that things will fit on our device */
 #undef NBR_TABLE_CONF_MAX_NEIGHBORS
 #undef UIP_CONF_DS6_ROUTE_NBU
+
 #define NBR_TABLE_CONF_MAX_NEIGHBORS  10
 #define UIP_CONF_DS6_ROUTE_NBU        10
 
