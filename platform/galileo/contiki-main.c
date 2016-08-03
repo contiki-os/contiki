1c631c8ef627b4c3be5f7b8de80fe2990c283a64
Michael LeMay <lemaymd@gmail.com>
2016-08-03 07:58:51 -0700

    x86: Support configurable UART baud rate
    This patch extends the Intel Quark X1000 SoC UART initialization API to
    accept a numeric baud rate specification.

--- platform/galileo/contiki-main.c
+++ platform/galileo/contiki-main.c
@@ -86,8 +86,10 @@ main(void)
   quarkX1000_imr_conf();
 #endif
   irq_init();
-  /* Initialize UART connected to Galileo Gen2 FTDI header */
-  quarkX1000_uart_init(QUARK_X1000_UART_1);
+  /* Initialize UART connected to Galileo Gen1 3.5mm audio-style jack or
+   * Galileo Gen2 FTDI header
+   */
+  quarkX1000_uart_init(QUARK_X1000_UART_1, 115200);
   clock_init();
   rtimer_init();
 
