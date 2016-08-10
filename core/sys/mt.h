f7507cb84dd5e0e5f6dd4b88a0dea42573787c7d
Benoît Thébaudeau <benoit.thebaudeau.dev@gmail.com>
2016-08-10 04:01:11 +0200

    mt: Fix preemption
    Preemption was supposed to be supported, but it had no means of safely
    updating the state of a thread, so mt_exec() could fail to resume a
    preempted thread.
    
    mt_exec() is allowed to be called only from the main Contiki thread, so
    the mt threads passed to it may be only ready or exited, not running.
    Consequently, there is no need for a distinction between the ready and
    running states, so merge them as a started state, which avoids having to
    update the state of a thread upon preemption.
    
    Signed-off-by: Benoît Thébaudeau <benoit.thebaudeau.dev@gmail.com>

--- core/sys/mt.h
+++ core/sys/mt.h
@@ -84,9 +84,8 @@
 
 #include "contiki.h"
 
-#define MT_STATE_READY   1
-#define MT_STATE_RUNNING 2
-#define MT_STATE_EXITED  5
+#define MT_STATE_STARTED 1
+#define MT_STATE_EXITED  2
 
 /**
  * An opaque structure that is used for holding the state of a thread.
