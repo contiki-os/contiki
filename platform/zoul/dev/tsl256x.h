72e0d135cb6238cfdea2291ded070e71776928a7
Antonio Lignan <alinan@zolertia.com>
2016-10-19 15:47:12 +0200

    Renamed driver and reworked to support both TSL2561/TSL2563

--- platform/zoul/dev/tsl256x.h
+++ platform/zoul/dev/tsl256x.h
@@ -34,98 +34,125 @@
  * \addtogroup zoul-sensors
  * @{
  *
- * \defgroup zoul-tsl2563-sensor TSL2563 Sensor
+ * \defgroup zoul-tsl256x-sensor TSL256X Sensor
  *
- * Driver for the TSL2563 sensor
+ * Driver for the TSL256X sensor
  *
- * The TSL2563 driver returns the converted light value value in lux
+ * The TSL256X driver returns the converted light value value in lux
  * @{
  *
  * \file
- * Header file for the external TSL2563 Sensor Driver
+ * Header file for the external TSL256X Sensor Driver
  *
  * \author
  *         Antonio Lignan <alinan@zolertia.com>
  *         Toni Lozano <tlozano@zolertia.com>
  */
 /*---------------------------------------------------------------------------*/
-#ifndef TSL2563_H_
-#define TSL2563_H_
+#ifndef TSL256X_H_
+#define TSL256X_H_
 #include <stdio.h>
 #include "lib/sensors.h"
 #include "dev/zoul-sensors.h"
 #include "i2c.h"
 /* -------------------------------------------------------------------------- */
 /**
- * \name TSL2563 digital Light sensor address and registers
+ * \name TSL256x digital Light sensor specific model information
  * @{
  */
-/* -------------------------------------------------------------------------- */
-#define TSL2563_ADDR                  0x39
-/* -------------------------------------------------------------------------- */
-#define TSL2563_CONTROL               0x00
-#define TSL2563_TIMMING               0x01
-#define TSL2563_THRLOWLOW             0x02
-#define TSL2563_THRLOWHIGH            0x03
-#define TSL2563_THRHIGHLOW            0x04
-#define TSL2563_THRHIGHHIGH           0x05
-#define TSL2563_INTERRUPT             0x06
-#define TSL2563_CRC                   0x08
-#define TSL2563_ID_REG                0x0A
-#define TSL2563_D0LOW                 0x0C
-#define TSL2563_D0HIGH                0x0D
-#define TSL2563_D1LOW                 0x0E
-#define TSL2563_D1HIGH                0x0F
-/* -------------------------------------------------------------------------- */
-/* Uses the word read/write operation protocol */
-#define TSL2563_COMMAND               0xA0
-#define TSL2563_CLEAR_INTERRUPT       0x40
-/* -------------------------------------------------------------------------- */
-#define TSL2563_CONTROL_POWER_ON      0x03
-#define TSL2563_CONTROL_POWER_OFF     0x00
-#define TSL2563_TIMMING_GAIN          0x10
-#define TSL2563_TIMMING_MANUAL        0x08
-#define TSL2563_TIMMING_INTEG_MANUAL  0x03
-#define TSL2563_TIMMING_INTEG_402MS   0x02
-#define TSL2563_TIMMING_INTEG_101MS   0x01
-#define TSL2563_TIMMING_INTEG_13_7MS  0x00
-#define TSL2563_TIMMING_INTEG_MASK    0x03
+/* This driver supports the TSL2563 (Zolertia) and the TSL2561 (Grove) */
+#define TSL2561_SENSOR_REF            0
+#define TSL2563_SENSOR_REF            1
 
-#define TSL2563_G16X_402MS (TSL2563_TIMMING_INTEG_402MS + TSL2563_TIMMING_GAIN)
-#define TSL2563_G1X_402MS                            TSL2563_TIMMING_INTEG_402MS
-#define TSL2563_G1X_101MS                            TSL2563_TIMMING_INTEG_101MS
-#define TSL2563_G1X_13_7MS                          TSL2563_TIMMING_INTEG_13_7MS
-
-#define TSL2563_INTR_SHIFT            0x04
-#define TSL2563_INTR_DISABLED         0x00
-#define TSL2563_INTR_LEVEL            0x01
-#define TSL2563_INTR_SMB_ALERT        0x02
-#define TSL2563_INTR_TEST             0x03
-
-#define TSL2563_INT_PERSIST_EVERY     0x00
-#define TSL2563_INT_PERSIST_ANY       0x01
-#define TSL2563_INT_PERSIST_2_CYCLES  0x02
-#define TSL2563_INT_PERSIST_3_CYCLES  0x03
-#define TSL2563_INT_PERSIST_4_CYCLES  0x04
-#define TSL2563_INT_PERSIST_5_CYCLES  0x05
-#define TSL2563_INT_PERSIST_6_CYCLES  0x06
-#define TSL2563_INT_PERSIST_7_CYCLES  0x07
-#define TSL2563_INT_PERSIST_8_CYCLES  0x08
-#define TSL2563_INT_PERSIST_9_CYCLES  0x09
-#define TSL2563_INT_PERSIST_10_CYCLES 0x0A
-#define TSL2563_INT_PERSIST_11_CYCLES 0x0B
-#define TSL2563_INT_PERSIST_12_CYCLES 0x0C
-#define TSL2563_INT_PERSIST_13_CYCLES 0x0D
-#define TSL2563_INT_PERSIST_14_CYCLES 0x0E
-#define TSL2563_INT_PERSIST_15_CYCLES 0x0F
-
-#define TSL2563_ID_PARTNO_MASK        0xF0
-#define TSL2563_ID_REV_MASK           0x0F
+/* The TSL2563 (from Zolertia) has a different part number than the TSL2561 from
+ * Grove (digital light sensor)
+ */
 #define TSL2563_EXPECTED_PARTNO       0x30
+#define TSL2561_EXPECTED_PARTNO       0x50
+
+#ifndef TSL256X_CONF_REF
+#define TSL256X_REF                   TSL2561_SENSOR_REF
+#else
+#define TSL256X_REF                   TSL256X_CONF_REF
+#endif
+
+#if TSL256X_CONF_REF == TSL2561_SENSOR
+#define TSL256X_ADDR                  0x29
+#define TSL256X_EXPECTED_PARTNO       TSL2561_EXPECTED_PARTNO
+#else
+#define TSL256X_ADDR                  0x39
+#define TSL256X_EXPECTED_PARTNO       TSL2563_EXPECTED_PARTNO
+#endif
+
 /** @} */
 /* -------------------------------------------------------------------------- */
 /**
- * \name TSL2563 convertion and calibration values
+ * \name TSL256X digital Light registers
+ * @{
+ */
+/* -------------------------------------------------------------------------- */
+#define TSL256X_CONTROL               0x00
+#define TSL256X_TIMMING               0x01
+#define TSL256X_THRLOWLOW             0x02
+#define TSL256X_THRLOWHIGH            0x03
+#define TSL256X_THRHIGHLOW            0x04
+#define TSL256X_THRHIGHHIGH           0x05
+#define TSL256X_INTERRUPT             0x06
+#define TSL256X_CRC                   0x08
+#define TSL256X_ID_REG                0x0A
+#define TSL256X_D0LOW                 0x0C
+#define TSL256X_D0HIGH                0x0D
+#define TSL256X_D1LOW                 0x0E
+#define TSL256X_D1HIGH                0x0F
+/* -------------------------------------------------------------------------- */
+/* Uses the word read/write operation protocol */
+#define TSL256X_COMMAND               0xA0
+#define TSL256X_CLEAR_INTERRUPT       0x40
+/* -------------------------------------------------------------------------- */
+#define TSL256X_CONTROL_POWER_ON      0x03
+#define TSL256X_CONTROL_POWER_OFF     0x00
+#define TSL256X_TIMMING_GAIN          0x10
+#define TSL256X_TIMMING_MANUAL        0x08
+#define TSL256X_TIMMING_INTEG_MANUAL  0x03
+#define TSL256X_TIMMING_INTEG_402MS   0x02
+#define TSL256X_TIMMING_INTEG_101MS   0x01
+#define TSL256X_TIMMING_INTEG_13_7MS  0x00
+#define TSL256X_TIMMING_INTEG_MASK    0x03
+
+#define TSL256X_G16X_402MS (TSL256X_TIMMING_INTEG_402MS + TSL256X_TIMMING_GAIN)
+#define TSL256X_G1X_402MS                            TSL256X_TIMMING_INTEG_402MS
+#define TSL256X_G1X_101MS                            TSL256X_TIMMING_INTEG_101MS
+#define TSL256X_G1X_13_7MS                          TSL256X_TIMMING_INTEG_13_7MS
+
+#define TSL256X_INTR_SHIFT            0x04
+#define TSL256X_INTR_DISABLED         0x00
+#define TSL256X_INTR_LEVEL            0x01
+#define TSL256X_INTR_SMB_ALERT        0x02
+#define TSL256X_INTR_TEST             0x03
+
+#define TSL256X_INT_PERSIST_EVERY     0x00
+#define TSL256X_INT_PERSIST_ANY       0x01
+#define TSL256X_INT_PERSIST_2_CYCLES  0x02
+#define TSL256X_INT_PERSIST_3_CYCLES  0x03
+#define TSL256X_INT_PERSIST_4_CYCLES  0x04
+#define TSL256X_INT_PERSIST_5_CYCLES  0x05
+#define TSL256X_INT_PERSIST_6_CYCLES  0x06
+#define TSL256X_INT_PERSIST_7_CYCLES  0x07
+#define TSL256X_INT_PERSIST_8_CYCLES  0x08
+#define TSL256X_INT_PERSIST_9_CYCLES  0x09
+#define TSL256X_INT_PERSIST_10_CYCLES 0x0A
+#define TSL256X_INT_PERSIST_11_CYCLES 0x0B
+#define TSL256X_INT_PERSIST_12_CYCLES 0x0C
+#define TSL256X_INT_PERSIST_13_CYCLES 0x0D
+#define TSL256X_INT_PERSIST_14_CYCLES 0x0E
+#define TSL256X_INT_PERSIST_15_CYCLES 0x0F
+
+#define TSL256X_ID_PARTNO_MASK        0xF0
+#define TSL256X_ID_REV_MASK           0x0F
+/** @} */
+/* -------------------------------------------------------------------------- */
+/**
+ * \name TSL256X convertion and calibration values
  * @{
  */
 
@@ -163,33 +190,33 @@
 /** @} */
 /* -------------------------------------------------------------------------- */
 /**
- * \name Callback function to handle the TSL2563 alarm interrupt and macro
+ * \name Callback function to handle the TSL256X alarm interrupt and macro
  * @{
  */
-#define TSL2563_REGISTER_INT(ptr) tsl2563_int_callback = ptr;
-extern void (*tsl2563_int_callback)(uint8_t value);
+#define TSL256X_REGISTER_INT(ptr) tsl256x_int_callback = ptr;
+extern void (*tsl256x_int_callback)(uint8_t value);
 /** @} */
 /* -------------------------------------------------------------------------- */
 /**
- * \name TSL2563 return and command values
+ * \name TSL256X return and command values
  * @{
  */
-#define TSL2563_SUCCESS         0x00
-#define TSL2563_LIGHT           0x01
-#define TSL2563_ERROR             -1
+#define TSL256X_SUCCESS         0x00
+#define TSL256X_LIGHT           0x01
+#define TSL256X_ERROR             -1
 
-#define TSL2563_ACTIVE          SENSORS_ACTIVE
-#define TSL2563_INT_OVER        HW_INT_OVER_THRS
-#define TSL2563_INT_BELOW       HW_INT_BELOW_THRS
-#define TSL2563_INT_DISABLE     HW_INT_DISABLE
-#define TSL2563_TIMMING_CFG     (HW_INT_DISABLE + 1)
+#define TSL256X_ACTIVE          SENSORS_ACTIVE
+#define TSL256X_INT_OVER        HW_INT_OVER_THRS
+#define TSL256X_INT_BELOW       HW_INT_BELOW_THRS
+#define TSL256X_INT_DISABLE     HW_INT_DISABLE
+#define TSL256X_TIMMING_CFG     (HW_INT_DISABLE + 1)
 
-#define TSL2563_VAL_READ        0x01
+#define TSL256X_VAL_READ        0x01
 /** @} */
 /* -------------------------------------------------------------------------- */
-#define TSL2563_SENSOR "TSL2563 Light Sensor"
+#define TSL256X_SENSOR "TSL256X Light Sensor"
 /* -------------------------------------------------------------------------- */
-extern const struct sensors_sensor tsl2563;
+extern const struct sensors_sensor tsl256x;
 /* -------------------------------------------------------------------------- */
 #endif
 /* -------------------------------------------------------------------------- */
