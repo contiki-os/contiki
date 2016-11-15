218f2f52320314f89416d0efd1e4281c3ecbde33
spoerk <m.spoerk@student.tugraz.at>
2016-11-15 13:25:40 +0100

    Disable debug messages in ble-mac implementation

--- cpu/cc26xx-cc13xx/net/ble-mac.c
+++ cpu/cc26xx-cc13xx/net/ble-mac.c
@@ -45,7 +45,7 @@
 #include <string.h>
 
 /*---------------------------------------------------------------------------*/
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
