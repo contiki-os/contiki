3a80394f3a424938a8bd3d759e5a92b041576f7a
flacode <flavianshemerirwe@gmail.com>
2017-04-26 16:54:41 +0300

    header file for WIMEA-ICT RSS2 application

---
+++ platform/avr-rss2/apps/wimea-ict-rss2/wimea-ict-rss2.h
@@ -0,0 +1,8 @@
+void print_help_command_menu();
+void display_reporting_interval();
+void change_reporting_interval(char *value);
+void display_system_information();
+void display_system_uptime();
+void change_node_name(char * value);
+void display_node_name();
+static void read_sensor_values(void);
