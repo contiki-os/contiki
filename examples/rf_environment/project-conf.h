22975df423d45841e89d83786f0a810fb22a8b2a
Robert Olsson <robert@herjulf.se>
2017-05-05 12:57:18 +0200

    examples/rf_environment for rf CCA (non-intrusive) monitoring added

---
+++ examples/rf_environment/project-conf.h
@@ -0,0 +1,11 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#define NETSTACK_CONF_RDC nullrdc_driver
+#define NETSTACK_CONF_MAC nullmac_driver
+#define NETSTACK_CONF_FRAMER  framer_802154
+#define NETSTACK_CONF_RADIO   rf230_driver
+
+#define RS232_BAUDRATE USART_BAUD_38400
+
+#endif /* PROJECT_CONF_H_ */
