e97f3bca269d92375c617ccd96574f5f28523558
Robert Olsson <robert@herjulf.se>
2017-06-16 10:48:52 +0200

    Fix misspelled copyright for AES128HW_test pointed out by Nicolas Tsiftes

--- examples/avr-rss2/AES128HW_test/AES128HW_test.c
+++ examples/avr-rss2/AES128HW_test/AES128HW_test.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Copyright Robert Olsson 
+ * Copyright (c) 2017, Robert Olsson 
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
