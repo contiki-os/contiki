aa3876d3f195694951057bd339961c45fe350de9
alexrayne <alexraynepe196@gmail.com>
2017-08-11 16:35:47 +0300

    !warn:DEBUG:cc26xx:prop-mode - DEBUG symbol now redefines silent

--- cpu/cc26xx-cc13xx/rf-core/prop-mode.c
+++ cpu/cc26xx-cc13xx/rf-core/prop-mode.c
@@ -79,6 +79,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 /*---------------------------------------------------------------------------*/
+#undef DEBUG
 #define DEBUG 0
 #if DEBUG
 #define PRINTF(...) printf(__VA_ARGS__)
