e5b5c801c490b545ba9e598bea9777dcea5652fd
alexrayne <alexraynepe196@gmail.com>
2017-09-01 19:20:19 +0300

    +process:process_abort - provides ability to extit process silent, without any events broadcast

--- core/sys/process.h
+++ core/sys/process.h
@@ -389,6 +389,17 @@ CCIF void process_post_synch(struct process *p,
  */
 CCIF void process_exit(struct process *p);
 
+/**
+ * \brief Makes status of current process not running, for silent process exiting
+ *
+ *       exiting of process generates events PROCESS_EVENT_EXITED for all
+ *       active processes, and PROCESS_EVENT_EXIT for current process
+ *       such notification can be useless and wasteful. so process_abort can
+ *       mark process alredy stops, to block notification in process_exit
+ * */
+CCIF void process_abort(void);
+
+#define PROCESS_ABORT()             process_abort()
 
 /**
  * Get a pointer to the currently running process.
