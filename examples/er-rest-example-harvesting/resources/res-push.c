74f53c6f24d2b17982f16eeec526f51fb4a8680e
Rjlmota <rjlmota@gmail.com>
2018-12-06 09:03:27 -0300

    .

--- examples/er-rest-example-harvesting/resources/res-push.c
+++ examples/er-rest-example-harvesting/resources/res-push.c
@@ -67,7 +67,7 @@ res_get_handler(void *request, void *response, uint8_t *buffer, uint16_t preferr
    */
   REST.set_header_content_type(response, REST.type.TEXT_PLAIN);
   REST.set_header_max_age(response, res_push.periodic->period / CLOCK_SECOND);
-  REST.set_response_payload(response, buffer, snprintf((char *)buffer, preferred_size, "VERY LONG EVENT %lu", event_counter));
+  REST.set_response_payload(response, buffer, snprintf((char *)buffer, preferred_size, "%d", 12));
 
   /* The REST.subscription_handler() will be called for observable resources by the REST framework. */
 }
